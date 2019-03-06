4e7976b390723f55c95a5606c8c2feeaafb39175
Emma Cunningham <the.cunning.ham@gmail.com>
2019-03-05 18:53:27 -0800

    test(series/scales): add test for counting clustered series

--- src/lib/series/scales.test.ts
+++ src/lib/series/scales.test.ts
@@ -1,16 +1,18 @@
+import { getGroupId } from '../utils/ids';
 import { ScaleType } from '../utils/scales/scales';
 import { XDomain } from './domains/x_domain';
-import { computeXScale } from './scales';
+import { computeXScale, countClusteredSeries } from './scales';
 
 describe('Series scales', () => {
+  const xDomain: XDomain = {
+    type: 'xDomain',
+    isBandScale: true,
+    domain: [0, 3],
+    minInterval: 1,
+    scaleType: ScaleType.Linear,
+  };
+
   test('X Scale linear min, max with bands', () => {
-    const xDomain: XDomain = {
-      type: 'xDomain',
-      isBandScale: true,
-      domain: [0, 3],
-      minInterval: 1,
-      scaleType: ScaleType.Linear,
-    };
     const scale = computeXScale(xDomain, 1, 0, 120);
     const expectedBandwidth = 120 / 4;
     expect(scale.bandwidth).toBe(expectedBandwidth);
@@ -20,13 +22,6 @@ describe('Series scales', () => {
     expect(scale.scale(3)).toBe(expectedBandwidth * 3);
   });
   test('X Scale linear inverse min, max with bands', () => {
-    const xDomain: XDomain = {
-      type: 'xDomain',
-      isBandScale: true,
-      domain: [0, 3],
-      minInterval: 1,
-      scaleType: ScaleType.Linear,
-    };
     const scale = computeXScale(xDomain, 1, 120, 0);
     const expectedBandwidth = 120 / 4;
     expect(scale.bandwidth).toBe(expectedBandwidth);
@@ -34,5 +29,45 @@ describe('Series scales', () => {
     expect(scale.scale(1)).toBe(expectedBandwidth * 2);
     expect(scale.scale(2)).toBe(expectedBandwidth * 1);
     expect(scale.scale(3)).toBe(expectedBandwidth * 0);
+  });
+  it('should count clustered series', () => {
+    const group1 = getGroupId('group_1');
+    const nonStackedSeries = [{
+      groupId: group1,
+      dataSeries: [],
+      counts: {
+        barSeries: 1,
+        lineSeries: 1,
+        areaSeries: 1,
+        basicSeries: 1,
+      },
+    }];
+    const stackedSeries = [{
+      groupId: group1,
+      dataSeries: [],
+      counts: {
+        barSeries: 3,
+        lineSeries: 1,
+        areaSeries: 1,
+        basicSeries: 1,
+      },
+    }, {
+      groupId: group1,
+      dataSeries: [],
+      counts: {
+        barSeries: 0,
+        lineSeries: 1,
+        areaSeries: 1,
+        basicSeries: 1,
+      },
+    }];
+
+    const expectedCount = {
+      nonStackedGroupCount: 1,
+      stackedGroupCount: 1,
+      totalGroupCount: 2,
+    };
+    const numClusteredSeries = countClusteredSeries(stackedSeries, nonStackedSeries);
+    expect(numClusteredSeries).toEqual(expectedCount);
   });
 });
