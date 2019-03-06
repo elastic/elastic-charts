bfd5b1de5c3a84b0828994045d73b21e6a29825b
Emma Cunningham <the.cunning.ham@gmail.com>
2019-03-05 19:10:38 -0800

    test(series/scales): fill in missing branch coverage for ordinal scales

--- src/lib/series/scales.test.ts
+++ src/lib/series/scales.test.ts
@@ -4,7 +4,7 @@ import { XDomain } from './domains/x_domain';
 import { computeXScale, countClusteredSeries } from './scales';
 
 describe('Series scales', () => {
-  const xDomain: XDomain = {
+  const xDomainLinear: XDomain = {
     type: 'xDomain',
     isBandScale: true,
     domain: [0, 3],
@@ -12,8 +12,16 @@ describe('Series scales', () => {
     scaleType: ScaleType.Linear,
   };
 
-  test('X Scale linear min, max with bands', () => {
-    const scale = computeXScale(xDomain, 1, 0, 120);
+  const xDomainOrdinal: XDomain = {
+    type: 'xDomain',
+    isBandScale: true,
+    domain: ['a', 'b'],
+    minInterval: 1,
+    scaleType: ScaleType.Ordinal,
+  };
+
+  test('should compute X Scale linear min, max with bands', () => {
+    const scale = computeXScale(xDomainLinear, 1, 0, 120);
     const expectedBandwidth = 120 / 4;
     expect(scale.bandwidth).toBe(expectedBandwidth);
     expect(scale.scale(0)).toBe(expectedBandwidth * 0);
@@ -21,8 +29,9 @@ describe('Series scales', () => {
     expect(scale.scale(2)).toBe(expectedBandwidth * 2);
     expect(scale.scale(3)).toBe(expectedBandwidth * 3);
   });
-  test('X Scale linear inverse min, max with bands', () => {
-    const scale = computeXScale(xDomain, 1, 120, 0);
+
+  test('should compute X Scale linear inverse min, max with bands', () => {
+    const scale = computeXScale(xDomainLinear, 1, 120, 0);
     const expectedBandwidth = 120 / 4;
     expect(scale.bandwidth).toBe(expectedBandwidth);
     expect(scale.scale(0)).toBe(expectedBandwidth * 3);
@@ -30,7 +39,19 @@ describe('Series scales', () => {
     expect(scale.scale(2)).toBe(expectedBandwidth * 1);
     expect(scale.scale(3)).toBe(expectedBandwidth * 0);
   });
-  it('should count clustered series', () => {
+
+  test('should compute X Scale ordinal', () => {
+    const nonZeroGroupScale = computeXScale(xDomainOrdinal, 1, 120, 0);
+    const expectedBandwidth = 60;
+    expect(nonZeroGroupScale.bandwidth).toBe(expectedBandwidth);
+    expect(nonZeroGroupScale.scale('a')).toBe(expectedBandwidth);
+    expect(nonZeroGroupScale.scale('b')).toBe(0);
+
+    const zeroGroupScale = computeXScale(xDomainOrdinal, 0, 120, 0);
+    expect(zeroGroupScale.bandwidth).toBe(expectedBandwidth);
+  });
+
+  test('should count clustered series', () => {
     const group1 = getGroupId('group_1');
     const nonStackedSeries = [{
       groupId: group1,
