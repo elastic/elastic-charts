ca861934e1d68b1b45df77c03267dd5f503fa782
Emma Cunningham <the.cunning.ham@gmail.com>
2019-03-18 07:44:06 -0700

    style: remove addressed TODOs

--- src/lib/axes/axis_utils.ts
+++ src/lib/axes/axis_utils.ts
@@ -620,8 +620,6 @@ export function isHorizontal(position: Position) {
   return !isVertical(position);
 }
 
-// TODO: only hold yDomain values (xDomain handle elsewhere), so return value can be Map<GroupId, DomainRange>
-// TODO: if domain defined on an xDomain axis, do not add and console.warn
 export function mergeDomainsByGroupId(
   axesSpecs: Map<AxisId, AxisSpec>,
   chartRotation: Rotation,
