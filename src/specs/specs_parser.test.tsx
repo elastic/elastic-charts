cc44438f35a5dd761f410eb8e20b322264aedeb2
Emma Cunningham <the.cunning.ham@gmail.com>
2019-03-23 19:10:53 -0700

    test(specs_parser): add test for componentDidUpdate

--- src/specs/specs_parser.test.tsx
+++ src/specs/specs_parser.test.tsx
@@ -11,6 +11,17 @@ describe('Specs parser', () => {
     mount(component);
     expect(chartStore.specsInitialized.get()).toBe(true);
   });
+  test('chart store initialized and computeChart on component update', () => {
+    const chartStore = new ChartStore();
+    const computeChart = jest.fn((): void => { return; });
+    chartStore.computeChart = computeChart;
+
+    const component = mount(<SpecsSpecRootComponent chartStore={chartStore} />);
+    component.update();
+    component.setState({ foo: 'bar' });
+    expect(chartStore.specsInitialized.get()).toBe(true);
+    expect(computeChart).toBeCalled();
+  });
   test('updates initialization state on unmount', () => {
     const chartStore = new ChartStore();
     chartStore.initialized.set(true);
