a1e10bcd6e241a6e5e489c910c871d38554ae0fd
Emma Cunningham <the.cunning.ham@gmail.com>
2019-03-26 15:13:08 -0700

    docs(annotations): add KIBANA_METRICS as data source

--- stories/annotations.tsx
+++ stories/annotations.tsx
@@ -1,21 +1,28 @@
 import { boolean } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import { BarSeries, Chart, getSpecId, LineAnnotation, ScaleType, Settings } from '../src';
+import { Axis, BarSeries, Chart, getSpecId, LineAnnotation, ScaleType, Settings, timeFormatter } from '../src';
+
 import {
   AnnotationDomainType,
   AnnotationPositionType,
   AnnotationType,
   LineAnnotationSpec,
+  Position,
   SeriesDatumAnnotationPosition,
 } from '../src/lib/series/specs';
-import { getAnnotationId } from '../src/lib/utils/ids';
+import { KIBANA_METRICS } from '../src/lib/series/utils/test_dataset_kibana';
+import { getAnnotationId, getAxisId } from '../src/lib/utils/ids';
+
+const dateFormatter = timeFormatter('HH:mm:ss');
 
 storiesOf('Annotations', module)
   .add('basic', () => {
+    const dataValues = [1551438150000, 1551438180000, 1551438330000, 1551438390000, 1551438450000, 1551438480000];
+
     const position: SeriesDatumAnnotationPosition = {
       domainType: AnnotationDomainType.XDomain,
-      dataValues: [1],
+      dataValues,
       positionType: AnnotationPositionType.SeriesDatum,
     };
 
@@ -29,13 +36,24 @@ storiesOf('Annotations', module)
       <Chart renderer="canvas" className={'story-chart'}>
         <Settings debug={boolean('debug', false)} />
         <LineAnnotation {...lineAnnotationProps} />
+        <Axis
+          id={getAxisId('bottom')}
+          position={Position.Bottom}
+          title={'Bottom axis'}
+          tickFormat={dateFormatter}
+        />
+        <Axis
+          id={getAxisId('left')}
+          title={'Left axis'}
+          position={Position.Left}
+        />
         <BarSeries
           id={getSpecId('bars')}
           xScaleType={ScaleType.Linear}
           yScaleType={ScaleType.Linear}
-          xAccessor="x"
-          yAccessors={['y']}
-          data={[{ x: 0, y: 2 }, { x: 1, y: 7 }, { x: 2, y: 3 }, { x: 3, y: 6 }]}
+          xAccessor={0}
+          yAccessors={[1]}
+          data={KIBANA_METRICS.metrics.kibana_os_load[0].data.slice(0, 20)}
           yScaleToDataExtent={false}
         />
       </Chart>
