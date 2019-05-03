079dc8e540973889f43d592138e435bf777ecc5e
Emma Cunningham <the.cunning.ham@gmail.com>
2019-05-03 11:51:48 -0700

    docs(bar values): add split & stacked series

--- stories/bar_chart.tsx
+++ stories/bar_chart.tsx
@@ -123,6 +123,12 @@ storiesOf('Bar Chart', module)
     );
     const data = frozenData[dataSize];
 
+    const isSplitSeries = boolean('split series', false);
+    const isStackedSeries = boolean('stacked series', false);
+
+    const splitSeriesAccessors = isSplitSeries ? ['g'] : undefined;
+    const stackAccessors = isStackedSeries ? ['x'] : undefined;
+
     return (
       <Chart renderer="canvas" className={'story-chart'}>
         <Settings theme={theme} debug={debug} rotation={chartRotation} />
@@ -145,8 +151,30 @@ storiesOf('Bar Chart', module)
           yScaleType={ScaleType.Linear}
           xAccessor="x"
           yAccessors={['y']}
+          splitSeriesAccessors={splitSeriesAccessors}
+          stackAccessors={stackAccessors}
           data={data}
           yScaleToDataExtent={false}
+        />
+        <BarSeries
+          id={getSpecId('bars2')}
+          displayValueSettings={displayValueSettings}
+          xScaleType={ScaleType.Linear}
+          yScaleType={ScaleType.Linear}
+          xAccessor="x"
+          yAccessors={['y']}
+          stackAccessors={['x']}
+          splitSeriesAccessors={['g']}
+          data={[
+            { x: 0, y: 2, g: 'a' },
+            { x: 1, y: 7, g: 'a' },
+            { x: 2, y: 3, g: 'a' },
+            { x: 3, y: 6, g: 'a' },
+            { x: 0, y: 4, g: 'b' },
+            { x: 1, y: 5, g: 'b' },
+            { x: 2, y: 8, g: 'b' },
+            { x: 3, y: 2, g: 'b' },
+          ]}
         />
       </Chart>
     );
