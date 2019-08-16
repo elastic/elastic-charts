cb91edf8bc6661a18d8cd5a5ec1cdea12071fe63
Marco Vettorello <vettorello.marco@gmail.com>
2019-08-16 10:19:28 +0200

    feat: add type overrides for chart size prop
    Now the Chart size prop takes multiple types as input: basic string or number, a tuple of string or
    number or undefined, an object with width and height (string, number or optionals).
    
    fix #177

---
+++ src/utils/chart_size.ts
@@ -0,0 +1,33 @@
+type ChartSizeArray = [number | string | undefined, number | string | undefined];
+interface ChartSizeObject {
+  width?: number | string;
+  height?: number | string;
+}
+export type ChartSize = number | string | ChartSizeArray | ChartSizeObject;
+
+function isSizeArray(size: ChartSize): size is ChartSizeArray {
+  return Array.isArray(size);
+}
+function isSizeObject(size: ChartSize): size is ChartSizeObject {
+  return !Array.isArray(size) && typeof size === 'object';
+}
+
+export function getChartSize(size: ChartSize) {
+  if (isSizeArray(size)) {
+    return {
+      width: size[0] === undefined ? '100%' : size[0],
+      height: size[1] === undefined ? '100%' : size[1],
+    };
+  }
+  if (isSizeObject(size)) {
+    return {
+      width: size.width === undefined ? '100%' : size.width,
+      height: size.height === undefined ? '100%' : size.height,
+    };
+  }
+  const sameSize = size === undefined ? '100%' : size;
+  return {
+    width: sameSize,
+    height: sameSize,
+  };
+}
