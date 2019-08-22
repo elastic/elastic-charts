ee469af6586d65d8b729a497b04717f47ff0379a
Marco Vettorello <vettorello.marco@gmail.com>
2019-08-23 01:20:40 +0200

    fix(histogram): fix overflowing annotation with single value
    This commit fix an issue where a rect annotation with x0 defined in a single value histogram, is
    drawn outisde of the chart.
    
    BREAKING CHANGE: The current coordinate configuration of a rect annotation were inverted. This
    commit now reverse them: a rect coordinate with only the x0 value will cover from the x0 value to
    the end of the domain, a rect coordinate with only the x1 value will cover the interval from the
    beginning of the domain till the x1 value.

--- .playground/playgroud.tsx
+++ .playground/playgroud.tsx
@@ -5,19 +5,17 @@ import {
   Chart,
   getAxisId,
   getSpecId,
-  niceTimeFormatter,
   Position,
   ScaleType,
   Settings,
-  BarSeries,
-  LineAnnotation,
+  RectAnnotation,
   getAnnotationId,
-  AnnotationDomainTypes,
+  HistogramBarSeries,
+  niceTimeFormatByDay,
+  timeFormatter,
 } from '../src';
-import { KIBANA_METRICS } from '../src/utils/data_samples/test_dataset_kibana';
 import { CursorEvent } from '../src/specs/settings';
 import { CursorUpdateListener } from '../src/chart_types/xy_chart/store/chart_state';
-import { Icon } from '../src/components/icons/icon';
 
 export class Playground extends React.Component {
   ref1 = React.createRef<Chart>();
@@ -32,86 +30,78 @@ export class Playground extends React.Component {
 
   render() {
     return (
-      <>
-        {renderChart(
-          '1',
-          this.ref1,
-          KIBANA_METRICS.metrics.kibana_os_load[0].data.slice(0, 15),
-          this.onCursorUpdate,
-          true,
-        )}
-        {renderChart(
-          '2',
-          this.ref2,
-          KIBANA_METRICS.metrics.kibana_os_load[1].data.slice(0, 15),
-          this.onCursorUpdate,
-          true,
-        )}
-        {renderChart('3', this.ref3, KIBANA_METRICS.metrics.kibana_os_load[1].data.slice(15, 30), this.onCursorUpdate)}
-      </>
+      <div className="chart">
+        <Chart size={{ height: 200 }}>
+          <Settings
+            tooltip={{ type: 'vertical' }}
+            debug={false}
+            legendPosition={Position.Right}
+            showLegend={true}
+            xDomain={{
+              min: 1566079200000,
+              max: 1566079200000,
+              minInterval: 86400000,
+            }}
+          />
+          <Axis id={getAxisId('count')} title="count" position={Position.Left} tickFormat={(d) => d.toFixed(2)} />
+          <Axis
+            id={getAxisId('timestamp')}
+            title="timestamp"
+            position={Position.Bottom}
+            tickFormat={timeFormatter(niceTimeFormatByDay(1))}
+          />
+          <RectAnnotation
+            annotationId={getAnnotationId('annotation1')}
+            dataValues={[
+              {
+                coordinates: {
+                  x0: 1566103254092,
+                },
+                details: `06:40`,
+              },
+            ]}
+            style={{
+              stroke: 'rgba(0, 0, 0, 0)',
+              strokeWidth: 1,
+              opacity: 0.5,
+              fill: 'rgba(200, 0, 0, 0.5)',
+            }}
+            zIndex={-2}
+          />
+          <RectAnnotation
+            annotationId={getAnnotationId('annotation2')}
+            dataValues={[
+              {
+                coordinates: {
+                  x1: 1566088404270,
+                },
+                details: `02:33`,
+              },
+            ]}
+            style={{
+              stroke: 'rgba(0, 0, 0, 0)',
+              strokeWidth: 1,
+              opacity: 0.5,
+              fill: 'rgba(0, 0, 200, 0.5)',
+            }}
+            zIndex={-2}
+          />
+          <HistogramBarSeries
+            id={getSpecId('dataset B')}
+            xScaleType={ScaleType.Time}
+            yScaleType={ScaleType.Linear}
+            data={[[1566079200000, 10]]}
+            xAccessor={0}
+            yAccessors={[1]}
+            timeZone={'local'}
+            barSeriesStyle={{
+              rect: {
+                opacity: 0.7,
+              },
+            }}
+          />
+        </Chart>
+      </div>
     );
   }
-}
-
-function renderChart(
-  key: string,
-  ref: React.RefObject<Chart>,
-  data: any,
-  onCursorUpdate?: CursorUpdateListener,
-  timeSeries: boolean = false,
-) {
-  return (
-    <div key={key} className="chart">
-      <Chart ref={ref}>
-        <Settings
-          tooltip={{ type: 'vertical' }}
-          debug={false}
-          legendPosition={Position.Right}
-          showLegend={true}
-          onCursorUpdate={onCursorUpdate}
-          rotation={0}
-        />
-        <Axis
-          id={getAxisId('timestamp')}
-          title="timestamp"
-          position={Position.Bottom}
-          tickFormat={niceTimeFormatter([1555819200000, 1555905600000])}
-        />
-        <Axis id={getAxisId('count')} title="count" position={Position.Left} tickFormat={(d) => d.toFixed(2)} />
-        <LineAnnotation
-          annotationId={getAnnotationId('annotation1')}
-          domainType={AnnotationDomainTypes.XDomain}
-          dataValues={[
-            {
-              dataValue: KIBANA_METRICS.metrics.kibana_os_load[1].data[5][0],
-              details: 'tooltip 1',
-            },
-            {
-              dataValue: KIBANA_METRICS.metrics.kibana_os_load[1].data[9][0],
-              details: 'tooltip 2',
-            },
-          ]}
-          hideLinesTooltips={true}
-          marker={<Icon type="alert" />}
-        />
-        <BarSeries
-          id={getSpecId('dataset A with long title')}
-          xScaleType={timeSeries ? ScaleType.Time : ScaleType.Linear}
-          yScaleType={ScaleType.Linear}
-          data={data}
-          xAccessor={0}
-          yAccessors={[1]}
-        />
-        <BarSeries
-          id={getSpecId('dataset B')}
-          xScaleType={ScaleType.Time}
-          yScaleType={ScaleType.Linear}
-          data={KIBANA_METRICS.metrics.kibana_os_load[1].data.slice(0, 15)}
-          xAccessor={0}
-          yAccessors={[1]}
-          stackAccessors={[0]}
-        />
-      </Chart>
-    </div>
-  );
 }
