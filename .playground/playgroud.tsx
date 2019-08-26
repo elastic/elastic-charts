aa9335432a0cd67b6f8526dd3ac24f3393f74878
Marco Vettorello <vettorello.marco@gmail.com>
2019-08-26 02:41:03 +0200

    feat: allow different series group share the same y domain
    This commit add a new property for the series: useDefaultGroupDomain that allows to split series by
    groupId but keeps and compute the new groupId domain merging it with the global one
    
    fix #169

--- .playground/playgroud.tsx
+++ .playground/playgroud.tsx
@@ -9,10 +9,11 @@ import {
   Position,
   ScaleType,
   Settings,
-  BarSeries,
+  AreaSeries,
   LineAnnotation,
   getAnnotationId,
   AnnotationDomainTypes,
+  getGroupId,
 } from '../src';
 import { KIBANA_METRICS } from '../src/utils/data_samples/test_dataset_kibana';
 import { CursorEvent } from '../src/specs/settings';
@@ -24,94 +25,63 @@ export class Playground extends React.Component {
   ref2 = React.createRef<Chart>();
   ref3 = React.createRef<Chart>();
 
-  onCursorUpdate: CursorUpdateListener = (event?: CursorEvent) => {
-    this.ref1.current!.dispatchExternalCursorEvent(event);
-    this.ref2.current!.dispatchExternalCursorEvent(event);
-    this.ref3.current!.dispatchExternalCursorEvent(event);
-  };
 
   render() {
     return (
-      <>
-        {renderChart(
-          '1',
-          this.ref1,
-          KIBANA_METRICS.metrics.kibana_os_load[0].data.slice(0, 15),
-          this.onCursorUpdate,
-          true,
-        )}
-        {renderChart(
-          '2',
-          this.ref2,
-          KIBANA_METRICS.metrics.kibana_os_load[1].data.slice(0, 15),
-          this.onCursorUpdate,
-          true,
-        )}
-        {renderChart('3', this.ref3, KIBANA_METRICS.metrics.kibana_os_load[1].data.slice(15, 30), this.onCursorUpdate)}
-      </>
-    );
+      <Chart>
+      <Settings
+        tooltip={{ type: 'vertical' }}
+        debug={false}
+        legendPosition={Position.Right}
+        showLegend={true}
+        rotation={0}
+      />
+      <Axis
+        id={getAxisId('timestamp')}
+        title="timestamp"
+        position={Position.Bottom}
+        tickFormat={niceTimeFormatter([1555819200000, 1555905600000])}
+      />
+      <Axis id={getAxisId('count')} title="count" position={Position.Left} tickFormat={(d) => d.toFixed(2)} />
+      <AreaSeries
+        id={getSpecId('dataset A1')}
+        xScaleType={ScaleType.Linear}
+        yScaleType={ScaleType.Linear}
+        data={KIBANA_METRICS.metrics.kibana_os_load[0].data.slice(0,50)}
+        xAccessor={0}
+        yAccessors={[1]}
+        stackAccessors={[0]}
+      />
+      <AreaSeries
+        id={getSpecId('dataset A2')}
+        xScaleType={ScaleType.Linear}
+        yScaleType={ScaleType.Linear}
+        data={KIBANA_METRICS.metrics.kibana_os_load[1].data.slice(0,50)}
+        xAccessor={0}
+        yAccessors={[1]}
+        stackAccessors={[0]}
+      />
+      <AreaSeries
+        id={getSpecId('dataset B1')}
+        groupId={getGroupId('aaa')}
+        useDefaultGroupDomain={true}
+        xScaleType={ScaleType.Time}
+        yScaleType={ScaleType.Linear}
+        data={KIBANA_METRICS.metrics.kibana_os_load[0].data.slice(0,50).map(d => [d[0], -d[1]])}
+        xAccessor={0}
+        yAccessors={[1]}
+        stackAccessors={[0]}
+      />
+      <AreaSeries
+        id={getSpecId('dataset B2')}
+        groupId={getGroupId('aaa')}
+        xScaleType={ScaleType.Time}
+        yScaleType={ScaleType.Linear}
+        data={KIBANA_METRICS.metrics.kibana_os_load[0].data.slice(0,50).map(d => [d[0], -d[1]])}
+        xAccessor={0}
+        yAccessors={[1]}
+        stackAccessors={[0]}
+      />
+    </Chart>);
   }
-}
-
-function renderChart(
-  key: string,
-  ref: React.RefObject<Chart>,
-  data: any,
-  onCursorUpdate?: CursorUpdateListener,
-  timeSeries: boolean = false,
-) {
-  return (
-    <div key={key} className="chart">
-      <Chart ref={ref}>
-        <Settings
-          tooltip={{ type: 'vertical' }}
-          debug={false}
-          legendPosition={Position.Right}
-          showLegend={true}
-          onCursorUpdate={onCursorUpdate}
-          rotation={0}
-        />
-        <Axis
-          id={getAxisId('timestamp')}
-          title="timestamp"
-          position={Position.Bottom}
-          tickFormat={niceTimeFormatter([1555819200000, 1555905600000])}
-        />
-        <Axis id={getAxisId('count')} title="count" position={Position.Left} tickFormat={(d) => d.toFixed(2)} />
-        <LineAnnotation
-          annotationId={getAnnotationId('annotation1')}
-          domainType={AnnotationDomainTypes.XDomain}
-          dataValues={[
-            {
-              dataValue: KIBANA_METRICS.metrics.kibana_os_load[1].data[5][0],
-              details: 'tooltip 1',
-            },
-            {
-              dataValue: KIBANA_METRICS.metrics.kibana_os_load[1].data[9][0],
-              details: 'tooltip 2',
-            },
-          ]}
-          hideLinesTooltips={true}
-          marker={<Icon type="alert" />}
-        />
-        <BarSeries
-          id={getSpecId('dataset A with long title')}
-          xScaleType={timeSeries ? ScaleType.Time : ScaleType.Linear}
-          yScaleType={ScaleType.Linear}
-          data={data}
-          xAccessor={0}
-          yAccessors={[1]}
-        />
-        <BarSeries
-          id={getSpecId('dataset B')}
-          xScaleType={ScaleType.Time}
-          yScaleType={ScaleType.Linear}
-          data={KIBANA_METRICS.metrics.kibana_os_load[1].data.slice(0, 15)}
-          xAccessor={0}
-          yAccessors={[1]}
-          stackAccessors={[0]}
-        />
-      </Chart>
-    </div>
-  );
 }
