426f1dc0bd4d0330fb461fe9e7a1763557c035e7
nickofthyme <nick.ryan.partridge@gmail.com>
2019-10-01 10:48:26 -0500

    test: run all base storybook visual screenshots

--- integration/tests/bar_chart/basic.test.ts
+++
@@ -1,9 +0,0 @@
-import { common } from '../../page_objects';
-
-describe('Bar charts - basic', () => {
-  it('visually looks correct', async () => {
-    await page.goto('http://localhost:9001/iframe.html?id=bar-chart--basic');
-    const chart = await common.getChartScreenshot();
-    expect(chart).toMatchImageSnapshot();
-  });
-});
