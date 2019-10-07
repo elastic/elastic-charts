d66153ef86f202190feb455734ac7577b2daec61
nickofthyme <nick.ryan.partridge@gmail.com>
2019-10-07 13:34:49 -0500

    WIP

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,9 +1,8 @@
 const merge = require('merge');
 const tsPreset = require('ts-jest/jest-preset');
-const puppeteerPreset = require('jest-puppeteer/jest-preset');
 const jestPuppeteerDocker = require('jest-puppeteer-docker/jest-preset');
 
-module.exports = merge(tsPreset, puppeteerPreset, jestPuppeteerDocker, {
+module.exports = merge(tsPreset, jestPuppeteerDocker, {
   setupFilesAfterEnv: ['<rootDir>/jest-env-setup.ts'],
   globals: {
     'ts-jest': {
