9a32799e1505b4fb5eb248484e08590d69738d15
nickofthyme <nick.ryan.partridge@gmail.com>
2019-10-08 09:43:36 -0500

    fix: pr comments

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,11 +1,15 @@
 const tsPreset = require('ts-jest/jest-preset');
 const jestPuppeteerDocker = require('jest-puppeteer-docker/jest-preset');
 
-module.exports = Object.assign(tsPreset, jestPuppeteerDocker, {
-  setupFilesAfterEnv: ['<rootDir>/jest-env-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json',
+module.exports = Object.assign(
+  {
+    setupFilesAfterEnv: ['<rootDir>/jest-env-setup.ts'],
+    globals: {
+      'ts-jest': {
+        tsConfig: '<rootDir>/tsconfig.json',
+      },
     },
   },
-});
+  jestPuppeteerDocker,
+  tsPreset,
+);
