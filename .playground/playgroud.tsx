35162bc1ac44fd57fe4440124850660165cfcc0e
Marco Vettorello <vettorello.marco@gmail.com>
2019-10-10 12:17:41 +0200

    fix(bars): render valid height with log scales

--- .playground/playgroud.tsx
+++ .playground/playgroud.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Axis, Chart, getAxisId, getSpecId, Position, ScaleType, Settings, AreaSeries } from '../src';
+import { Axis, Chart, getAxisId, getSpecId, Position, ScaleType, BarSeries } from '../src';
 
 export class Playground extends React.Component {
   render() {
@@ -7,7 +7,6 @@ export class Playground extends React.Component {
       <Fragment>
         <div className="chart">
           <Chart>
-            <Settings showLegend theme={{ areaSeriesStyle: { point: { visible: true } } }} />
             <Axis
               id={getAxisId('bottom')}
               position={Position.Bottom}
@@ -20,49 +19,32 @@ export class Playground extends React.Component {
               position={Position.Left}
               tickFormat={(d: any) => Number(d).toFixed(2)}
             />
-            <AreaSeries
-              id={getSpecId('bars1')}
+
+            <BarSeries
+              id={getSpecId('bars')}
               xScaleType={ScaleType.Linear}
-              yScaleType={ScaleType.Linear}
+              yScaleType={ScaleType.Log}
               xAccessor="x"
               yAccessors={['y']}
-              stackAccessors={['x']}
               splitSeriesAccessors={['g']}
-              // curve={CurveType.CURVE_MONOTONE_X}
-              data={[
-                { x: 0, y: 2, g: 'a' },
-                { x: 1, y: 7, g: 'a' },
-                { x: 2, y: 3, g: 'a' },
-                { x: 3, y: 6, g: 'a' },
-                { x: 0, y: 4, g: 'b' },
-                { x: 1, y: 5, g: 'b' },
-                { x: 2, y: 8, g: 'b' },
-                { x: 3, y: 2, g: 'b' },
-                { x: 4, y: 6, g: 'b' },
-                { x: 5, y: 7, g: 'a' },
-                { x: 5, y: 7, g: 'b' },
-                { x: 6, y: 7, g: 'a' },
-                { x: 6, y: 7, g: 'b' },
-              ]}
-            />
-            <AreaSeries
-              id={getSpecId('area2')}
-              xScaleType={ScaleType.Linear}
-              yScaleType={ScaleType.Linear}
-              xAccessor="x"
-              yAccessors={['y']}
               stackAccessors={['x']}
-              splitSeriesAccessors={['g']}
-              // curve={CurveType.CURVE_MONOTONE_X}
               data={[
-                { x: 1, y: 7, g: 'a' },
-                { x: 2, y: 3, g: 'a' },
-                { x: 3, y: 6, g: 'a' },
-                { x: 0, y: 4, g: 'b' },
-                { x: 1, y: 5, g: 'b' },
-                { x: 2, y: 8, g: 'b' },
+                { x: 1, y: 0, g: 'a' },
+                { x: 1, y: 0, g: 'b' },
+                { x: 2, y: 1, g: 'a' },
+                { x: 2, y: 1, g: 'b' },
+                { x: 3, y: 2, g: 'a' },
                 { x: 3, y: 2, g: 'b' },
-                { x: 4, y: 6, g: 'b' },
+                { x: 4, y: 3, g: 'a' },
+                { x: 4, y: 0, g: 'b' },
+                { x: 5, y: 4, g: 'a' },
+                { x: 5, y: 0.5, g: 'b' },
+                { x: 6, y: 5, g: 'a' },
+                { x: 6, y: 1, g: 'b' },
+                { x: 7, y: 6, g: 'b' },
+                { x: 8, y: 7, g: 'a' },
+                { x: 8, y: 10, g: 'b' },
+                { x: 9, y: 4, g: 'a' },
               ]}
             />
           </Chart>
