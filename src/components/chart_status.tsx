0d93c577a9ee80f4e923929d6384052226087b91
Marco Vettorello <vettorello.marco@gmail.com>
2019-11-21 13:07:10 +0100

    fix: reenable onRenderChange

--- src/components/chart_status.tsx
+++ src/components/chart_status.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { GlobalChartState } from '../state/chart_state';
+import { getSettingsSpecSelector } from '../state/selectors/get_settings_specs';
+import { RenderChangeListener } from '../specs';
 
 interface ChartStatusStateProps {
-  chartRendered: boolean;
-  chartRenderedCount: number;
+  rendered: boolean;
+  renderedCount: number;
+  onRenderChange?: RenderChangeListener;
 }
 class ChartStatusComponent extends React.Component<ChartStatusStateProps> {
+  componentDidMount() {
+    const { onRenderChange, rendered } = this.props;
+    if (onRenderChange) {
+      onRenderChange(rendered);
+    }
+  }
+  componentDidUpdate() {
+    const { onRenderChange, rendered } = this.props;
+    if (onRenderChange) {
+      onRenderChange(rendered);
+    }
+  }
   render() {
-    const { chartRendered, chartRenderedCount } = this.props;
-    return (
-      <div
-        className="echChartStatus"
-        data-ech-render-complete={chartRendered}
-        data-ech-render-count={chartRenderedCount}
-      />
-    );
+    const { rendered, renderedCount } = this.props;
+    return <div className="echChartStatus" data-ech-render-complete={rendered} data-ech-render-count={renderedCount} />;
   }
 }
 
@@ -23,8 +32,9 @@ const mapDispatchToProps = () => ({});
 
 const mapStateToProps = (state: GlobalChartState) => {
   return {
-    chartRendered: state.chartRendered,
-    chartRenderedCount: state.chartRenderedCount,
+    rendered: state.chartRendered,
+    renderedCount: state.chartRenderedCount,
+    onRenderChange: getSettingsSpecSelector(state).onRenderChange,
   };
 };
 
