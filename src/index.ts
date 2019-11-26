5fc93cffd655ceb3ac2da6876eda5c0ff74944b3
Marco Vettorello <vettorello.marco@gmail.com>
2019-11-26 10:01:39 +0100

    fix: add chartTypes type

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ export { RecursivePartial } from './utils/commons';
 export { CurveType } from './utils/curves';
 export { timeFormatter, niceTimeFormatter, niceTimeFormatByDay } from './utils/data/formatters';
 export { DataGenerator } from './utils/data_generators/data_generator';
-
+export { ChartTypes } from './chart_types';
 export { Position, Rendering, Rotation, TickFormatter } from './chart_types/xy_chart/utils/specs';
 export { TooltipType, TooltipValue, TooltipValueFormatter } from './chart_types/xy_chart/utils/interactions';
 export { DataSeriesColorsValues } from './chart_types/xy_chart/utils/series';
