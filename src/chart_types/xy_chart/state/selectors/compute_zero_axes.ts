301ded11276b35cbe5e5d344152b145b35e08515
Marta Bondyra <marta.bondyra@elastic.co>
2019-11-27 18:28:15 +0100

    fix: wrong selector

--- src/chart_types/xy_chart/state/selectors/compute_zero_axes.ts
+++ src/chart_types/xy_chart/state/selectors/compute_zero_axes.ts
@@ -3,22 +3,17 @@ import { getSettingsSpecSelector } from '../../../../state/selectors/get_setting
 import { getChartIdSelector } from '../../../../state/selectors/get_chart_id';
 import { ZeroAxisSpec } from '../../utils/specs';
 import { computeZeroAxes } from '../../utils/zero_axis_utils';
-import { getChartContainerDimensionsSelector } from '../../../../state/selectors/get_chart_container_dimensions';
 import { getChartRotationSelector } from '../../../../state/selectors/get_chart_rotation';
 import { computeSeriesGeometriesSelector } from './compute_series_geometries';
+import { computeChartDimensionsSelector } from './compute_chart_dimensions';
 
 export const computeZeroAxesSelector = createCachedSelector(
-  [
-    getSettingsSpecSelector,
-    computeSeriesGeometriesSelector,
-    getChartContainerDimensionsSelector,
-    getChartRotationSelector,
-  ],
+  [getSettingsSpecSelector, computeSeriesGeometriesSelector, computeChartDimensionsSelector, getChartRotationSelector],
   (settingsSpecs, seriesGeometries, chartDimensions, chartRotation): ZeroAxisSpec[] => {
     if (settingsSpecs.hideZeroAxes) {
       return [];
     }
     const { yScales } = seriesGeometries.scales;
-    return computeZeroAxes(yScales, chartDimensions, chartRotation);
+    return computeZeroAxes(yScales, chartDimensions.chartDimensions, chartRotation);
   },
 )(getChartIdSelector);
