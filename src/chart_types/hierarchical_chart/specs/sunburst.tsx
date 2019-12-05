da4660351d4bc0cc90e6f8bfd3ea0c4632982840
monfera <monfera.robert@gmail.com>
2019-12-05 09:02:47 +0100

    chore: minor fixes

--- src/chart_types/hierarchical_chart/specs/sunburst.tsx
+++ src/chart_types/hierarchical_chart/specs/sunburst.tsx
@@ -2,6 +2,7 @@ import { SunburstSpec } from './sunburst_spec';
 import { ChartTypes } from '../../index';
 import { getConnect, specComponentFactory } from '../../../state/spec_factory';
 import { SpecTypes } from '../../xy_chart/utils/specs';
+import { FunctionComponent } from 'react';
 
 const defaultProps = {
   chartType: ChartTypes.Sunburst,
@@ -13,6 +14,6 @@ const defaultProps = {
 type SpecRequiredProps = Pick<SunburstSpec, 'id' | 'data'>;
 type SpecOptionalProps = Partial<Omit<SunburstSpec, 'chartType' | 'specType' | 'id' | 'data'>>;
 
-export const Sunburst: React.FunctionComponent<SpecRequiredProps & SpecOptionalProps> = getConnect()(
+export const Sunburst: FunctionComponent<SpecRequiredProps & SpecOptionalProps> = getConnect()(
   specComponentFactory<SunburstSpec, 'yAccessor' | 'donut'>(defaultProps),
 );
--- src/chart_types/hierarchical_chart/state/selectors/compute_geometries.ts
+++ src/chart_types/hierarchical_chart/state/selectors/compute_geometries.ts
@@ -1,4 +1,4 @@
-import { arc, pie } from 'd3-shape';
+import { arc as d3arc, pie as d3pie } from 'd3-shape';
 import createCachedSelector from 're-reselect';
 import { GlobalChartState } from '../../../../state/chart_state';
 import { getChartThemeSelector } from '../../../../state/selectors/get_chart_theme';
@@ -14,13 +14,13 @@ const getSpecs = (state: GlobalChartState) => state.specs;
 const getParentDimensionsSelector = (state: GlobalChartState) => state.parentDimensions;
 
 function render(sunburstSpec: SunburstSpec, parentDimensions: Dimensions, theme: Theme) {
-  const paths = pie().value((d: any) => {
+  const paths = d3pie().value((d: any) => {
     return d[sunburstSpec.yAccessor];
   })(sunburstSpec.data);
   const { width, height } = parentDimensions;
   const outerRadius = width < height ? width / 2 : height / 2;
   const innerRadius = sunburstSpec.donut ? outerRadius / 2 : 0;
-  const arcGenerator = arc();
+  const arcGenerator = d3arc();
   const arcs = paths.map<ArcGeometry>((path, i) => {
     const arc = arcGenerator({
       ...path,
