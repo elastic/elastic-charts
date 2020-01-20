a5d90fadf5366380b6d80379f84a369fad865d58
monfera <monfera.robert@gmail.com>
2020-01-20 12:07:23 +0100

    fix: only the 1st layer can have a specialFirstInnermostSector

--- src/chart_types/partition_chart/layout/utils/sunburst.ts
+++ src/chart_types/partition_chart/layout/utils/sunburst.ts
@@ -9,22 +9,22 @@ export function sunburst(
   specialFirstInnermostSector: boolean,
 ): Array<Part> {
   const result: Array<Part> = [];
-  const laySubtree = (nodes: HierarchyOfArrays, { x0, y0 }: Origin) => {
+  const laySubtree = (nodes: HierarchyOfArrays, { x0, y0 }: Origin, depth: number) => {
     let currentOffsetX = x0;
     const nodeCount = nodes.length;
     for (let i = 0; i < nodeCount; i++) {
       const index = clockwiseSectors ? i : nodeCount - i - 1;
-      const node = nodes[specialFirstInnermostSector ? (index + 1) % nodeCount : index];
+      const node = nodes[depth === 1 && specialFirstInnermostSector ? (index + 1) % nodeCount : index];
       const area = areaAccessor(node);
       const X0 = currentOffsetX;
       currentOffsetX += area;
       result.push({ node, x0: X0, y0, x1: X0 + area, y1: y0 + 1 });
       const children = childrenAccessor(node);
       if (children && children.length) {
-        laySubtree(children, { x0: X0, y0: y0 + 1 });
+        laySubtree(children, { x0: X0, y0: y0 + 1 }, depth + 1);
       }
     }
   };
-  laySubtree(nodes, { x0, y0 });
+  laySubtree(nodes, { x0, y0 }, 1);
   return result;
 }
