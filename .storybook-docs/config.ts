f1b66303e7a511808c98b0e8fdff445c5401384c
rshen91 <rachelwshen@gmail.com>
2020-01-23 13:37:24 -0700

    feat: set up yarn start-docs

---
+++ .storybook-docs/config.ts
@@ -0,0 +1,43 @@
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator, configure, addParameters } from '@storybook/react';
+
+import { switchTheme } from '../.storybook/theme_service';
+
+switchTheme('light');
+
+import './style.scss';
+
+import { create } from '@storybook/theming';
+
+addParameters({
+  options: {
+    theme: create({
+      base: 'light',
+      brandTitle: 'Elastic Charts',
+      brandUrl: 'https://github.com/elastic/elastic-charts',
+      brandImage:
+        'https://static-www.elastic.co/v3/assets/bltefdd0b53724fa2ce/blt6ae3d6980b5fd629/5bbca1d1af3a954c36f95ed3/logo-elastic.svg',
+    }),
+    panelPosition: 'right',
+    sidebarAnimations: true,
+  },
+  info: {
+    inline: true,
+    source: true,
+    propTables: false,
+    styles: {
+      infoBody: {
+        fontSize: '14px',
+        marginTop: 0,
+        marginBottom: 0,
+      },
+    },
+  },
+  docs: {},
+});
+
+addDecorator(withKnobs);
+addDecorator(withInfo);
+
+configure(require.context('../docs', true, /\.tsx$/), module);
---
+++ .storybook-docs/webpack.config.js
@@ -0,0 +1,95 @@
+// eslint-disable-next-line
+const path = require('path');
+const webpack = require('webpack');
+
+const nonce = 'Pk1rZ1XDlMuYe8ubWV3Lh0BzwrTigJQ=';
+const scssLoaders = [
+  {
+    loader: 'css-loader',
+    options: { importLoaders: 1 },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: [require('autoprefixer')],
+    },
+  },
+  'sass-loader',
+];
+
+module.exports = async ({ config }) => {
+  config.plugins.push(new webpack.EnvironmentPlugin({ RNG_SEED: null }));
+
+  config.module.rules.push({
+    test: /\.tsx?$/,
+    loader: 'ts-loader',
+    exclude: /node_modules/,
+    options: {
+      configFile: 'tsconfig.json',
+      transpileOnly: true,
+    },
+  });
+
+  config.module.rules.push({
+    test: /\.tsx?$/,
+    loader: require.resolve('react-docgen-typescript-loader'),
+    exclude: /node_modules/,
+  });
+
+  // Replace default css rules with nonce
+  config.module.rules = config.module.rules.filter(({ test }) => !test.test('.css'));
+  config.module.rules.push({
+    test: /\.css$/,
+    use: [
+      {
+        loader: 'style-loader',
+        options: {
+          attrs: {
+            nonce,
+          },
+        },
+      },
+      {
+        loader: 'css-loader',
+        options: { importLoaders: 1 },
+      },
+    ],
+  });
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    include: [path.resolve(__dirname, '../.storybook'), path.resolve(__dirname, '../node_modules/@elastic')],
+    use: [
+      {
+        loader: 'style-loader',
+        options: {
+          attrs: {
+            nonce,
+          },
+        },
+      },
+      ...scssLoaders,
+    ],
+  });
+
+  // Used for lazy loaded scss files
+  config.module.rules.push({
+    test: /\.scss$/,
+    resourceQuery: /^\?lazy$/,
+    use: [
+      {
+        loader: 'style-loader/useable',
+        options: {
+          attrs: {
+            nonce,
+          },
+        },
+      },
+      ...scssLoaders,
+    ],
+  });
+
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
+};
