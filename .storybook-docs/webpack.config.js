926f0f681d034975a58027f7027028c64c020f5e
rshen91 <rachelwshen@gmail.com>
2020-01-31 08:55:56 -0700

    WIP

--- .storybook-docs/webpack.config.js
+++ .storybook-docs/webpack.config.js
@@ -76,12 +76,12 @@ module.exports = async ({ config }) => {
   });
 
   config.module.rules.push({
-    test: /\.(ts|tsx|mdx)$/,
+    test: /\.(ts|tsx)$/,
     use: [
       {
         loader: require.resolve('babel-loader'),
         options: {
-          presets: [['react-app', { flow: false, typescript: true }]],
+          presets: [['react-app', { flow: false, typescript: true, sourceLoaderOptions: null }]],
         },
       },
       {
