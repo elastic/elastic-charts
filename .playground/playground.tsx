14a4da43701ee4fbeafd5c686141d7381bec8285
Marco Vettorello <vettorello.marco@gmail.com>
2020-03-10 10:29:39 +0100

    chore: removed partition from playground

--- .playground/playground.tsx
+++ .playground/playground.tsx
@@ -25,38 +25,9 @@ import {
   Settings,
   PartitionElementEvent,
   XYChartElementEvent,
-  Partition,
   BarSeries,
 } from '../src';
 
-type PieDatum = [string, number, string, number];
-const pieData: Array<PieDatum> = [
-  ['CN', 301, 'CN', 64],
-  ['CN', 301, 'IN', 44],
-  ['CN', 301, 'US', 24],
-  ['CN', 301, 'ID', 13],
-  ['CN', 301, 'BR', 8],
-  ['IN', 245, 'IN', 41],
-  ['IN', 245, 'CN', 36],
-  ['IN', 245, 'US', 22],
-  ['IN', 245, 'BR', 11],
-  ['IN', 245, 'ID', 10],
-  ['US', 130, 'CN', 33],
-  ['US', 130, 'IN', 23],
-  ['US', 130, 'US', 9],
-  ['US', 130, 'ID', 7],
-  ['US', 130, 'BR', 5],
-  ['ID', 55, 'CN', 9],
-  ['ID', 55, 'IN', 8],
-  ['ID', 55, 'ID', 5],
-  ['ID', 55, 'BR', 4],
-  ['ID', 55, 'US', 3],
-  ['PK', 43, 'CN', 8],
-  ['PK', 43, 'IN', 5],
-  ['PK', 43, 'US', 5],
-  ['PK', 43, 'FR', 2],
-  ['PK', 43, 'PK', 2],
-];
 export class Playground extends React.Component<{}, { isSunburstShown: boolean }> {
   onClick = (elements: Array<PartitionElementEvent | XYChartElementEvent>) => {
     // eslint-disable-next-line no-console
@@ -101,36 +72,6 @@ export class Playground extends React.Component<{}, { isSunburstShown: boolean }
                   return `${d} $`;
                 },
               }}
-            />
-          </Chart>
-        </div>
-        <div className="chart">
-          <Chart>
-            <Settings onElementClick={this.onClick} />
-            <Partition
-              id="pie"
-              data={pieData}
-              valueAccessor={(d) => {
-                return d[3];
-              }}
-              layers={[
-                {
-                  groupByRollup: (d: PieDatum) => {
-                    return d[0];
-                  },
-                  nodeLabel: (d) => {
-                    return `dest: ${d}`;
-                  },
-                },
-                {
-                  groupByRollup: (d: PieDatum) => {
-                    return d[2];
-                  },
-                  nodeLabel: (d) => {
-                    return `source: ${d}`;
-                  },
-                },
-              ]}
             />
           </Chart>
         </div>
