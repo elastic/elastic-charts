698617be8f98701cd81df5f753e40388c18d7df4
Marco Vettorello <vettorello.marco@gmail.com>
2020-04-08 10:22:19 +0200

    refactor: cleanup highlighter code

--- src/chart_types/partition_chart/renderer/canvas/highlighter.tsx
+++ src/chart_types/partition_chart/renderer/canvas/highlighter.tsx
@@ -29,26 +29,49 @@ import { getPickedShapes } from '../../state/selectors/picked_shapes';
 import { PartitionLayout } from '../../layout/types/config_types';
 
 interface HighlighterProps {
+  chartId: string;
   initialized: boolean;
-  type: PartitionLayout;
+  partitionLayout: PartitionLayout;
   geometries: QuadViewModel[];
   diskCenter: PointObject;
-  radius: number;
-  inverted: boolean;
+  outerRadius: number;
+  renderAsOverlay: boolean;
 }
 
 const EPSILON = 1e-6;
 
-function getShapeFromValues(x: number, y: number, r: number, a0: number, a1: number, ccw: number): string {
+/**
+ * This function return an SVG arc path from the same parameters of the canvas.arc function call
+ * @param x The horizontal coordinate of the arc's center
+ * @param y The vertical coordinate of the arc's center
+ * @param r The arc's radius. Must be positive
+ * @param a0 The angle at which the arc starts in radians, measured from the positive x-axis
+ * @param a1 The angle at which the arc ends in radians, measured from the positive x-axis
+ * @param ccw If 1, draws the arc counter-clockwise between the start and end angles
+ */
+function getSectorShapeFromCanvasArc(x: number, y: number, r: number, a0: number, a1: number, ccw: number): string {
   const cw = 1 ^ ccw;
   const da = ccw ? a0 - a1 : a1 - a0;
   return `A${r},${r},0,${+(da >= Math.PI)},${cw},${x + r * Math.cos(a1)},${y + r * Math.sin(a1)}`;
 }
 
+/**
+ * Renders an SVG Rect from a partition chart QuadViewModel
+ * @param geometry the QuadViewModel
+ * @param key the key to apply to the react element
+ * @param fillColor the optional fill color
+ */
 function renderRectangles(geometry: QuadViewModel, key: string, fillColor = 'black') {
   const { x0, x1, y0px, y1px } = geometry;
   return <rect key={key} x={x0} y={y0px} width={Math.abs(x1 - x0)} height={Math.abs(y1px - y0px)} fill={fillColor} />;
 }
+
+/**
+ * Render an SVG path or circle from a partition chart QuadViewModel
+ * @param geometry the QuadViewModel
+ * @param key the key to apply to the react element
+ * @param fillColor the optional fill color
+ */
 function renderSector(geometry: QuadViewModel, key: string, fillColor = 'black') {
   const { x0, x1, y0px, y1px } = geometry;
   if ((Math.abs(x0 - x1) + TAU) % TAU < EPSILON) {
@@ -58,17 +81,18 @@ function renderSector(geometry: QuadViewModel, key: string, fillColor = 'black')
   const X1 = x1 - TAU / 4;
   const path = [
     `M${y0px * Math.cos(X0)},${y0px * Math.sin(X0)}`,
-    getShapeFromValues(0, 0, y0px, X0, X1, 0),
+    getSectorShapeFromCanvasArc(0, 0, y0px, X0, X1, 0),
     `L${y1px * Math.cos(X1)},${y1px * Math.sin(X1)}`,
-    getShapeFromValues(0, 0, y1px, X1, X0, 1),
+    getSectorShapeFromCanvasArc(0, 0, y1px, X1, X0, 1),
     'Z',
   ].join(' ');
   return <path key={key} d={path} fill={fillColor} />;
 }
 
-function renderGeometries(geometries: QuadViewModel[], type: PartitionLayout, fillColor = 'black') {
+function renderGeometries(geometries: QuadViewModel[], partitionLayout: PartitionLayout, fillColor = 'black') {
   let maxDepth = -1;
-  if (type === PartitionLayout.treemap) {
+  // we should render only the deepest geometries of the tree to avoid overlaying highlighted geometries
+  if (partitionLayout === PartitionLayout.treemap) {
     maxDepth = geometries.reduce((acc, geom) => {
       return Math.max(acc, geom.depth);
     }, 0);
@@ -81,7 +105,7 @@ function renderGeometries(geometries: QuadViewModel[], type: PartitionLayout, fi
       return true;
     })
     .map((geometry, index) => {
-      if (type === PartitionLayout.sunburst) {
+      if (partitionLayout === PartitionLayout.sunburst) {
         return renderSector(geometry, `${index}`, fillColor);
       }
 
@@ -91,96 +115,122 @@ function renderGeometries(geometries: QuadViewModel[], type: PartitionLayout, fi
 
 class HighlighterComponent extends React.Component<HighlighterProps> {
   static displayName = 'Highlighter';
-  renderMask() {
-    const { geometries, diskCenter, radius, type } = this.props;
+
+  renderAsMask() {
+    const { geometries, diskCenter, outerRadius, partitionLayout, chartId } = this.props;
+    const maskId = `echHighlighterMask__${chartId}`;
     return (
       <>
         <defs>
           <mask id="echHighlighterMask">
             <rect x={0} y={0} width="1500" height="1500" fill="white" />
-            <g transform={`translate(${diskCenter.x}, ${diskCenter.y})`}>{renderGeometries(geometries, type)}</g>
+            <g transform={`translate(${diskCenter.x}, ${diskCenter.y})`}>
+              {renderGeometries(geometries, partitionLayout)}
+            </g>
           </mask>
         </defs>
 
         <circle
           cx={diskCenter.x}
           cy={diskCenter.y}
-          r={radius}
-          mask="url(#echHighlighterMask)"
+          r={outerRadius}
+          mask={`url(#${maskId})`}
           opacity="0.75"
           fill="white"
         />
       </>
     );
   }
-  renderInverted() {
-    const { geometries, diskCenter, type } = this.props;
+
+  renderAsOverlay() {
+    const { geometries, diskCenter, partitionLayout } = this.props;
     return (
       <g transform={`translate(${diskCenter.x}, ${diskCenter.y})`}>
-        {renderGeometries(geometries, type, 'rgba(255, 255, 255, 0.35')}
+        {renderGeometries(geometries, partitionLayout, 'rgba(255, 255, 255, 0.35')}
       </g>
     );
   }
+
   render() {
-    const { geometries, inverted } = this.props;
-    if (geometries.length === 0) {
-      return null;
-    }
-    const elements = inverted ? this.renderInverted() : this.renderMask();
+    const { renderAsOverlay } = this.props;
+
     return (
       <svg className="echHighlighter" width="100%" height="100%">
-        {elements}
+        {renderAsOverlay ? this.renderAsOverlay() : this.renderAsMask()}
       </svg>
     );
   }
 }
 
 const DEFAULT_PROPS: HighlighterProps = {
+  chartId: 'empty',
   initialized: false,
   geometries: [],
   diskCenter: {
     x: 0,
     y: 0,
   },
-  radius: 10,
-  inverted: false,
-  type: PartitionLayout.sunburst,
+  outerRadius: 10,
+  renderAsOverlay: false,
+  partitionLayout: PartitionLayout.sunburst,
 };
 
 const legendMapStateToProps = (state: GlobalChartState): HighlighterProps => {
   if (!isInitialized(state)) {
     return DEFAULT_PROPS;
   }
-  const model = partitionGeometries(state);
+
+  const { chartId } = state;
+  const {
+    outerRadius,
+    diskCenter,
+    config: { partitionLayout },
+  } = partitionGeometries(state);
+
+  const geometries = getHighlightedSectorsSelector(state);
   return {
+    chartId,
     initialized: true,
-    geometries: getHighlightedSectorsSelector(state),
-    diskCenter: model.diskCenter,
-    radius: model.outerRadius,
-    inverted: false,
-    type: model.config.partitionLayout,
+    renderAsOverlay: false,
+    geometries,
+    diskCenter,
+    outerRadius,
+    partitionLayout,
   };
 };
-
-/** @internal */
-export const HighlighterFromLegend = connect(legendMapStateToProps)(HighlighterComponent);
 
 const hoverMapStateToProps = (state: GlobalChartState): HighlighterProps => {
   if (!isInitialized(state)) {
     return DEFAULT_PROPS;
   }
-  const model = partitionGeometries(state);
+
+  const { chartId } = state;
+  const {
+    outerRadius,
+    diskCenter,
+    config: { partitionLayout },
+  } = partitionGeometries(state);
+
   const geometries = getPickedShapes(state);
-  const type = model.config.partitionLayout;
   return {
+    chartId,
     initialized: true,
+    renderAsOverlay: true,
+    diskCenter,
+    outerRadius,
     geometries,
-    diskCenter: model.diskCenter,
-    radius: model.outerRadius,
-    inverted: true,
-    type,
+    partitionLayout,
   };
 };
 
-/** @internal */
+/**
+ * Partition chart highlighter from legend events
+ * @internal
+ */
+export const HighlighterFromLegend = connect(legendMapStateToProps)(HighlighterComponent);
+
+/**
+ * Partition chart highlighter from mouse hover events
+ * @internal
+ */
 export const HighlighterFromHover = connect(hoverMapStateToProps)(HighlighterComponent);
