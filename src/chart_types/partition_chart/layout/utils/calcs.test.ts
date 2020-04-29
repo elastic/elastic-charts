fc86b2afe8ab87743ea2ea7bbf7cada7b68dab2a
rshen91 <rachelwshen@gmail.com>
2020-04-29 13:38:38 -0600

    test: add unit tests for calc file

---
+++ src/chart_types/partition_chart/layout/utils/calcs.test.ts
@@ -0,0 +1,63 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. */
+
+import { makeHighContrastColor, getBackgroundWithContainerColorFromUser } from './calcs';
+
+describe('Get ideal contrast color', () => {
+  // test inputs
+  test('hex input - should change white text to black when background is white', () => {
+    const expected = '#000';
+    const result = makeHighContrastColor('#fff', '#fff');
+    expect(result).toEqual(expected);
+  });
+  test('rgb input - should change white text to black when background is white ', () => {
+    const expected = '#000';
+    const result = makeHighContrastColor('rgb(255, 255, 255)', 'rgb(255, 255, 255)');
+    expect(result).toEqual(expected);
+  });
+  test('rgba input - should change white text to black when background is white ', () => {
+    const expected = '#000';
+    const result = makeHighContrastColor('rgba(255, 255, 255, 1)', 'rgba(255, 255, 255, 1)');
+    expect(result).toEqual(expected);
+  });
+  test('word input - should change white text to black when background is white ', () => {
+    const expected = '#000';
+    const result = makeHighContrastColor('white', 'white');
+    expect(result).toEqual(expected);
+  });
+  // test contrast computation
+  test('should provide at least 4.5 contrast', () => {
+    const foreground = '#fff'; // white
+    const background = 'rgba(255, 255, 51, 0.3)'; // light yellow
+    const result = '#000'; // black
+    expect(result).toEqual(makeHighContrastColor(foreground, background));
+  });
+});
+
+describe('Get sum of two colors for background color', () => {
+  test('should return correct RGBA tuple with opacity greater than 0.7', () => {
+    const expected = [13, 5, 35, 1];
+    const result = getBackgroundWithContainerColorFromUser('rgba(121, 47, 249, 0.8)', '#1c1c24');
+    expect(result).toEqual(expected);
+  });
+  test('should return correct RGBA tuple with opacity less than 0.7', () => {
+    const expected = [227, 141, 142, 0.51];
+    const result = getBackgroundWithContainerColorFromUser('rgba(228, 26, 28, 0.3)', 'rgba(225, 255, 255, 1)');
+    expect(result).toEqual(expected);
+  });
+});
