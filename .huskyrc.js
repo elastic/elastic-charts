efea7bc30db2a1674a8ab02407f5e5db5dcb4dab
Marco Vettorello <vettorello.marco@gmail.com>
2020-05-26 20:07:26 +0200

    ci: remove running tests on pre-commit hook

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,10 +4,7 @@ module.exports = {
   hooks: {
     'pre-commit': tasks([
       'pretty-quick',
-      'yarn run typecheck:all',
       'yarn run lint',
-      'yarn run test',
-      'yarn run test:tz',
     ]),
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
