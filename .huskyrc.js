e2136d0cf02bb96ba8a75458355fd3086c5af755
Marco Vettorello <vettorello.marco@gmail.com>
2020-05-26 20:19:52 +0200

    ci: fix husky

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,10 +2,6 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks([
-      'pretty-quick',
-      'yarn run lint',
-    ]),
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS && pretty-quick && yarn run lint',
   },
 };
