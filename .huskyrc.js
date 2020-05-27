a5a15babba59477782e9d776782c3c0dbf0fbe18
Marco Vettorello <vettorello.marco@gmail.com>
2020-05-27 17:41:46 +0200

    build: add tscheck to commit hook

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,6 +2,6 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS && pretty-quick && yarn run lint',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS && yarn typecheck:all && yarn pq && yarn lint',
   },
 };
