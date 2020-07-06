a734b7252928e112a322675223599b58f88774d7
Marco Vettorello <vettorello.marco@gmail.com>
2020-07-06 16:05:42 +0200

    ci: add gh checks config only

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -15,10 +15,10 @@ yarn build
 ### run linter
 ###
 echo " -- run linter"
-yarn run github-checks-reporter yarn lint yarn lint
+yarn lint
 
 ###
 ### run prettier check
 ###
 echo " -- run prettier check"
-yarn run github-checks-reporter yarn prettier yarn prettier:check
+yarn prettier:check
