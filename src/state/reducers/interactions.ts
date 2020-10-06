6719ba494b0eafea64ae959a5df6d69780514044
Marco Vettorello <vettorello.marco@gmail.com>
2020-10-06 17:15:59 +0200

    fix: detect dragging only by the delta changes

--- src/state/reducers/interactions.ts
+++ src/state/reducers/interactions.ts
@@ -62,14 +62,15 @@ export function interactionsReducer(
       return state;
 
     case ON_POINTER_MOVE:
-      const delta =
-        state.pointer.down && getDelta(state.pointer.down.position, action.position) > DRAG_DETECTION_PIXEL_DELTA;
+      const isDragging = !!(
+        state.pointer.down && getDelta(state.pointer.down.position, action.position) > DRAG_DETECTION_PIXEL_DELTA
+      );
       return {
         ...state,
         pointer: {
           ...state.pointer,
-          // enable the dragging flag only if the time between the down action and the move action is > 100ms
-          dragging: !!(state.pointer.down && action.time - state.pointer.down.time >= DRAG_DETECTION_TIMEOUT && delta),
+          // enable the dragging flag only if the pixel delta between down and move is greater then 4 pixel
+          dragging: isDragging,
           current: {
             position: {
               ...action.position,
