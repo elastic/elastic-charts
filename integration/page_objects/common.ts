d14a03af9314eb242c83028f76325017679a3455
rshen91 <rachelwshen@gmail.com>
2020-12-02 15:55:02 -0700

    test: add style consistency to test

--- integration/page_objects/common.ts
+++ integration/page_objects/common.ts
@@ -385,6 +385,18 @@ class CommonPage {
   ) {
     const action = async () => {
       await this.clickMouseRelativeToDOMElement({ top: 242, left: 910 }, this.chartSelector);
+      await page.addStyleTag({
+        content: `*,
+                  *::after,
+                  *::before {
+                    transition-delay: 0s !important;
+                    transition-duration: 0s !important;
+                    animation-delay: -0.0001s !important;
+                    animation-duration: 0s !important;
+                    animation-play-state: paused !important;
+                    caret-color: transparent !important;
+                  }`,
+      });
       // eslint-disable-next-line no-restricted-syntax
       for (const actions of keyboardEvents) {
         await this.pressKey(actions.actionLabel, actions.count);
