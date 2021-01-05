46f7336121121758373737235088344aa45d26a3
Marco Vettorello <vettorello.marco@gmail.com>
2021-01-05 15:06:10 +0100

    build(jest): update jest related packages
    The commit updates all jest related files. Increase TS version to 3.9 to align with the jest
    required versions.

--- src/chart_types/partition_chart/layout/viewmodel/fill_text_layout.ts
+++ src/chart_types/partition_chart/layout/viewmodel/fill_text_layout.ts
@@ -391,7 +391,7 @@ function fill<C>(
     leftAlign: boolean,
     middleAlign: boolean,
   ) {
-    const { maxRowCount, fillLabel, fontFamily: configFontFamily } = config;
+    const { maxRowCount, fillLabel } = config;
     return (allFontSizes: Pixels[][], textFillOrigin: PointTuple, node: QuadViewModel): RowSet => {
       const container = shapeConstructor(node);
       const rotation = getRotation(node);
@@ -415,9 +415,6 @@ function fill<C>(
         textContrast,
         textOpacity,
       } = {
-        fontFamily: configFontFamily,
-        fontWeight: 'normal',
-        padding: 2,
         ...fillLabel,
         valueFormatter: formatter,
         ...layer.fillLabel,
@@ -432,8 +429,6 @@ function fill<C>(
       );
 
       const valueFont = {
-        fontFamily: configFontFamily,
-        fontWeight: 'normal',
         ...fillLabel,
         ...fillLabel.valueFont,
         ...layer.fillLabel,
