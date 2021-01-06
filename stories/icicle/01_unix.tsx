5ec771844199a3e07cad7a606898e37c9b1fa627
Robert Monfera <monfera.robert@gmail.com>
2021-01-07 00:43:56 +0100

    test: image baseline with no flatMap and apparent vertical truncation

--- stories/icicle/01_unix.tsx
+++ stories/icicle/01_unix.tsx
@@ -42,7 +42,16 @@ const flatTree = ({ c, n, v }: Node, depth: number): Row[] => {
   if (!c) {
     return [{ [`layer_${depth}`]: n, value: v, depth }];
   }
-  const childrenRows: Row[] = c.flatMap((child) => flatTree(child, depth + 1));
+  // looks like our test runner can't run c.flatMap(...)
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  // eslint-disable-next-line prefer-spread
+  const childrenRows: Row[] = [].concat.apply(
+    [],
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    c.map((child) => flatTree(child, depth + 1)),
+  );
   const childrenTotal = childrenRows.reduce((p, { value }) => p + value, 0);
   const missing = Math.max(0, v - childrenTotal);
   if (missing > 0) {
