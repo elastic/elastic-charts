f682f03822dbc2b8104aa7f5ff929798f529083c
Marco Vettorello <vettorello.marco@gmail.com>
2021-01-29 15:19:39 +0100

    fix: add theme min radius to point shape

--- src/chart_types/xy_chart/rendering/points.ts
+++ src/chart_types/xy_chart/rendering/points.ts
@@ -20,7 +20,7 @@ import { Scale } from '../../../scales';
 import { Color, isNil } from '../../../utils/common';
 import { Dimensions } from '../../../utils/dimensions';
 import { BandedAccessorType, PointGeometry } from '../../../utils/geometry';
-import { LineStyle, PointStyle } from '../../../utils/themes/theme';
+import { PointStyle } from '../../../utils/themes/theme';
 import { GeometryType, IndexedGeometryMap } from '../utils/indexed_geometry_map';
 import { DataSeries, DataSeriesDatum, FilledValues, XYChartSeriesIdentifier } from '../utils/series';
 import { PointStyleAccessor, StackMode } from '../utils/specs';
@@ -35,8 +35,6 @@ import {
   YDefinedFn,
 } from './utils';
 
-const DEFAULT_RADIUS = 1;
-
 /** @internal */
 export function renderPoints(
   shift: number,
@@ -45,7 +43,6 @@ export function renderPoints(
   yScale: Scale,
   panel: Dimensions,
   color: Color,
-  lineStyle: LineStyle,
   pointStyle: PointStyle,
   hasY0Accessors: boolean,
   markSizeOptions: MarkSizeOptions,
@@ -117,7 +114,7 @@ export function renderPoints(
       const pointGeometry: PointGeometry = {
         x,
         y,
-        radius: Math.max(radius, DEFAULT_RADIUS),
+        radius: Math.max(radius, pointStyle.radius),
         color,
         style,
         value: {
