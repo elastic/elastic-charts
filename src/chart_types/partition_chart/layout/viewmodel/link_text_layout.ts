135c6942a0d2bef8ec1d43af3b1aadf9c606e104
Robert Monfera <monfera.robert@gmail.com>
2021-02-05 23:21:49 +0100

    refactor: break down large link label function

--- src/chart_types/partition_chart/layout/viewmodel/link_text_layout.ts
+++ src/chart_types/partition_chart/layout/viewmodel/link_text_layout.ts
@@ -22,6 +22,7 @@ import { TAU } from '../../../../common/constants';
 import {
   Distance,
   meanAngle,
+  Pixels,
   PointTuple,
   PointTuples,
   trueBearingToStandardPositionAngle,
@@ -30,7 +31,7 @@ import { integerSnap, monotonicHillClimb } from '../../../../common/optimize';
 import { Box, Font, TextAlign, TextMeasure } from '../../../../common/text_utils';
 import { ValueFormatter, Color } from '../../../../utils/common';
 import { Point } from '../../../../utils/point';
-import { Config } from '../types/config_types';
+import { Config, LinkLabelConfig } from '../types/config_types';
 import { LinkLabelVM, RawTextGetter, ShapeTreeNode, ValueGetterFunction } from '../types/viewmodel_types';
 
 function cutToLength(s: string, maxLength: number) {
@@ -89,83 +90,24 @@ export function linkTextLayout(
     .filter((n: ShapeTreeNode) => n.depth === maxDepth) // only the outermost ring can have links
     .sort((n1: ShapeTreeNode, n2: ShapeTreeNode) => Math.abs(n2.x0 - n2.x1) - Math.abs(n1.x0 - n1.x1))
     .slice(0, linkLabel.maxCount) // largest linkLabel.MaxCount slices
-    .sort((n1: ShapeTreeNode, n2: ShapeTreeNode) => {
-      const mid1 = meanAngle(n1.x0, n1.x1);
-      const mid2 = meanAngle(n2.x0, n2.x1);
-      const dist1 = Math.min(Math.abs(mid1 - TAU / 4), Math.abs(mid1 - (3 * TAU) / 4));
-      const dist2 = Math.min(Math.abs(mid2 - TAU / 4), Math.abs(mid2 - (3 * TAU) / 4));
-      return dist1 - dist2;
-    })
-    .map((node: ShapeTreeNode) => {
-      const midAngle = trueBearingToStandardPositionAngle((node.x0 + node.x1) / 2);
-      const north = midAngle < TAU / 2 ? 1 : -1;
-      const rightSide = TAU / 4 < midAngle && midAngle < (3 * TAU) / 4 ? 0 : 1;
-      const west = rightSide ? 1 : -1;
-      const cos = Math.cos(midAngle);
-      const sin = Math.sin(midAngle);
-      const x0 = cos * anchorRadius;
-      const y0 = sin * anchorRadius;
-      const x = cos * (anchorRadius + linkLabel.radiusPadding);
-      const y = sin * (anchorRadius + linkLabel.radiusPadding);
-      const poolIndex = rightSide + (1 - north);
-      const relativeY = north * y;
-      currentY[poolIndex] = Math.max(currentY[poolIndex] + rowPitch, relativeY + yRelativeIncrement, rowPitch / 2);
-      const cy = north * currentY[poolIndex];
-      const stemFromX = x;
-      const stemFromY = y;
-      const stemToX = x + north * west * cy - west * relativeY;
-      const stemToY = cy;
-      const rawText = rawTextGetter(node);
-      const labelText = cutToLength(rawText, maxTextLength);
-      const valueText = valueFormatter(valueGetter(node));
-
-      // eslint-disable-next-line no-shadow
-      const labelFontSpec: Font = {
-        ...linkLabel,
-      };
-      // eslint-disable-next-line no-shadow
-      const valueFontSpec: Font = {
-        ...linkLabel,
-        ...linkLabel.valueFont,
-      };
-      const translateX = stemToX + west * (linkLabel.horizontalStemLength + linkLabel.gap);
-      const { width: valueWidth } = measure(linkLabel.fontSize, [{ ...valueFontSpec, text: valueText }])[0];
-      const widthAdjustment = valueWidth + 2 * linkLabel.fontSize; // gap between label and value, plus possibly 2em wide ellipsis
-      const allottedLabelWidth = Math.max(
-        0,
-        rightSide
-          ? rectWidth - diskCenter.x - translateX - widthAdjustment
-          : diskCenter.x + translateX - widthAdjustment,
-      );
-      const { text, width, verticalOffset } =
-        linkLabel.fontSize / 2 <= cy + diskCenter.y && cy + diskCenter.y <= rectHeight - linkLabel.fontSize / 2
-          ? fitText(measure, labelText, allottedLabelWidth, linkLabel.fontSize, {
-              ...labelFontSpec,
-              text: labelText,
-            })
-          : { text: '', width: 0, verticalOffset: 0 };
-      // eslint-disable-next-line no-shadow
-      const linkLabels: PointTuples = [
-        [x0, y0],
-        [stemFromX, stemFromY],
-        [stemToX, stemToY],
-        [stemToX + west * linkLabel.horizontalStemLength, stemToY],
-      ];
-      const translate: PointTuple = [translateX, stemToY];
-      const textAlign: TextAlign = rightSide ? 'left' : 'right';
-      return {
-        linkLabels,
-        translate,
-        textAlign,
-        text,
-        valueText,
-        width,
-        valueWidth,
-        verticalOffset,
-        labelFontSpec,
-        valueFontSpec,
-      };
-    })
+    .sort(linkLabelCompare)
+    .map(
+      nodeToLinkLabel({
+        linkLabel,
+        anchorRadius,
+        currentY,
+        rowPitch,
+        yRelativeIncrement,
+        rawTextGetter,
+        maxTextLength,
+        valueFormatter,
+        valueGetter,
+        measure,
+        rectWidth,
+        rectHeight,
+        diskCenter,
+      }),
+    )
     .filter(({ text }) => text !== ''); // cull linked labels whose text was truncated to nothing;
   return { linkLabels, valueFontSpec, labelFontSpec, strokeColor };
 }
@@ -180,5 +122,109 @@ function fitText(measure: TextMeasure, desiredText: string, allottedWidth: numbe
     width,
     verticalOffset: -(emHeightDescent + emHeightAscent) / 2, // meaning, `middle`
     text,
+  };
+}
+
+function linkLabelCompare(n1: ShapeTreeNode, n2: ShapeTreeNode) {
+  const mid1 = meanAngle(n1.x0, n1.x1);
+  const mid2 = meanAngle(n2.x0, n2.x1);
+  const dist1 = Math.min(Math.abs(mid1 - TAU / 4), Math.abs(mid1 - (3 * TAU) / 4));
+  const dist2 = Math.min(Math.abs(mid2 - TAU / 4), Math.abs(mid2 - (3 * TAU) / 4));
+  return dist1 - dist2;
+}
+
+function nodeToLinkLabel({
+  linkLabel,
+  anchorRadius,
+  currentY,
+  rowPitch,
+  yRelativeIncrement,
+  rawTextGetter,
+  maxTextLength,
+  valueFormatter,
+  valueGetter,
+  measure,
+  rectWidth,
+  rectHeight,
+  diskCenter,
+}: {
+  linkLabel: LinkLabelConfig;
+  anchorRadius: Distance;
+  currentY: Distance[];
+  rowPitch: Pixels;
+  yRelativeIncrement: Distance;
+  rawTextGetter: RawTextGetter;
+  maxTextLength: number;
+  valueFormatter: ValueFormatter;
+  valueGetter: ValueGetterFunction;
+  measure: TextMeasure;
+  rectWidth: Distance;
+  rectHeight: Distance;
+  diskCenter: Point;
+}) {
+  return function nodeToLinkLabelMap(node: ShapeTreeNode): LinkLabelVM {
+    const midAngle = trueBearingToStandardPositionAngle((node.x0 + node.x1) / 2);
+    const north = midAngle < TAU / 2 ? 1 : -1;
+    const rightSide = TAU / 4 < midAngle && midAngle < (3 * TAU) / 4 ? 0 : 1;
+    const west = rightSide ? 1 : -1;
+    const cos = Math.cos(midAngle);
+    const sin = Math.sin(midAngle);
+    const x0 = cos * anchorRadius;
+    const y0 = sin * anchorRadius;
+    const x = cos * (anchorRadius + linkLabel.radiusPadding);
+    const y = sin * (anchorRadius + linkLabel.radiusPadding);
+    const poolIndex = rightSide + (1 - north);
+    const relativeY = north * y;
+    currentY[poolIndex] = Math.max(currentY[poolIndex] + rowPitch, relativeY + yRelativeIncrement, rowPitch / 2);
+    const cy = north * currentY[poolIndex];
+    const stemFromX = x;
+    const stemFromY = y;
+    const stemToX = x + north * west * cy - west * relativeY;
+    const stemToY = cy;
+    const rawText = rawTextGetter(node);
+    const labelText = cutToLength(rawText, maxTextLength);
+    const valueText = valueFormatter(valueGetter(node));
+
+    const labelFontSpec: Font = {
+      ...linkLabel,
+    };
+    const valueFontSpec: Font = {
+      ...linkLabel,
+      ...linkLabel.valueFont,
+    };
+    const translateX = stemToX + west * (linkLabel.horizontalStemLength + linkLabel.gap);
+    const { width: valueWidth } = measure(linkLabel.fontSize, [{ ...valueFontSpec, text: valueText }])[0];
+    const widthAdjustment = valueWidth + 2 * linkLabel.fontSize; // gap between label and value, plus possibly 2em wide ellipsis
+    const allottedLabelWidth = Math.max(
+      0,
+      rightSide ? rectWidth - diskCenter.x - translateX - widthAdjustment : diskCenter.x + translateX - widthAdjustment,
+    );
+    const { text, width, verticalOffset } =
+      linkLabel.fontSize / 2 <= cy + diskCenter.y && cy + diskCenter.y <= rectHeight - linkLabel.fontSize / 2
+        ? fitText(measure, labelText, allottedLabelWidth, linkLabel.fontSize, {
+            ...labelFontSpec,
+            text: labelText,
+          })
+        : { text: '', width: 0, verticalOffset: 0 };
+    const linkLabels: PointTuples = [
+      [x0, y0],
+      [stemFromX, stemFromY],
+      [stemToX, stemToY],
+      [stemToX + west * linkLabel.horizontalStemLength, stemToY],
+    ];
+    const translate: PointTuple = [translateX, stemToY];
+    const textAlign: TextAlign = rightSide ? 'left' : 'right';
+    return {
+      linkLabels,
+      translate,
+      textAlign,
+      text,
+      valueText,
+      width,
+      valueWidth,
+      verticalOffset,
+      labelFontSpec,
+      valueFontSpec,
+    };
   };
 }
