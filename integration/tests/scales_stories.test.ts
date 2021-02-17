1054511730705acf815c7b6888eb11be1c704ac1
nickofthyme <nick.ryan.partridge@gmail.com>
2021-02-17 16:46:11 -0600

    test: update vrt for scales and areas based on changes

--- integration/tests/scales_stories.test.ts
+++ integration/tests/scales_stories.test.ts
@@ -21,9 +21,33 @@ import { LogBase } from '../../src/scales/scale_continuous';
 import { common } from '../page_objects';
 
 describe('Scales stories', () => {
-  it.each(Object.values(LogBase))('should render proper tick count', async (base) => {
-    await common.expectChartAtUrlToMatchScreenshot(
-      `http://localhost:9001/?path=/story/scales--log-scale-options&knob-Log base_Y - Axis=${base}`,
-    );
+  describe.each`
+    polarity      | value
+    ${'Negative'} | ${true}
+    ${'Positive'} | ${false}
+  `('$polarity values', ({ value: negative }) => {
+    it.each(Object.values(LogBase))('should render proper ticks with %s base', async (base) => {
+      await common.expectChartAtUrlToMatchScreenshot(
+        `http://localhost:9001/?path=/story/scales--log-scale-options&knob-Use negative values_Y - Axis=${negative}&knob-Log base_Y - Axis=${base}&knob-Fit domain_Y - Axis=true&knob-Use default limit_Y - Axis=true`,
+      );
+    });
+
+    it('should render with baseline set to 1 if fit is false', async () => {
+      await common.expectChartAtUrlToMatchScreenshot(
+        `http://localhost:9001/?path=/story/scales--log-scale-options&knob-Use negative values_Y - Axis=${negative}&knob-Log base_Y - Axis=common&knob-Fit domain_Y - Axis=false&knob-Use default limit_Y - Axis=true`,
+      );
+    });
+
+    it('should render with baseline set to 1 if fit is false and limit is set', async () => {
+      await common.expectChartAtUrlToMatchScreenshot(
+        `http://localhost:9001/?path=/story/scales--log-scale-options&knob-Use negative values_Y - Axis=${negative}&knob-Log base_Y - Axis=common&knob-Fit domain_Y - Axis=false&knob-Use default limit_Y - Axis=true&knob-Log min limit_Y - Axis=0.01`,
+      );
+    });
+
+    it('should render values with log limit', async () => {
+      await common.expectChartAtUrlToMatchScreenshot(
+        `http://localhost:9001/?path=/story/scales--log-scale-options&knob-Use negative values_Y - Axis=${negative}&knob-Log base_Y - Axis=common&knob-Fit domain_Y - Axis=true&knob-Log min limit_Y - Axis=0.01&knob-Use default limit_Y - Axis=false`,
+      );
+    });
   });
 });
