044ed905a8c51ed6ab7b9d82f766eff51f77d0b8
Robert Monfera <monfera.robert@gmail.com>
2021-02-18 15:31:35 +0100

    test: add drilldown

--- stories/icicle/03_cpu_profile_flame.tsx
+++ stories/icicle/03_cpu_profile_flame.tsx
@@ -50,6 +50,7 @@ export const Example = () => {
         config={{
           ...config,
           partitionLayout: PartitionLayout.icicle,
+          drilldown: true,
         }}
       />
     </Chart>
