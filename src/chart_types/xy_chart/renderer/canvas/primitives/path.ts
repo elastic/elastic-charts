58f3a7363e28f2748f92c6ff74e3761f9ae0592b
Debajyoti Halder <rondebajyoti@gmail.com>
2021-04-18 19:03:42 +0530

    feat: texture: Add svg image to chart_types

--- src/chart_types/xy_chart/renderer/canvas/primitives/path.ts
+++ src/chart_types/xy_chart/renderer/canvas/primitives/path.ts
@@ -24,6 +24,29 @@ import { ClippedRanges } from '../../../../../utils/geometry';
 import { Point } from '../../../../../utils/point';
 import { renderMultiLine } from './line';
 
+// function drawCheckeredBackground(can: HTMLCanvasElement, nRow: number, nCol: number, fill: Fill) {
+//   const ctx = can.getContext('2d');
+//   if (ctx != null) {
+//     let w = can.width;
+//     let h = can.height;
+
+//     nRow = nRow || 8; // default number of rows
+//     nCol = nCol || 8; // default number of columns
+
+//     w /= nCol; // width of a block
+//     h /= nRow; // height of a block
+
+//     for (let i = 0; i < nRow; ++i) {
+//       for (let j = 0, col = nCol / 2; j < col; ++j) {
+//         ctx.rect(2 * j * w + (i % 2 ? 0 : w), i * h, w, h);
+//       }
+//     }
+
+//     ctx.fillStyle = RGBtoString(fill.color);
+//     ctx.fill();
+//   }
+// }
+
 /** @internal */
 export function renderLinePaths(
   context: CanvasRenderingContext2D,
@@ -65,33 +88,53 @@ export function renderAreaPath(
   clippings: Rect,
   hideClippedRanges = false,
 ) {
-  if (clippedRanges.length > 0) {
-    withClipRanges(ctx, clippedRanges, clippings, false, (ctx) => {
-      ctx.translate(transform.x, transform.y);
-      renderPathFill(ctx, area, fill);
-    });
-    if (hideClippedRanges) {
-      return;
+  const imgCanvas = document.createElement('canvas');
+  imgCanvas.width = 100;
+  imgCanvas.height = 100;
+  const imgCtx = imgCanvas.getContext('2d');
+  const img = new Image();
+  img.src = require('../images/pattern.svg');
+
+  img.addEventListener('load', function () {
+    if (imgCtx != null) {
+      imgCtx.drawImage(img, 0, 0, 200, 200);
+      if (clippedRanges.length > 0) {
+        withClipRanges(ctx, clippedRanges, clippings, false, (ctx) => {
+          ctx.translate(transform.x, transform.y);
+          renderPathFill(ctx, imgCanvas, area, fill);
+        });
+        if (hideClippedRanges) {
+          return;
+        }
+        withClipRanges(ctx, clippedRanges, clippings, true, (ctx) => {
+          ctx.translate(transform.x, transform.y);
+          const { opacity } = fill.color;
+          const color = {
+            ...fill.color,
+            opacity: opacity / 2,
+          };
+          renderPathFill(ctx, imgCanvas, area, { ...fill, color });
+        });
+        return;
+      }
+      withContext(ctx, (ctx) => {
+        ctx.translate(transform.x, transform.y);
+        renderPathFill(ctx, imgCanvas, area, fill);
+      });
     }
-    withClipRanges(ctx, clippedRanges, clippings, true, (ctx) => {
-      ctx.translate(transform.x, transform.y);
-      const { opacity } = fill.color;
-      const color = {
-        ...fill.color,
-        opacity: opacity / 2,
-      };
-      renderPathFill(ctx, area, { ...fill, color });
-    });
-    return;
-  }
-  withContext(ctx, (ctx) => {
-    ctx.translate(transform.x, transform.y);
-    renderPathFill(ctx, area, fill);
   });
+  // drawCheckeredBackground(imgCanvas, 10, 10, fill);
 }
 
-function renderPathFill(ctx: CanvasRenderingContext2D, path: string, fill: Fill) {
+function renderPathFill(ctx: CanvasRenderingContext2D, imgCanvas: HTMLCanvasElement, path: string, fill: Fill) {
   const path2d = new Path2D(path);
+  const pattern = ctx.createPattern(imgCanvas, 'repeat');
+  if (pattern) {
+    ctx.fillStyle = pattern;
+    ctx.beginPath();
+    ctx.fill(path2d);
+    return;
+  }
   ctx.fillStyle = RGBtoString(fill.color);
   ctx.beginPath();
   ctx.fill(path2d);
