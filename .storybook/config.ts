0f9fdceb21e82dea014c7d3b7e4ab4802df047df
nickofthyme <nick.ryan.partridge@gmail.com>
2021-04-21 11:44:54 -0500

    ci: fix icon loading for vrt

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -22,10 +22,12 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { addDecorator, configure, addParameters } from '@storybook/react';
 import { create } from '@storybook/theming';
 
+import { preloadIcons } from './preload_icons';
 import { switchTheme } from './theme_service';
 import './style.scss';
 
 switchTheme('light');
+preloadIcons();
 
 addParameters({
   options: {
