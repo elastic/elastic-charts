3c04c2c3eecea13fc444270ec0a238db6a4dfecb
Marco Vettorello <vettorello.marco@gmail.com>
2021-04-28 22:38:07 +0200

    ci: merged vrts

--- .ci/vrts.sh
+++ .ci/vrts.sh
@@ -5,10 +5,9 @@
 ###
 source .ci/global_setup.sh
 
-VRTS_FILES=$1
 ###
 ### visual testing
 ###
 echo " -- visual testing"
 yarn test:integration:generate
-yarn test:integration --ci $VRTS_FILES
+yarn test:integration --ci
