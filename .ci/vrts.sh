3108b33a897a2e6db75ca20a6601aa9b1d1ec73f
Marco Vettorello <vettorello.marco@gmail.com>
2021-04-28 23:23:53 +0200

    Revert "ci: merged vrts"
    This reverts commit 3c04c2c3eecea13fc444270ec0a238db6a4dfecb.

--- .ci/vrts.sh
+++ .ci/vrts.sh
@@ -5,9 +5,10 @@
 ###
 source .ci/global_setup.sh
 
+VRTS_FILES=$1
 ###
 ### visual testing
 ###
 echo " -- visual testing"
 yarn test:integration:generate
-yarn test:integration --ci
+yarn test:integration --ci $VRTS_FILES
