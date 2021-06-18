9884efdd7cb3db124cb364af6b70fbda472e1247
Robert Monfera <monfera.robert@gmail.com>
2021-06-18 22:19:29 +0200

    refactor: renderAxis simplification with fewer branching

--- packages/charts/src/chart_types/xy_chart/renderer/canvas/axes/index.ts
+++ packages/charts/src/chart_types/xy_chart/renderer/canvas/axes/index.ts
@@ -25,7 +25,6 @@ import { Point } from '../../../../../utils/point';
 import { AxisStyle } from '../../../../../utils/themes/theme';
 import { PerPanelAxisGeoms } from '../../../state/selectors/compute_per_panel_axes_geoms';
 import { getSpecsById } from '../../../state/utils/spec';
-import { isHorizontalAxis } from '../../../utils/axis_type_utils';
 import { AxisTick, AxisViewModel, shouldShowTicks } from '../../../utils/axis_utils';
 import { AxisSpec } from '../../../utils/specs';
 import { renderDebugRect } from '../utils/debug';
@@ -109,13 +108,8 @@ function renderAxis(ctx: CanvasRenderingContext2D, props: AxisProps) {
   const { ticks, size, anchorPoint, debug, axisStyle, axisSpec, panelAnchor, secondary } = props;
   const showTicks = shouldShowTicks(axisStyle.tickLine, axisSpec.hide);
   const { position } = axisSpec;
-  const isHorizontal = isHorizontalAxis(position);
-  const x = isHorizontal
-    ? anchorPoint.x + panelAnchor.x
-    : anchorPoint.x + (position === Position.Right ? -1 : 1) * panelAnchor.x;
-  const y = isHorizontal
-    ? anchorPoint.y + (position === Position.Top ? 1 : -1) * panelAnchor.y
-    : anchorPoint.y + panelAnchor.y;
+  const x = anchorPoint.x + (position === Position.Right ? -1 : 1) * panelAnchor.x;
+  const y = anchorPoint.y + (position === Position.Bottom ? -1 : 1) * panelAnchor.y;
 
   withContext(ctx, (ctx) => {
     ctx.translate(x, y);
@@ -127,8 +121,7 @@ function renderAxis(ctx: CanvasRenderingContext2D, props: AxisProps) {
     renderLine(ctx, props); // render the axis line
 
     // TODO: compute axis dimensions per panels
-    // For now just rendering axis line
-    if (secondary) return;
+    if (secondary) return; // For now, just render the axis line
 
     if (showTicks) {
       ticks.forEach((tick) => renderTick(ctx, tick, props));
