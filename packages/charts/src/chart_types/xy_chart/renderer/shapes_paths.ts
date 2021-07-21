8637423b929d3ee55f2e7d3ba8b556f4a605d45a
nickofthyme <nick.ryan.partridge@gmail.com>
2021-07-21 13:25:25 -0500

    fix: square/diamond shape size based on rotation

--- packages/charts/src/chart_types/xy_chart/renderer/shapes_paths.ts
+++ packages/charts/src/chart_types/xy_chart/renderer/shapes_paths.ts
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 
+import { getRadians } from '../../../utils/common';
 import { PointShape, TextureShape } from '../../../utils/themes/theme';
 
 /** @internal */
@@ -14,30 +15,32 @@ export type SVGPath = string;
 /** @internal */
 export type SVGPathFn = (radius: number) => SVGPath;
 
-/** @internal */
-export const cross: SVGPathFn = (r: number) => {
+const cross: SVGPathFn = (r: number) => {
   return `M ${-r} 0 L ${r} 0 M 0 ${r} L 0 ${-r}`;
 };
 
-/** @internal */
-export const triangle: SVGPathFn = (r: number) => {
+const triangle: SVGPathFn = (r: number) => {
   const h = (r * Math.sqrt(3)) / 2;
   const hr = r / 2;
   return `M ${-h} ${hr} L ${h} ${hr} L 0 ${-r} Z`;
 };
 
-/** @internal */
-export const square: SVGPathFn = (r: number) => {
-  return `M ${-r} ${-r} L ${-r} ${r} L ${r} ${r} L ${r} ${-r} Z`;
+/**
+ * Returns shape function based on rotation of square in degrees
+ */
+const square = (rotation = 0): SVGPathFn => (r: number) => {
+  const d = getRadians(rotation);
+  const s = Math.cos(d) + Math.sin(d);
+  // scaled r to account for rotation;
+  const sr = s > 0 ? r / s : r;
+  return `M ${-sr} ${-sr} L ${-sr} ${sr} L ${sr} ${sr} L ${sr} ${-sr} Z`;
 };
 
-/** @internal */
-export const circle: SVGPathFn = (r: number) => {
+const circle: SVGPathFn = (r: number) => {
   return `M ${-r} 0 a ${r},${r} 0 1,0 ${r * 2},0 a ${r},${r} 0 1,0 ${-r * 2},0`;
 };
 
-/** @internal */
-export const line: SVGPathFn = (r: number) => {
+const line: SVGPathFn = (r: number) => {
   return `M 0 ${-r} l 0 ${r * 2}`;
 };
 
@@ -46,8 +49,8 @@ export const ShapeRendererFn: Record<PointShape, [SVGPathFn, number]> = {
   [PointShape.Circle]: [circle, 0],
   [PointShape.X]: [cross, 45],
   [PointShape.Plus]: [cross, 0],
-  [PointShape.Diamond]: [square, 45],
-  [PointShape.Square]: [square, 0],
+  [PointShape.Diamond]: [square(45), 45],
+  [PointShape.Square]: [square(0), 0],
   [PointShape.Triangle]: [triangle, 0],
 };
 
