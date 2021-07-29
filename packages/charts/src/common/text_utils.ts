2bfffa692f36871c32ba190e22a95facd30367f8
Robert Monfera <monfera.robert@gmail.com>
2021-07-29 13:27:05 +0200

    chore: pure line reshuffling

--- packages/charts/src/common/text_utils.ts
+++ packages/charts/src/common/text_utils.ts
@@ -17,6 +17,14 @@ const FONT_WEIGHTS_NUMERIC = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 const FONT_WEIGHTS_ALPHA = ['normal', 'bold', 'lighter', 'bolder', 'inherit', 'initial', 'unset'];
 
 /** @public */
+export type TextContrast = boolean | number;
+/**
+ * todo consider doing tighter control for permissible font families, eg. as in Kibana Canvas - expression language
+ *  - though the same applies for permissible (eg. known available or loaded) font weights, styles, variants...
+ * @public
+ */
+export type FontFamily = string;
+/** @public */
 export const FONT_WEIGHTS = Object.freeze([...FONT_WEIGHTS_NUMERIC, ...FONT_WEIGHTS_ALPHA] as const);
 /** @public */
 export const FONT_VARIANTS = Object.freeze(['normal', 'small-caps'] as const);
@@ -30,6 +38,21 @@ export type NumericFontWeight = number & typeof FONT_WEIGHTS[number];
 export const FONT_STYLES = Object.freeze(['normal', 'italic', 'oblique', 'inherit', 'initial', 'unset'] as const);
 /** @public */
 export type FontStyle = typeof FONT_STYLES[number];
+/** @public */
+export type PartialFont = Partial<Font>;
+/** @public */
+export const TEXT_ALIGNS = Object.freeze(['start', 'end', 'left', 'right', 'center'] as const);
+/** @public */
+export type TextAlign = typeof TEXT_ALIGNS[number];
+/** @public */
+export type TextBaseline = typeof TEXT_BASELINE[number];
+
+/** @internal */
+export type VerticalAlignments = Values<typeof VerticalAlignments>;
+/** @internal */
+export type Relation = Array<Datum>;
+/** @internal */
+export type TextMeasure = (fontSize: number, boxes: Box[]) => TextMetrics[];
 
 /**
  * this doesn't include the font size, so it's more like a font face (?) - unfortunately all vague terms
@@ -45,12 +68,6 @@ export interface Font {
 }
 
 /** @public */
-export type PartialFont = Partial<Font>;
-/** @public */
-export const TEXT_ALIGNS = Object.freeze(['start', 'end', 'left', 'right', 'center'] as const);
-/** @public */
-export type TextAlign = typeof TEXT_ALIGNS[number];
-/** @public */
 export const TEXT_BASELINE = Object.freeze([
   'top',
   'hanging',
@@ -59,28 +76,16 @@ export const TEXT_BASELINE = Object.freeze([
   'ideographic',
   'bottom',
 ] as const);
-/** @public */
-export type TextBaseline = typeof TEXT_BASELINE[number];
 
-/**
- * @internal
- */
+/** @internal */
 export interface Box extends Font {
   text: string;
 }
 
 /** @internal */
-export type Relation = Array<Datum>;
-
-/** @internal */
 export interface Part extends Rectangle {
   node: ArrayEntry;
 }
-
-/**
- * @internal
- */
-export type TextMeasure = (fontSize: number, boxes: Box[]) => TextMetrics[];
 
 /** @internal */
 export function cssFontShorthand({ fontStyle, fontVariant, fontWeight, fontFamily }: Font, fontSize: Pixels) {
@@ -96,18 +101,6 @@ export function measureText(ctx: CanvasRenderingContext2D): TextMeasure {
     });
 }
 
-/**
- * todo consider doing tighter control for permissible font families, eg. as in Kibana Canvas - expression language
- *  - though the same applies for permissible (eg. known available or loaded) font weights, styles, variants...
- * @public
- */
-export type FontFamily = string;
-
-/**
- * @public
- */
-export type TextContrast = boolean | number;
-
 /** @internal */
 export const VerticalAlignments = Object.freeze({
   top: 'top' as const,
@@ -117,9 +110,6 @@ export const VerticalAlignments = Object.freeze({
   hanging: 'hanging' as const,
   ideographic: 'ideographic' as const,
 });
-
-/** @internal */
-export type VerticalAlignments = Values<typeof VerticalAlignments>;
 
 /** @internal */
 export function measureOneBoxWidth(measure: TextMeasure, fontSize: number, box: Box) {
