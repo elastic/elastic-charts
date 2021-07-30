b4391821045c95ff2cb2ef6a33b925034eeba98d
Marco Vettorello <vettorello.marco@gmail.com>
2021-07-30 10:58:32 +0200

    fix: heatmap snap domain to interval (#1253)
    Heatmaps with a time scale on the X-axis now adjust the rendered time range to fully cover the edges when a custom domain is used. We also took this opportunity to clean and abstract the code used for computing and handling date and time. The library is now able to abstract from the underlying implementation library (moment or luxon at the moment), allowing us to experiment and work with diverse libraries removing some tech debt.
    
    fix #1165

---
+++ packages/charts/src/utils/chrono/elasticsearch.test.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { DateTime } from 'luxon';
+
+import { snapDateToESInterval } from './elasticsearch';
+
+describe('snap to interval', () => {
+  it('should snap to begin of calendar interval', () => {
+    const initialDate = DateTime.fromISO('2020-01-03T07:00:01Z');
+    const snappedDate = snapDateToESInterval(
+      initialDate.toMillis(),
+      { type: 'calendar', unit: 'd', quantity: 1 },
+      'start',
+      'UTC',
+    );
+    expect(DateTime.fromMillis(snappedDate, { zone: 'utc' }).toISO()).toBe('2020-01-03T00:00:00.000Z');
+  });
+
+  it('should snap to end of calendar interval', () => {
+    const initialDate = DateTime.fromISO('2020-01-03T07:00:01Z');
+    const snappedDate = snapDateToESInterval(
+      initialDate.toMillis(),
+      { type: 'calendar', unit: 'd', quantity: 1 },
+      'end',
+      'UTC',
+    );
+    expect(DateTime.fromMillis(snappedDate, { zone: 'utc' }).toISO()).toBe('2020-01-03T23:59:59.999Z');
+  });
+
+  it('should snap to begin of fixed interval', () => {
+    const initialDate = DateTime.fromISO('2020-01-03T07:00:01Z');
+    const snappedDate = snapDateToESInterval(
+      initialDate.toMillis(),
+      { type: 'fixed', unit: 'm', quantity: 30 },
+      'start',
+      'UTC',
+    );
+    expect(DateTime.fromMillis(snappedDate, { zone: 'utc' }).toISO()).toBe('2020-01-03T07:00:00.000Z');
+  });
+
+  it('should snap to end of fixed interval', () => {
+    const initialDate = DateTime.fromISO('2020-01-03T07:00:01Z');
+    const snappedDate = snapDateToESInterval(
+      initialDate.toMillis(),
+      { type: 'fixed', unit: 'm', quantity: 30 },
+      'end',
+      'UTC',
+    );
+    expect(DateTime.fromMillis(snappedDate, { zone: 'utc' }).toISO()).toBe('2020-01-03T07:29:59.999Z');
+  });
+});
