48d927976de1ab3432dcddbc71bc32d287f22130
Robert Monfera <monfera.robert@gmail.com>
2021-08-06 22:44:18 +0200

    refactor: we never not call with fill and stroke

--- packages/charts/src/chart_types/xy_chart/renderer/canvas/primitives/rect.ts
+++ packages/charts/src/chart_types/xy_chart/renderer/canvas/primitives/rect.ts
@@ -16,44 +16,42 @@ import { MIN_STROKE_WIDTH } from './line';
 export function renderRect(
   ctx: CanvasRenderingContext2D,
   rect: Rect,
-  fill?: Fill,
-  stroke?: Stroke,
+  fill: Fill,
+  stroke: Stroke,
   disableBorderOffset: boolean = false,
 ) {
-  if (fill) {
-    const borderOffset = disableBorderOffset || !stroke || stroke.width < MIN_STROKE_WIDTH ? 0 : stroke.width;
-    const x = rect.x + borderOffset;
-    const y = rect.y + borderOffset;
-    const width = rect.width - borderOffset * 2;
-    const height = rect.height - borderOffset * 2;
+  const borderOffset = disableBorderOffset || stroke.width < MIN_STROKE_WIDTH ? 0 : stroke.width;
+  const x = rect.x + borderOffset;
+  const y = rect.y + borderOffset;
+  const width = rect.width - borderOffset * 2;
+  const height = rect.height - borderOffset * 2;
 
-    ctx.beginPath();
-    ctx.rect(x, y, width, height);
-    ctx.fillStyle = RGBtoString(fill.color);
-    ctx.fill();
+  ctx.beginPath();
+  ctx.rect(x, y, width, height);
+  ctx.fillStyle = RGBtoString(fill.color);
+  ctx.fill();
 
-    if (fill.texture) {
-      const { texture } = fill;
-      const { offset } = texture;
-      const rotation = degToRad(texture.rotation ?? 0);
-      withContext(ctx, (ctx) => {
-        ctx.clip();
+  if (fill.texture) {
+    const { texture } = fill;
+    const { offset } = texture;
+    const rotation = degToRad(texture.rotation ?? 0);
+    withContext(ctx, (ctx) => {
+      ctx.clip();
 
-        if (offset?.global) ctx.translate(offset.x ?? 0, offset.y ?? 0);
-        if (rotation) ctx.rotate(rotation);
-        if (offset && !offset.global) ctx.translate(offset.x ?? 0, offset.y ?? 0);
+      if (offset?.global) ctx.translate(offset.x ?? 0, offset.y ?? 0);
+      if (rotation) ctx.rotate(rotation);
+      if (offset && !offset.global) ctx.translate(offset.x ?? 0, offset.y ?? 0);
 
-        ctx.fillStyle = texture.pattern;
+      ctx.fillStyle = texture.pattern;
 
-        // Use oversized rect to fill rotation/offset beyond path
-        const rotationRectFillSize = ctx.canvas.clientWidth * ctx.canvas.clientHeight;
-        ctx.translate(-rotationRectFillSize / 2, -rotationRectFillSize / 2);
-        ctx.fillRect(0, 0, rotationRectFillSize, rotationRectFillSize);
-      });
-    }
+      // Use oversized rect to fill rotation/offset beyond path
+      const rotationRectFillSize = ctx.canvas.clientWidth * ctx.canvas.clientHeight;
+      ctx.translate(-rotationRectFillSize / 2, -rotationRectFillSize / 2);
+      ctx.fillRect(0, 0, rotationRectFillSize, rotationRectFillSize);
+    });
   }
 
-  if (stroke && stroke.width >= MIN_STROKE_WIDTH) {
+  if (stroke.width >= MIN_STROKE_WIDTH) {
     const borderOffset = disableBorderOffset ? 0 : stroke.width / 2;
     const x = rect.x + borderOffset;
     const y = rect.y + borderOffset;
