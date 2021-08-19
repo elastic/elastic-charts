ee513dc77347699feb47e427c7ee069feeb0c0f8
Robert Monfera <monfera.robert@gmail.com>
2021-08-19 16:13:07 +0200

    refactor: simplified arc.ts

--- packages/charts/src/chart_types/xy_chart/renderer/canvas/primitives/arc.ts
+++ packages/charts/src/chart_types/xy_chart/renderer/canvas/primitives/arc.ts
@@ -6,24 +6,18 @@
  * Side Public License, v 1.
  */
 
-import { Circle, Stroke, Fill, Arc } from '../../../../../geoms/types';
+import { Circle, Fill, Stroke } from '../../../../../geoms/types';
 import { withContext } from '../../../../../renderers/canvas';
 import { fillAndStroke } from './utils';
 
 /** @internal */
-export function renderCircle(ctx: CanvasRenderingContext2D, circle: Circle, fill?: Fill, stroke?: Stroke) {
+export function renderCircle(ctx: CanvasRenderingContext2D, { x, y, radius }: Circle, fill?: Fill, stroke?: Stroke) {
   if (fill || stroke) {
-    renderArc(ctx, { ...circle, startAngle: 0, endAngle: Math.PI * 2 }, fill, stroke);
+    withContext(ctx, () => {
+      ctx.translate(x, y);
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, 2 * Math.PI, false);
+      fillAndStroke(ctx, fill, stroke);
+    });
   }
-}
-
-/** @internal */
-export function renderArc(ctx: CanvasRenderingContext2D, arc: Arc, fill?: Fill, stroke?: Stroke) {
-  withContext(ctx, () => {
-    const { x, y, radius, startAngle, endAngle } = arc;
-    ctx.translate(x, y);
-    ctx.beginPath();
-    ctx.arc(0, 0, radius, startAngle, endAngle, false);
-    fillAndStroke(ctx, fill, stroke);
-  });
 }
