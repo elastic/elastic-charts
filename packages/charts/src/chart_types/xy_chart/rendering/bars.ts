d228c6196c818bb7ae75b267cdb07ac51a3ad435
Robert Monfera <monfera.robert@gmail.com>
2021-08-24 16:23:36 +0200

    refactor: super minor extractions

--- packages/charts/src/chart_types/xy_chart/rendering/bars.ts
+++ packages/charts/src/chart_types/xy_chart/rendering/bars.ts
@@ -38,149 +38,146 @@ export function renderBars(
   chartRotation?: number,
 ): BarTuple {
   const padding = 1; // default padding for now
+  const fontSizeFactor = 0.7; // Take 70% of space for the label text
   const { fontSize, fontFamily } = sharedSeriesStyle.displayValue;
+  const initialBarTuple: BarTuple = { barGeometries: [], indexedGeometryMap: new IndexedGeometryMap() } as BarTuple;
   return withTextMeasure((textMeasure) =>
-    dataSeries.data.reduce(
-      (barTuple: BarTuple, datum) => {
-        const { barGeometries, indexedGeometryMap } = barTuple;
-        const { y0, y1, initialY1, filled } = datum;
-        // don't create a bar if not within the xScale domain
-        if (!xScale.isValueInDomain(datum.x)) {
-          return barTuple;
-        }
-
-        let y: number | null;
-        let y0Scaled;
-        if (yScale.type === ScaleType.Log) {
-          y = y1 === 0 || y1 === null ? yScale.range[0] : yScale.scale(y1);
-          if (yScale.isInverted) {
-            y0Scaled = y0 === 0 || y0 === null ? yScale.range[1] : yScale.scale(y0);
-          } else {
-            y0Scaled = y0 === 0 || y0 === null ? yScale.range[0] : yScale.scale(y0);
-          }
-        } else {
-          y = yScale.scale(y1);
-          // use always zero as baseline if y0 is null
-          y0Scaled = y0 === null ? yScale.scale(0) : yScale.scale(y0);
-        }
-
-        const absMinHeight = Math.abs(minBarHeight);
-
-        // safeguard against null y values
-        let height = isNil(y0Scaled) || isNil(y) ? 0 : y0Scaled - y;
-
-        if (isNil(y0Scaled) || isNil(y)) {
-          y = 0;
-        }
-
-        if (absMinHeight !== undefined && height !== 0 && Math.abs(height) < absMinHeight) {
-          const heightDelta = absMinHeight - Math.abs(height);
-          if (height < 0) {
-            height = -absMinHeight;
-            y += heightDelta;
-          } else {
-            height = absMinHeight;
-            y -= heightDelta;
-          }
-        }
-        const isUpsideDown = height < 0;
-        height = Math.abs(height);
-        y = isUpsideDown ? y - height : y;
-
-        const xScaled = xScale.scale(datum.x);
-
-        if (xScaled === null) {
-          return barTuple;
-        }
-
-        const seriesIdentifier: XYChartSeriesIdentifier = {
-          key: dataSeries.key,
-          specId: dataSeries.specId,
-          yAccessor: dataSeries.yAccessor,
-          splitAccessors: dataSeries.splitAccessors,
-          seriesKeys: dataSeries.seriesKeys,
-          smHorizontalAccessorValue: dataSeries.smHorizontalAccessorValue,
-          smVerticalAccessorValue: dataSeries.smVerticalAccessorValue,
-        };
-
-        const seriesStyle = getBarStyleOverrides(datum, seriesIdentifier, sharedSeriesStyle, styleAccessor);
-
-        const maxPixelWidth = clamp(seriesStyle.rect.widthRatio ?? 1, 0, 1) * xScale.bandwidth;
-        const minPixelWidth = clamp(seriesStyle.rect.widthPixel ?? 0, 0, maxPixelWidth);
-
-        const width = clamp(seriesStyle.rect.widthPixel ?? xScale.bandwidth, minPixelWidth, maxPixelWidth);
-        const x = xScaled + xScale.bandwidth * orderIndex + xScale.bandwidth / 2 - width / 2;
-
-        const originalY1Value = stackMode === StackMode.Percentage ? (isNil(y1) ? null : y1 - (y0 ?? 0)) : initialY1;
-        const formattedDisplayValue = displayValueSettings?.valueFormatter?.(originalY1Value);
-
-        // only show displayValue for even bars if showOverlappingValue
-        const displayValueText =
-          displayValueSettings?.isAlternatingValueLabel && barGeometries.length % 2 ? undefined : formattedDisplayValue;
-
-        const { displayValueWidth, fixedFontScale } = computeBoxWidth(
-          displayValueText ?? '',
-          { padding, fontSize, fontFamily, textMeasure, width },
-          displayValueSettings,
-        );
-
-        const isHorizontalRotation = chartRotation == null || [0, 180].includes(chartRotation);
-        // Take 70% of space for the label text
-        const fontSizeFactor = 0.7;
-        // Pick the right side of the label's box to use as factor reference
-        const referenceWidth = Math.max(isHorizontalRotation ? displayValueWidth : fixedFontScale, 1);
-
-        const textScalingFactor = getFinalFontScalingFactor(
-          (width * fontSizeFactor) / referenceWidth,
-          fixedFontScale,
-          fontSize,
-        );
-        const overflowConstraints: Set<LabelOverflowConstraint> = new Set(
-          displayValueSettings?.overflowConstraints ?? [
-            LabelOverflowConstraint.ChartEdges,
-            LabelOverflowConstraint.BarGeometry,
-          ],
-        );
-
-        // Based on rotation scale the width of the text box
-        const bboxWidthFactor = isHorizontalRotation ? textScalingFactor : 1;
-
-        const displayValue: BarGeometry['displayValue'] | undefined =
-          displayValueText && displayValueSettings?.showValueLabel
-            ? {
-                fontScale: textScalingFactor,
-                fontSize: fixedFontScale,
-                text: displayValueText,
-                width: bboxWidthFactor * displayValueWidth,
-                height: textScalingFactor * fixedFontScale,
-                overflowConstraints,
-                isValueContainedInElement: displayValueSettings?.isValueContainedInElement ?? false,
-              }
-            : undefined;
-
-        const barGeometry: BarGeometry = {
-          displayValue,
-          x,
-          y,
-          transform: { x: 0, y: 0 },
-          width,
-          height,
-          color,
-          value: { x: datum.x, y: originalY1Value, mark: null, accessor: BandedAccessorType.Y1, datum: datum.datum },
-          seriesIdentifier,
-          seriesStyle,
-          panel,
-        };
-        indexedGeometryMap.set(barGeometry);
-
-        if (y1 !== null && initialY1 !== null && filled?.y1 === undefined) {
-          barGeometries.push(barGeometry);
-        }
-
+    dataSeries.data.reduce((barTuple: BarTuple, datum) => {
+      const { barGeometries, indexedGeometryMap } = barTuple;
+      const { y0, y1, initialY1, filled } = datum;
+      // don't create a bar if not within the xScale domain
+      if (!xScale.isValueInDomain(datum.x)) {
         return barTuple;
-      },
-      { barGeometries: [], indexedGeometryMap: new IndexedGeometryMap() },
-    ),
+      }
+
+      let y: number | null;
+      let y0Scaled;
+      if (yScale.type === ScaleType.Log) {
+        y = y1 === 0 || y1 === null ? yScale.range[0] : yScale.scale(y1);
+        if (yScale.isInverted) {
+          y0Scaled = y0 === 0 || y0 === null ? yScale.range[1] : yScale.scale(y0);
+        } else {
+          y0Scaled = y0 === 0 || y0 === null ? yScale.range[0] : yScale.scale(y0);
+        }
+      } else {
+        y = yScale.scale(y1);
+        // use always zero as baseline if y0 is null
+        y0Scaled = y0 === null ? yScale.scale(0) : yScale.scale(y0);
+      }
+
+      const absMinHeight = Math.abs(minBarHeight);
+
+      // safeguard against null y values
+      let height = isNil(y0Scaled) || isNil(y) ? 0 : y0Scaled - y;
+
+      if (isNil(y0Scaled) || isNil(y)) {
+        y = 0;
+      }
+
+      if (absMinHeight !== undefined && height !== 0 && Math.abs(height) < absMinHeight) {
+        const heightDelta = absMinHeight - Math.abs(height);
+        if (height < 0) {
+          height = -absMinHeight;
+          y += heightDelta;
+        } else {
+          height = absMinHeight;
+          y -= heightDelta;
+        }
+      }
+      const isUpsideDown = height < 0;
+      height = Math.abs(height);
+      y = isUpsideDown ? y - height : y;
+
+      const xScaled = xScale.scale(datum.x);
+
+      if (xScaled === null) {
+        return barTuple;
+      }
+
+      const seriesIdentifier: XYChartSeriesIdentifier = {
+        key: dataSeries.key,
+        specId: dataSeries.specId,
+        yAccessor: dataSeries.yAccessor,
+        splitAccessors: dataSeries.splitAccessors,
+        seriesKeys: dataSeries.seriesKeys,
+        smHorizontalAccessorValue: dataSeries.smHorizontalAccessorValue,
+        smVerticalAccessorValue: dataSeries.smVerticalAccessorValue,
+      };
+
+      const seriesStyle = getBarStyleOverrides(datum, seriesIdentifier, sharedSeriesStyle, styleAccessor);
+
+      const maxPixelWidth = clamp(seriesStyle.rect.widthRatio ?? 1, 0, 1) * xScale.bandwidth;
+      const minPixelWidth = clamp(seriesStyle.rect.widthPixel ?? 0, 0, maxPixelWidth);
+
+      const width = clamp(seriesStyle.rect.widthPixel ?? xScale.bandwidth, minPixelWidth, maxPixelWidth);
+      const x = xScaled + xScale.bandwidth * orderIndex + xScale.bandwidth / 2 - width / 2;
+
+      const originalY1Value = stackMode === StackMode.Percentage ? (isNil(y1) ? null : y1 - (y0 ?? 0)) : initialY1;
+      const formattedDisplayValue = displayValueSettings?.valueFormatter?.(originalY1Value);
+
+      // only show displayValue for even bars if showOverlappingValue
+      const displayValueText =
+        displayValueSettings?.isAlternatingValueLabel && barGeometries.length % 2 ? undefined : formattedDisplayValue;
+
+      const { displayValueWidth, fixedFontScale } = computeBoxWidth(
+        displayValueText ?? '',
+        { padding, fontSize, fontFamily, textMeasure, width },
+        displayValueSettings,
+      );
+
+      const isHorizontalRotation = chartRotation == null || [0, 180].includes(chartRotation);
+      // Pick the right side of the label's box to use as factor reference
+      const referenceWidth = Math.max(isHorizontalRotation ? displayValueWidth : fixedFontScale, 1);
+
+      const textScalingFactor = getFinalFontScalingFactor(
+        (width * fontSizeFactor) / referenceWidth,
+        fixedFontScale,
+        fontSize,
+      );
+      const overflowConstraints: Set<LabelOverflowConstraint> = new Set(
+        displayValueSettings?.overflowConstraints ?? [
+          LabelOverflowConstraint.ChartEdges,
+          LabelOverflowConstraint.BarGeometry,
+        ],
+      );
+
+      // Based on rotation scale the width of the text box
+      const bboxWidthFactor = isHorizontalRotation ? textScalingFactor : 1;
+
+      const displayValue: BarGeometry['displayValue'] | undefined =
+        displayValueText && displayValueSettings?.showValueLabel
+          ? {
+              fontScale: textScalingFactor,
+              fontSize: fixedFontScale,
+              text: displayValueText,
+              width: bboxWidthFactor * displayValueWidth,
+              height: textScalingFactor * fixedFontScale,
+              overflowConstraints,
+              isValueContainedInElement: displayValueSettings?.isValueContainedInElement ?? false,
+            }
+          : undefined;
+
+      const barGeometry: BarGeometry = {
+        displayValue,
+        x,
+        y,
+        transform: { x: 0, y: 0 },
+        width,
+        height,
+        color,
+        value: { x: datum.x, y: originalY1Value, mark: null, accessor: BandedAccessorType.Y1, datum: datum.datum },
+        seriesIdentifier,
+        seriesStyle,
+        panel,
+      };
+      indexedGeometryMap.set(barGeometry);
+
+      if (y1 !== null && initialY1 !== null && filled?.y1 === undefined) {
+        barGeometries.push(barGeometry);
+      }
+
+      return barTuple;
+    }, initialBarTuple),
   );
 }
 
