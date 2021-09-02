8358dd80e446b2b96ce5f45dc0fc250ae41cfc21
rshen91 <rachelwshen@gmail.com>
2021-09-02 14:42:01 -0600

    refactor: clean up picked shapes selector

--- packages/charts/src/chart_types/heatmap/state/selectors/get_cursor_pointer.ts
+++ packages/charts/src/chart_types/heatmap/state/selectors/get_cursor_pointer.ts
@@ -8,18 +8,13 @@
 
 import { DEFAULT_CSS_CURSOR } from '../../../../common/constants';
 import { createCustomCachedSelector } from '../../../../state/create_selector';
-import { getGridHeightParamsSelector } from './get_grid_full_height';
 import { isBrushingSelector } from './is_brushing';
 import { getPickedShapes } from './picked_shapes';
 
 /** @internal */
 export const getPointerCursorSelector = createCustomCachedSelector(
-  [getPickedShapes, isBrushingSelector, getGridHeightParamsSelector],
-  (pickedShapes, isBrushing, gridParams) => {
-    return isBrushing ||
-      (Array.isArray(pickedShapes) &&
-        pickedShapes.filter(({ y }) => y < gridParams.gridCellHeight * gridParams.pageSize).length > 0)
-      ? 'pointer'
-      : DEFAULT_CSS_CURSOR;
+  [getPickedShapes, isBrushingSelector],
+  (pickedShapes, isBrushing) => {
+    return isBrushing || (Array.isArray(pickedShapes) && pickedShapes.length > 0) ? 'pointer' : DEFAULT_CSS_CURSOR;
   },
 );
