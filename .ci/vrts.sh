53c3ebd3ba27f57a46545c26d6f81558659ecbe7
Marco Vettorello <vettorello.marco@gmail.com>
2021-09-16 10:45:18 +0200

    fix ci to ignore canvas package

--- .ci/vrts.sh
+++ .ci/vrts.sh
@@ -5,10 +5,17 @@
 ###
 source .ci/global_setup.sh
 
+
+###
+### removing the unused canvas package conflicting with the CI missing C++ lib
+###
+yarn remove canvas
+
 VRTS_FILES=$1
 ###
 ### visual testing
 ###
 echo " -- visual testing"
+
 yarn test:integration:generate
 yarn test:integration --ci $VRTS_FILES
