6e250329eaeef704b54a8d7401cc39e178a5e1b5
Marco Vettorello <vettorello.marco@gmail.com>
2021-09-16 11:34:26 +0200

    fix vrts without jsdom

--- .ci/vrts.sh
+++ .ci/vrts.sh
@@ -6,16 +6,10 @@
 source .ci/global_setup.sh
 
 
-###
-### removing the unused canvas package conflicting with the CI missing C++ lib
-###
-yarn --ignore-workspace-root-check remove canvas
-
 VRTS_FILES=$1
 ###
 ### visual testing
 ###
 echo " -- visual testing"
-
 yarn test:integration:generate
 yarn test:integration --ci $VRTS_FILES
