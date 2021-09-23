effc22684b92f469438d62f8d16f92e6ce685a57
rshen91 <rachelwshen@gmail.com>
2021-09-23 07:01:33 -0600

    fix: fix headers of tooltip for ordinal axis to have no tick formatter

--- packages/charts/src/chart_types/xy_chart/tooltip/tooltip.ts
+++ packages/charts/src/chart_types/xy_chart/tooltip/tooltip.ts
@@ -34,7 +34,6 @@ export function getHighligthedValues(
   defaultValue?: string,
 ): Map<SeriesKey, LegendItemExtraValues> {
   const seriesTooltipValues = new Map<SeriesKey, LegendItemExtraValues>();
-
   tooltipValues.forEach(({ formattedValue, seriesIdentifier, valueAccessor }) => {
     const seriesValue = defaultValue || formattedValue;
     const current: LegendItemExtraValues = seriesTooltipValues.get(seriesIdentifier.key) ?? new Map();
@@ -85,7 +84,7 @@ export function formatTooltip(
     valueAccessor: accessor,
     label,
     value,
-    formattedValue: tickFormatter(value, tickFormatOptions),
+    formattedValue: typeof value === 'string' ? value : tickFormatter(value, tickFormatOptions),
     markValue,
     ...(isDefined(markValue) && {
       formattedMarkValue: spec.markFormat
