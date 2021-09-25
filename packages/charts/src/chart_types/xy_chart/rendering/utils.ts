58cc66a84715365ce0c4a8e9fdbc5851079e0fa3
Robert Monfera <monfera.robert@gmail.com>
2021-09-25 14:14:15 +0200

    refactor: minor if nesting 4

--- packages/charts/src/chart_types/xy_chart/rendering/utils.ts
+++ packages/charts/src/chart_types/xy_chart/rendering/utils.ts
@@ -214,13 +214,10 @@ export function getY0ScaledValueFn(yScale: Scale<number>): (datum: DataSeriesDat
   const isLogScale = isLogarithmicScale(yScale);
   const domainPolarity = getDomainPolarity(yScale.domain);
   const logBaseline = domainPolarity >= 0 ? Math.min(...yScale.domain) : Math.max(...yScale.domain);
-
   return ({ y0 }) => {
     return isLogScale // checking wrong y0 polarity
-      ? y0 === null
+      ? y0 === null || (domainPolarity >= 0 && y0 <= 0) || (domainPolarity < 0 && y0 >= 0) // if all positive domain use 1 as baseline, -1 otherwise
         ? yScale.scale(logBaseline) ?? NaN
-        : (domainPolarity >= 0 && y0 <= 0) || (domainPolarity < 0 && y0 >= 0)
-        ? yScale.scale(logBaseline) ?? NaN // if all positive domain use 1 as baseline, -1 otherwise
         : yScale.scale(y0) ?? NaN // if negative value, use -1 as max reference, 1 otherwise
       : yScale.scale(y0 === null ? DEFAULT_ZERO_BASELINE : y0) ?? NaN;
   };
