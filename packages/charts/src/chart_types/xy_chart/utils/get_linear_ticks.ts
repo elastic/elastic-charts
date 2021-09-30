4ef1b49b95241ed96cca4c407b849f9bf8e3c3f9
nickofthyme <nick.ryan.partridge@gmail.com>
2021-09-30 08:24:14 -0500

    chore: update license for d3 code

--- packages/charts/src/chart_types/xy_chart/utils/get_linear_ticks.ts
+++ packages/charts/src/chart_types/xy_chart/utils/get_linear_ticks.ts
@@ -1,12 +1,25 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
+/* eslint-disable header/header, no-param-reassign */
 
-/* eslint-disable no-param-reassign */
+/**
+ * @notice
+ * This product includes code that is adapted from d3-array@3.0.4 and d3-scale@4.0.2,
+ * which are both available under a "ISC" license.
+ *
+ * ISC License
+ *
+ * Copyright 2010-2021 Mike Bostock
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
 
 import { ScaleContinuousNumeric } from 'd3-scale';
 
@@ -112,4 +125,4 @@ export function getNiceLinearTicks(
   return scale;
 }
 
-/* eslint-enable no-param-reassign */
+/* eslint-enable header/header, no-param-reassign */
