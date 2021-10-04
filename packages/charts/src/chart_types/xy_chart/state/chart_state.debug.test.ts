ba38aa968e24383bfa9b579e01e58b252cd0556a
Marco Vettorello <vettorello.marco@gmail.com>
2021-10-04 18:58:23 +0200

    use match snapshot to fully test the debug state

--- packages/charts/src/chart_types/xy_chart/state/chart_state.debug.test.ts
+++ packages/charts/src/chart_types/xy_chart/state/chart_state.debug.test.ts
@@ -13,40 +13,36 @@ import { Position, Rotation } from '../../../utils/common';
 import { getDebugStateSelector } from './selectors/get_debug_state';
 
 describe('XYChart - debug state', () => {
-  it.each<Rotation>([0, 90, -90, 180])(
-    'return a sorted list of axis labels from the axis origin rotation: %i',
-    (rotation) => {
-      const store = MockStore.default({ width: 200, height: 200, top: 0, left: 0 });
-      MockStore.addSpecs(
-        [
-          MockGlobalSpec.settings({ rotation, debugState: true }),
-          MockSeriesSpec.line({
-            xAccessor: 0,
-            yAccessors: [1],
-            data: [
-              [0, 0],
-              [4, 3],
-              [10, 10],
-            ],
-            xScaleType: ScaleType.Linear,
-            yScaleType: ScaleType.Linear,
-          }),
-          MockGlobalSpec.axis({
-            id: 'bottom',
-            position: Position.Bottom,
-            integersOnly: true,
-          }),
-          MockGlobalSpec.axis({
-            id: 'left',
-            position: Position.Left,
-            integersOnly: true,
-          }),
-        ],
-        store,
-      );
-      const debugState = getDebugStateSelector(store.getState());
-      expect(debugState.axes?.x[0].labels).toEqual(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
-      expect(debugState.axes?.y[0].labels).toEqual(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
-    },
-  );
+  it.each<Rotation>([0, 90, -90, 180])('line chart rotation: %i', (rotation) => {
+    const store = MockStore.default({ width: 200, height: 200, top: 0, left: 0 });
+    MockStore.addSpecs(
+      [
+        MockGlobalSpec.settings({ rotation, debugState: true }),
+        MockSeriesSpec.line({
+          xAccessor: 0,
+          yAccessors: [1],
+          data: [
+            [0, 0],
+            [4, 3],
+            [10, 10],
+          ],
+          xScaleType: ScaleType.Linear,
+          yScaleType: ScaleType.Linear,
+        }),
+        MockGlobalSpec.axis({
+          id: 'bottom',
+          position: Position.Bottom,
+          integersOnly: true,
+        }),
+        MockGlobalSpec.axis({
+          id: 'left',
+          position: Position.Left,
+          integersOnly: true,
+        }),
+      ],
+      store,
+    );
+    const debugState = getDebugStateSelector(store.getState());
+    expect(debugState).toMatchSnapshot();
+  });
 });
