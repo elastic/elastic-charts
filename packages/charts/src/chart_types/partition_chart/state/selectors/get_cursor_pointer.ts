623d62d3f073eb7e27786fda0bd6178ada88604c
rshen91 <rachelwshen@gmail.com>
2021-10-15 08:35:49 -0600

    fix: add get cursor pointer selector to partition charts

---
+++ packages/charts/src/chart_types/partition_chart/state/selectors/get_cursor_pointer.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { DEFAULT_CSS_CURSOR } from '../../../../common/constants';
+import { createCustomCachedSelector } from '../../../../state/create_selector';
+import { getPickedShapes } from './picked_shapes';
+
+/** @internal */
+export const getPointerCursorSelector = createCustomCachedSelector([getPickedShapes], (pickedShapes) => {
+  return Array.isArray(pickedShapes) && pickedShapes.length > 0 ? 'pointer' : DEFAULT_CSS_CURSOR;
+});
