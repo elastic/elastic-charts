4ce83e9d7bc3be33e0c4149fc02fed1a9d905474
rshen91 <rachelwshen@gmail.com>
2021-10-18 07:30:03 -0600

    fix: add onElementClick and onElementOver from settings to selector

--- packages/charts/src/chart_types/partition_chart/state/selectors/get_cursor_pointer.ts
+++ packages/charts/src/chart_types/partition_chart/state/selectors/get_cursor_pointer.ts
@@ -8,9 +8,15 @@
 
 import { DEFAULT_CSS_CURSOR } from '../../../../common/constants';
 import { createCustomCachedSelector } from '../../../../state/create_selector';
+import { getSettingsSpecSelector } from '../../../../state/selectors/get_settings_specs';
 import { getPickedShapes } from './picked_shapes';
 
 /** @internal */
-export const getPointerCursorSelector = createCustomCachedSelector([getPickedShapes], (pickedShapes) => {
-  return Array.isArray(pickedShapes) && pickedShapes.length > 0 ? 'pointer' : DEFAULT_CSS_CURSOR;
-});
+export const getPointerCursorSelector = createCustomCachedSelector(
+  [getPickedShapes, getSettingsSpecSelector],
+  (pickedShapes, { onElementClick, onElementOver }) => {
+    return Array.isArray(pickedShapes) && pickedShapes.length > 0 && (onElementClick || onElementOver)
+      ? 'pointer'
+      : DEFAULT_CSS_CURSOR;
+  },
+);
