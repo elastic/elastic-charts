989542e6729303db7b8a2b82ea11b24caf7ec0e2
nickofthyme <nick.ryan.partridge@gmail.com>
2021-11-04 17:00:44 -0500

    feat(theme): move to amsterdam eui theme

--- storybook/stories/legend/9_color_picker.story.tsx
+++ storybook/stories/legend/9_color_picker.story.tsx
@@ -8,6 +8,7 @@
 
 import { EuiColorPicker, EuiWrappingPopover, EuiButton, EuiSpacer, EuiFlexItem, EuiButtonEmpty } from '@elastic/eui';
 import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
 import React, { useState, useMemo } from 'react';
 
 import {
@@ -25,11 +26,13 @@ import {
 import { BARCHART_1Y1G } from '@elastic/charts/src/utils/data_samples/test_dataset';
 
 import { useBaseTheme } from '../../use_base_theme';
+import { getLegendAction } from '../utils/components/get_legend_action';
 
 const onChangeAction = action('onChange');
 
 export const Example = () => {
   const [colors, setColors] = useState<Record<SeriesKey, Color | null>>({});
+  const showAction = boolean('show legend action', false);
 
   const CustomColorPicker: LegendColorPicker = useMemo(
     () => ({ anchor, color, onClose, seriesIdentifiers, onChange }) => {
@@ -71,7 +74,12 @@ export const Example = () => {
   CustomColorPicker.displayName = 'CustomColorPicker';
   return (
     <Chart>
-      <Settings showLegend legendColorPicker={CustomColorPicker} baseTheme={useBaseTheme()} />
+      <Settings
+        showLegend
+        legendColorPicker={CustomColorPicker}
+        baseTheme={useBaseTheme()}
+        legendAction={showAction ? getLegendAction() : undefined}
+      />
       <Axis id="bottom" position={Position.Bottom} title="Bottom axis" showOverlappingTicks />
       <Axis id="left2" title="Left axis" position={Position.Left} tickFormat={(d: any) => Number(d).toFixed(2)} />
 
