5fa6c570b253d5ba8908e570faae732feb43e4ec
Marco Vettorello <vettorello.marco@gmail.com>
2021-11-19 18:07:34 +0100

    Add computeRatioByGroups function

---
+++ packages/charts/src/utils/data/data_processing.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { groupBy, GroupKeysOrKeyFn } from '../../chart_types/xy_chart/utils/group_data_series';
+import { isFiniteNumber } from '../common';
+
+/**
+ * The function computes the participation ratio of a value in the total sum of its membership group.
+ * It returns a shallow copy of the input array where each object is augmented with the computed ratio.
+ *
+ * @remarks
+ * The ratio is computed using absolute values.
+ * Product A made a profit of $200, and product B has a loss of $300. In total, the company lost $100 ($200 – $300).
+ * Product A contributed: abs(200) / ( abs(200) + abs(-300) ) * 100% = 40%
+ * Product B contributed: abs(-300) / ( abs(200) + abs(-300) ) * 100% = 60%
+ * Product A and product B contributed 40% and 60% respectively on a total loss of $100.
+ *
+ * We don't compute the ratio for non-finite values. In this case, the original non-finite value is returned.
+ *
+ * If the sum of the group values is 0, each ratio is considered 0.
+ *
+ * @public
+ * @param data an array of Record<string, unknown>
+ * @param groupAccessors an array of accessor keys or a fn to describe an unique id for each group
+ * @param valueAccessor a fn that returns the value to use
+ * @param ratioKeyName the object key used to store the computed ratio
+ */
+export function computeRatioByGroups<T extends Record<string, unknown>>(
+  data: T[],
+  groupAccessors: GroupKeysOrKeyFn<T>,
+  valueAccessor: (k: T) => number | null | undefined,
+  ratioKeyName: string,
+) {
+  return groupBy(data, groupAccessors, true)
+    .map((groupedData) => {
+      const groupSum = groupedData.reduce((sum, datum) => {
+        const value = valueAccessor(datum);
+        return sum + (isFiniteNumber(value) ? Math.abs(value) : 0);
+      }, 0);
+      return groupedData.map((datum) => {
+        const value = valueAccessor(datum);
+        return {
+          ...datum,
+          // if the value is null/undefined we don't compute the ratio, we just return the original null/undefined value
+          [ratioKeyName]: isFiniteNumber(value) ? (groupSum === 0 ? 0 : Math.abs(value) / groupSum) : value,
+        };
+      });
+    })
+    .flat();
+}
