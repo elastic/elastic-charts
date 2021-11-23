6637aaf337f415cc49d6994a2c541f76f12a46ee
nickofthyme <nicholas.partridge@elastic.co>
2021-11-23 08:15:11 -0600

    fix: stacked polarity

---
+++ storybook/stories/mixed/8_polarized_stacked.story.tsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { boolean, select } from '@storybook/addon-knobs';
+import numeral from 'numeral';
+import React from 'react';
+
+import { Axis, Chart, BarSeries, Position, ScaleType, Settings, StackMode } from '@elastic/charts';
+
+import { useBaseTheme } from '../../use_base_theme';
+import { data } from '../utils/datasets/product_profits';
+
+export const Example = () => {
+  const stacked = boolean('stacked', true);
+  const positive = boolean('positive only', false);
+  const customDomain = boolean('custom domain', false);
+  const stackMode =
+    select<StackMode | undefined>(
+      'stackMode',
+      {
+        None: undefined,
+        Silhouette: StackMode.Silhouette,
+        Wiggle: StackMode.Wiggle,
+        Percentage: StackMode.Percentage,
+      },
+      undefined,
+    ) ?? undefined;
+  return (
+    <Chart>
+      <Settings showLegend showLegendExtra legendPosition={Position.Right} baseTheme={useBaseTheme()} />
+      <Axis id="bottom" position={Position.Bottom} title="Products" />
+      <Axis
+        id="left"
+        title="Profit"
+        position={Position.Left}
+        domain={customDomain ? { min: -25000, max: 50000 } : undefined}
+        tickFormat={(d) => numeral(d).format(stackMode === 'percentage' ? '0%' : '$0,0')}
+      />
+
+      <BarSeries
+        id="bars"
+        color={['#7BC0F7', '#3C8AD8', '#F18026', '#FEDB69']}
+        xScaleType={ScaleType.Ordinal}
+        yScaleType={ScaleType.Linear}
+        xAccessor="product"
+        yAccessors={[({ profit }) => (positive ? Math.abs(profit) : profit)]}
+        splitSeriesAccessors={['state']}
+        stackMode={stackMode}
+        stackAccessors={stacked ? ['yes'] : undefined}
+        data={data}
+      />
+    </Chart>
+  );
+};
---
+++ storybook/stories/utils/datasets/product_profits.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+interface Row {
+  state: string;
+  profit: number;
+  product: string;
+}
+
+export const data: Row[] = [
+  { state: 'Florida', product: 'Powder', profit: 10000 },
+  { state: 'Florida', product: 'Mascara', profit: 9000 },
+  { state: 'Florida', product: 'Lip gloss', profit: 17000 },
+  { state: 'Florida', product: 'Foundation', profit: 6000 },
+  { state: 'Florida', product: 'Eyeliner', profit: 7000 },
+  { state: 'Florida', product: 'Eseshadow', profit: 1000 },
+  { state: 'Florida', product: 'Lipstick', profit: 4000 },
+  { state: 'Florida', product: 'Rouge', profit: 6000 },
+  { state: 'Florida', product: '', profit: 8000 },
+  { state: 'Florida', product: 'Nail polish', profit: 8000 },
+  { state: 'Texas', product: 'Powder', profit: 9000 },
+  { state: 'Texas', product: 'Mascara', profit: 7000 },
+  { state: 'Texas', product: 'Lip gloss', profit: 8000 },
+  { state: 'Texas', product: 'Foundation', profit: 6000 },
+  { state: 'Texas', product: 'Eyeliner', profit: 10000 },
+  { state: 'Texas', product: 'Eseshadow', profit: 9000 },
+  { state: 'Texas', product: 'Lipstick', profit: 5000 },
+  { state: 'Texas', product: 'Rouge', profit: -4000 },
+  { state: 'Texas', product: '', profit: -3000 },
+  { state: 'Texas', product: 'Nail polish', profit: -2000 },
+  { state: 'Nevada', product: 'Powder', profit: -5000 },
+  { state: 'Nevada', product: 'Mascara', profit: -4000 },
+  { state: 'Nevada', product: 'Lip gloss', profit: 2000 },
+  { state: 'Nevada', product: 'Foundation', profit: -2000 },
+  { state: 'Nevada', product: 'Eyeliner', profit: 1000 },
+  { state: 'Nevada', product: 'Eseshadow', profit: -2000 },
+  { state: 'Nevada', product: 'Lipstick', profit: 1000 },
+  { state: 'Nevada', product: 'Rouge', profit: -1000 },
+  { state: 'Nevada', product: '', profit: -1000 },
+  { state: 'Nevada', product: 'Nail polish', profit: -1000 },
+  { state: 'Arizona', product: 'Powder', profit: 20000 },
+  { state: 'Arizona', product: 'Mascara', profit: 21000 },
+  { state: 'Arizona', product: 'Lip gloss', profit: -1000 },
+  { state: 'Arizona', product: 'Foundation', profit: 8000 },
+  { state: 'Arizona', product: 'Eyeliner', profit: -2000 },
+  { state: 'Arizona', product: 'Eseshadow', profit: -1000 },
+  { state: 'Arizona', product: 'Lipstick', profit: -4000 },
+  { state: 'Arizona', product: 'Rouge', profit: 2000 },
+  { state: 'Arizona', product: '', profit: -1000 },
+  { state: 'Arizona', product: 'Nail polish', profit: -1000 },
+];
