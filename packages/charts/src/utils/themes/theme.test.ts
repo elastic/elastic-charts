6fe55a0873a3f97ff1f1c81e784571da0ac5df03
Marco Vettorello <vettorello.marco@gmail.com>
2021-11-26 12:31:16 +0100

    Fix RGBATuple to Color changes

--- packages/charts/src/utils/themes/theme.test.ts
+++ packages/charts/src/utils/themes/theme.test.ts
@@ -163,7 +163,7 @@ describe('Theme', () => {
             opacity: 314571,
             visible: false,
             dash: [1, 2, 3, 4],
-            color: [1, 2, 3, 4],
+            color: 'rgba(1, 2, 3, 4)',
           },
         },
       };
@@ -215,13 +215,13 @@ describe('Theme', () => {
           area: {
             opacity: 314571,
             visible: false,
-            color: [1, 2, 3, 4],
+            color: 'rgba(1, 2, 3, 4)',
           },
           line: {
             opacity: 314571,
             visible: false,
             dash: [1, 2, 3, 4],
-            color: [1, 2, 3, 4],
+            color: 'rgba(1, 2, 3, 4)',
           },
         },
       };
