578eaa618791b76ca8c9815a3cca5c5de328fefa
Marco Vettorello <vettorello.marco@gmail.com>
2021-12-20 21:20:54 +0100

    Remove console log

--- packages/charts/src/components/brush/brush.tsx
+++ packages/charts/src/components/brush/brush.tsx
@@ -99,7 +99,6 @@ class BrushToolComponent extends React.Component<StateProps> {
       return;
     }
     const { top, left, width, height } = brushEvent;
-    console.log({ top, left, width, height });
     withContext(ctx, () => {
       ctx.scale(this.devicePixelRatio, this.devicePixelRatio);
       withClip(
