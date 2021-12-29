elastic/elastic-charts

commit 6db267718cbc37c678c565ac4a0a174cc3e9df9f

refactor(partition): remove config in favor on spec and theme controls (#1402)
BREAKING CHANGES: The `PartitionSpec.config` prop is removed. All properties have been moved/renamed under new `Theme.partition` options or `PartitionSpec` itself with the following exceptions:

- `PartitionSpec.config.margin` is removed in favor of `Theme.chartPaddings`. This no longer supports ratios.
- `PartitionSpec.config.backgroundColor` is removed in favor of `Theme.background.color`.
- `PartitionSpec.config.width` and `PartitionSpec.config.height` both removed as they were never used.

packages/charts/src/chart_types/partition_chart/layout/types/config_types.ts
@@ -6,13 +6,9 @@
  * Side Public License, v 1.
  */
 
-import { $Values as Values } from 'utility-types';
+import { $Values } from 'utility-types';
 
-import { Color } from '../../../../common/colors';
-import { Distance, Pixels, Radian, Radius, Ratio, SizeRatio, TimeMs } from '../../../../common/geometry';
-import { Font, FontFamily, PartialFont } from '../../../../common/text_utils';
-import { ColorVariant, StrokeStyle, ValueFormatter } from '../../../../utils/common';
-import { PerSideDistance } from '../../../../utils/dimensions';
+import { Ratio, TimeMs } from '../../../../common/geometry';
 
 /** @public */
 export const PartitionLayout = Object.freeze({
@@ -25,100 +21,7 @@ export const PartitionLayout = Object.freeze({
 });
 
 /** @public */
-export type PartitionLayout = Values<typeof PartitionLayout>; // could use ValuesType<typeof HierarchicalChartTypes>
-
-/** @public */
-export type PerSidePadding = PerSideDistance;
-
-/** @public */
-export type Padding = Pixels | Partial<PerSidePadding>;
-
-interface LabelConfig extends Font {
-  textColor: Color | typeof ColorVariant.Adaptive;
-  valueFormatter: ValueFormatter;
-  valueFont: PartialFont;
-  padding: Padding;
-}
-
-/** @public */
-export interface FillLabelConfig extends LabelConfig {
-  clipText: boolean;
-}
-
-/** @public */
-export interface LinkLabelConfig extends LabelConfig {
-  fontSize: Pixels; // todo consider putting it in Font
-  maximumSection: Distance; // use linked labels below this limit
-  gap: Pixels;
-  spacing: Pixels;
-  minimumStemLength: Distance;
-  stemAngle: Radian;
-  horizontalStemLength: Distance;
-  radiusPadding: Distance;
-  lineWidth: Pixels;
-  maxCount: number;
-  maxTextLength: number;
-}
-
-/** @public */
-export interface FillFontSizeRange {
-  minFontSize: Pixels;
-  maxFontSize: Pixels;
-  idealFontSizeJump: Ratio;
-  /**
-   * When `maximizeFontSize` is false (the default), text font will not be larger than font sizes in larger sectors/rectangles in the same pie chart,
-   * sunburst ring or treemap layer. When it is set to true, the largest font, not exceeding `maxFontSize`, that fits in the slice/sector/rectangle
-   * will be chosen for easier text readability, irrespective of the value.
-   */
-  maximizeFontSize: boolean;
-}
-
-/** @public */
-export interface RelativeMargins {
-  left: SizeRatio;
-  right: SizeRatio;
-  top: SizeRatio;
-  bottom: SizeRatio;
-}
-
-// todo switch to `io-ts` style, generic way of combining static and runtime type info
-/** @public */
-export interface StaticConfig extends FillFontSizeRange {
-  // shape geometry
-  width: number;
-  height: number;
-  margin: RelativeMargins;
-  emptySizeRatio: SizeRatio;
-  outerSizeRatio: SizeRatio;
-  clockwiseSectors: boolean;
-  specialFirstInnermostSector: boolean;
-  partitionLayout: PartitionLayout;
-  /** @alpha */
-  drilldown: boolean;
-
-  // general text config
-  fontFamily: FontFamily;
-
-  // fill text layout config
-  circlePadding: Distance;
-  radialPadding: Distance;
-  horizontalTextAngleThreshold: Radian;
-  horizontalTextEnforcer: Ratio;
-  maxRowCount: number;
-  fillOutside: boolean;
-  radiusOutside: Radius;
-  fillRectangleWidth: Distance;
-  fillRectangleHeight: Distance;
-  fillLabel: FillLabelConfig;
-
-  // linked labels (primarily: single-line)
-  linkLabel: LinkLabelConfig;
-
-  // global
-  backgroundColor: Color;
-  sectorLineWidth: Pixels;
-  sectorLineStroke: StrokeStyle;
-}
+export type PartitionLayout = $Values<typeof PartitionLayout>; // could use ValuesType<typeof HierarchicalChartTypes>
 
 /** @alpha */
 export type EasingFunction = (x: Ratio) => Ratio;
@@ -127,14 +30,14 @@ export type EasingFunction = (x: Ratio) => Ratio;
 export interface AnimKeyframe {
   time: number;
   easingFunction: EasingFunction;
-  keyframeConfig: Partial<StaticConfig>;
+  // keyframeConfig: Partial<StaticConfig>;
 }
 
 /** @public */
-export interface Config extends StaticConfig {
+export interface AnimationConfig {
   /** @alpha */
-  animation: {
+  animation?: {
     duration: TimeMs;
-    keyframes: Array<AnimKeyframe>;
+    keyframes?: Array<AnimKeyframe>;
   };
 }

packages/charts/src/chart_types/partition_chart/layout/types/viewmodel_types.ts
@@ -21,12 +21,15 @@ import {
 import { Font, HorizontalAlignment, VerticalAlignments } from '../../../../common/text_utils';
 import { GroupByAccessor } from '../../../../specs';
 import { LegendPath } from '../../../../state/actions/legend';
+import { Size } from '../../../../utils/dimensions';
+import { LIGHT_THEME } from '../../../../utils/themes/light_theme';
+import { Theme } from '../../../../utils/themes/theme';
 import { ContinuousDomainFocus } from '../../renderer/canvas/partition';
 import { Layer } from '../../specs';
-import { config, MODEL_KEY, ValueGetterName } from '../config';
+import { MODEL_KEY, ValueGetterName } from '../config';
 import { ArrayNode, HierarchyOfArrays } from '../utils/group_by_rollup';
 import { LinkLabelsViewModelSpec } from '../viewmodel/link_text_layout';
-import { Config, PartitionLayout } from './config_types';
+import { AnimationConfig, PartitionLayout } from './config_types';
 
 /** @internal */
 export type LinkLabelVM = {
@@ -105,7 +108,7 @@ export type PickFunction = (x: Pixels, y: Pixels, focus: ContinuousDomainFocus) 
 /** @internal */
 export interface PartitionSmallMultiplesModel extends SmallMultiplesDescriptors {
   smAccessorValue: number | string;
-  partitionLayout: PartitionLayout;
+  layout: PartitionLayout;
   top: SizeRatio;
   left: SizeRatio;
   width: SizeRatio;
@@ -126,8 +129,9 @@ export interface PartitionSmallMultiplesModel extends SmallMultiplesDescriptors 
 }
 
 /** @internal */
-export interface ShapeViewModel extends PartitionSmallMultiplesModel {
-  config: Config;
+export interface ShapeViewModel extends PartitionSmallMultiplesModel, AnimationConfig {
+  style: Theme['partition'];
+  chartDimensions: Size;
   layers: Layer[];
   quadViewModel: QuadViewModel[];
   rowSets: RowSet[];
@@ -147,7 +151,9 @@ const defaultFont: Font = {
 };
 
 /** @internal */
-export const nullPartitionSmallMultiplesModel = (partitionLayout: PartitionLayout): PartitionSmallMultiplesModel => ({
+export const nullPartitionSmallMultiplesModel = (
+  layout: PartitionLayout = PartitionLayout.sunburst,
+): PartitionSmallMultiplesModel => ({
   index: 0,
   innerIndex: 0,
   smAccessorValue: '',
@@ -161,7 +167,7 @@ export const nullPartitionSmallMultiplesModel = (partitionLayout: PartitionLayou
   innerColumnIndex: 0,
   marginLeftPx: 0,
   marginTopPx: 0,
-  partitionLayout,
+  layout,
   panel: {
     title: '',
     innerWidth: 0,
@@ -178,9 +184,13 @@ export const nullPartitionSmallMultiplesModel = (partitionLayout: PartitionLayou
 });
 
 /** @internal */
-export const nullShapeViewModel = (specifiedConfig?: Config, diskCenter?: PointObject): ShapeViewModel => ({
-  ...nullPartitionSmallMultiplesModel((specifiedConfig || config).partitionLayout),
-  config: specifiedConfig || config,
+export const nullShapeViewModel = (
+  layout: PartitionLayout = PartitionLayout.sunburst,
+  style: Theme['partition'] = LIGHT_THEME.partition,
+  diskCenter?: PointObject,
+): ShapeViewModel => ({
+  ...nullPartitionSmallMultiplesModel(layout),
+  style,
   layers: [],
   quadViewModel: [],
   rowSets: [],
@@ -194,6 +204,10 @@ export const nullShapeViewModel = (specifiedConfig?: Config, diskCenter?: PointO
   diskCenter: diskCenter || { x: 0, y: 0 },
   pickQuads: () => [],
   outerRadius: 0,
+  chartDimensions: {
+    width: 0,
+    height: 0,
+  },
 });
 
 /** @internal */

packages/charts/src/chart_types/partition_chart/layout/viewmodel/viewmodel.ts
@@ -7,7 +7,7 @@
  */
 
 import { colorToRgba } from '../../../../common/color_library_wrappers';
-import { Color, Colors } from '../../../../common/colors';
+import { Colors } from '../../../../common/colors';
 import { TAU } from '../../../../common/constants';
 import { fillTextColor } from '../../../../common/fill_text_color';
 import {
@@ -19,12 +19,15 @@ import {
   trueBearingToStandardPositionAngle,
 } from '../../../../common/geometry';
 import { Part, TextMeasure } from '../../../../common/text_utils';
-import { GroupByAccessor, SmallMultiplesStyle } from '../../../../specs';
-import { StrokeStyle, ValueFormatter, RecursivePartial, ColorVariant } from '../../../../utils/common';
+import { GroupByAccessor } from '../../../../specs';
+import { StrokeStyle, ColorVariant } from '../../../../utils/common';
+import { Size } from '../../../../utils/dimensions';
 import { Logger } from '../../../../utils/logger';
-import { Layer } from '../../specs';
-import { config as defaultConfig, MODEL_KEY, percentValueGetter } from '../config';
-import { Config, FillLabelConfig, PartitionLayout } from '../types/config_types';
+import { FillLabelConfig, PartitionStyle } from '../../../../utils/themes/partition';
+import { BackgroundStyle } from '../../../../utils/themes/theme';
+import { Layer, PartitionSpec } from '../../specs';
+import { MODEL_KEY, percentValueGetter } from '../config';
+import { PartitionLayout } from '../types/config_types';
 import {
   nullShapeViewModel,
   OutsideLinksViewModel,
@@ -84,8 +87,8 @@ export const isWaffle = (p: PartitionLayout | undefined) => p === PartitionLayou
 export const isLinear = (p: PartitionLayout | undefined) => isFlame(p) || isIcicle(p);
 
 /** @internal */
-export const isSimpleLinear = (config: RecursivePartial<Config>, layers: Layer[]) =>
-  isLinear(config.partitionLayout) && layers.every((l) => l.fillLabel?.clipText ?? config.fillLabel?.clipText);
+export const isSimpleLinear = (layout: PartitionLayout, fillLabel: FillLabelConfig, layers: Layer[]) =>
+  isLinear(layout) && layers.every((l) => l.fillLabel?.clipText ?? fillLabel?.clipText);
 
 function grooveAccessor(n: ArrayEntry) {
   return entryValue(n).depth > 1 ? 1 : [0, 2][entryValue(n).depth];
@@ -139,14 +142,14 @@ export function makeQuadViewModel(
   index: number,
   innerIndex: number,
   fillLabel: FillLabelConfig,
-  isSunburstLayout: boolean,
-  containerBackgroundColor?: Color,
+  { color: backgroundColor, fallbackColor: fallbackBGColor }: BackgroundStyle,
 ): Array<QuadViewModel> {
-  if (colorToRgba(containerBackgroundColor ?? Colors.White.keyword)[3] < 1) {
+  if (colorToRgba(backgroundColor)[3] < 1) {
+    // Override handled in fill_text_color.ts
     Logger.expected(
-      `Text contrast requires a opaque background color, using white as fallback`,
+      'Text contrast requires an opaque background color, using fallbackColor',
       'an opaque color',
-      containerBackgroundColor,
+      backgroundColor,
     );
   }
   return childNodes.map((node) => {
@@ -156,11 +159,10 @@ export function makeQuadViewModel(
     const strokeWidth = sectorLineWidth;
     const strokeStyle = sectorLineStroke;
     const textNegligible = node.y1px - node.y0px < minRectHeightForText;
-
     const textColor = textNegligible
       ? Colors.Transparent.keyword
       : fillLabel.textColor === ColorVariant.Adaptive
-      ? fillTextColor(fillColor, containerBackgroundColor)
+      ? fillTextColor(fallbackBGColor, fillColor, backgroundColor)
       : fillLabel.textColor;
 
     return { index, innerIndex, smAccessorValue, strokeWidth, strokeStyle, fillColor, textColor, ...node };
@@ -287,43 +289,47 @@ const rawChildNodes = (
 /** @internal */
 export function shapeViewModel(
   textMeasure: TextMeasure,
-  config: Config,
-  layers: Layer[],
+  spec: PartitionSpec,
+  style: PartitionStyle,
+  chartDimensions: Size,
   rawTextGetter: RawTextGetter,
-  specifiedValueFormatter: ValueFormatter,
-  specifiedPercentFormatter: ValueFormatter,
   valueGetter: ValueGetterFunction,
   tree: HierarchyOfArrays,
-  topGroove: Pixels,
-  containerBackgroundColor: Color,
-  smallMultiplesStyle: SmallMultiplesStyle,
+  backgroundStyle: BackgroundStyle,
   panelModel: PartitionSmallMultiplesModel,
 ): ShapeViewModel {
   const {
-    width,
-    height,
+    layout,
+    layers,
+    topGroove,
+    valueFormatter: specifiedValueFormatter,
+    percentFormatter: specifiedPercentFormatter,
+    fillOutside,
+    clockwiseSectors,
+    maxRowCount,
+    specialFirstInnermostSector,
+  } = spec;
+  const {
     emptySizeRatio,
     outerSizeRatio,
-    fillOutside,
     linkLabel,
-    clockwiseSectors,
-    specialFirstInnermostSector,
     minFontSize,
-    partitionLayout,
     sectorLineWidth,
-  } = config;
-
+    sectorLineStroke,
+    fillLabel,
+  } = style;
+  const { width, height } = chartDimensions;
   const { marginLeftPx, marginTopPx, panel } = panelModel;
 
-  const treemapLayout = isTreemap(partitionLayout);
-  const mosaicLayout = isMosaic(partitionLayout);
-  const sunburstLayout = isSunburst(partitionLayout);
-  const icicleLayout = isIcicle(partitionLayout);
-  const flameLayout = isFlame(partitionLayout);
-  const simpleLinear = isSimpleLinear(config, layers);
-  const waffleLayout = isWaffle(partitionLayout);
+  const treemapLayout = isTreemap(layout);
+  const mosaicLayout = isMosaic(layout);
+  const sunburstLayout = isSunburst(layout);
+  const icicleLayout = isIcicle(layout);
+  const flameLayout = isFlame(layout);
+  const simpleLinear = isSimpleLinear(layout, fillLabel, layers);
+  const waffleLayout = isWaffle(layout);
 
-  const diskCenter = isSunburst(partitionLayout)
+  const diskCenter = isSunburst(layout)
     ? {
         x: marginLeftPx + panel.innerWidth / 2,
         y: marginTopPx + panel.innerHeight / 2,
@@ -335,14 +341,14 @@ export function shapeViewModel(
 
   // don't render anything if the total, the width or height is not positive
   if (!(width > 0) || !(height > 0) || tree.length === 0) {
-    return nullShapeViewModel(config, diskCenter);
+    return nullShapeViewModel(layout, style, diskCenter);
   }
 
   const longestPath = ([, { children, path }]: ArrayEntry): number =>
     children.length > 0 ? children.reduce((p, n) => Math.max(p, longestPath(n)), 0) : path.length;
   const maxDepth = longestPath(tree[0]) - 2; // don't include the root node
   const childNodes = rawChildNodes(
-    partitionLayout,
+    layout,
     tree,
     topGroove,
     panel.innerWidth,
@@ -371,14 +377,13 @@ export function shapeViewModel(
   const quadViewModel = makeQuadViewModel(
     shownChildNodes.slice(1).map(partToShapeFn),
     layers,
-    config.sectorLineWidth,
-    config.sectorLineStroke,
+    sectorLineWidth,
+    sectorLineStroke,
     panelModel.smAccessorValue,
     panelModel.index,
     panelModel.innerIndex,
-    config.fillLabel,
-    sunburstLayout,
-    containerBackgroundColor,
+    fillLabel,
+    backgroundStyle,
   );
 
   // fill text
@@ -398,9 +403,9 @@ export function shapeViewModel(
 
   const getRowSets = sunburstLayout
     ? fillTextLayout(
-        ringSectorConstruction(config, innerRadius, ringThickness),
+        ringSectorConstruction(spec, style, innerRadius, ringThickness),
         getSectorRowGeometry,
-        inSectorRotation(config.horizontalTextEnforcer, config.horizontalTextAngleThreshold),
+        inSectorRotation(style.horizontalTextEnforcer, style.horizontalTextAngleThreshold),
       )
     : simpleLinear || waffleLayout
     ? () => [] // no multirow layout needed for simpleLinear partitions; no text at all for waffles
@@ -416,9 +421,10 @@ export function shapeViewModel(
     valueGetter,
     valueFormatter,
     nodesWithRoom,
-    config,
+    style,
     layers,
     textFillOrigins,
+    maxRowCount,
     !sunburstLayout,
     !(treemapLayout || mosaicLayout),
   );
@@ -438,12 +444,12 @@ export function shapeViewModel(
           // successful text render if found, and has some row(s)
           return !(foundInFillText && foundInFillText.rows.length > 0);
         });
-  const maxLinkedLabelTextLength = config.linkLabel.maxTextLength;
+  const maxLinkedLabelTextLength = style.linkLabel.maxTextLength;
   const linkLabelViewModels = linkTextLayout(
     panel.innerWidth,
     panel.innerHeight,
     textMeasure,
-    config,
+    style,
     nodesWithoutRoom,
     currentY,
     outerRadius,
@@ -455,7 +461,7 @@ export function shapeViewModel(
       x: width * panelModel.left + panel.innerWidth / 2,
       y: height * panelModel.top + panel.innerHeight / 2,
     },
-    containerBackgroundColor,
+    backgroundStyle,
   );
 
   const pickQuads: PickFunction = sunburstLayout
@@ -477,9 +483,8 @@ export function shapeViewModel(
 
   // combined viewModel
   return {
-    partitionLayout: config?.partitionLayout ?? defaultConfig.partitionLayout,
+    layout,
     smAccessorValue: panelModel.smAccessorValue,
-
     index: panelModel.index,
     innerIndex: panelModel.innerIndex,
     width: panelModel.width,
@@ -495,8 +500,7 @@ export function shapeViewModel(
     panel: {
       ...panelModel.panel,
     },
-
-    config,
+    style,
     layers,
     diskCenter,
     quadViewModel,
@@ -505,6 +509,7 @@ export function shapeViewModel(
     outsideLinksViewModel,
     pickQuads,
     outerRadius,
+    chartDimensions,
   };
 }
 

packages/charts/src/chart_types/partition_chart/specs/index.ts
@@ -9,25 +9,21 @@
 import React from 'react';
 
 import { ChartType } from '../..';
-import { Pixels } from '../../../common/geometry';
+import { Distance, Pixels, Radius } from '../../../common/geometry';
 import { Spec } from '../../../specs';
 import { SpecType } from '../../../specs/constants'; // kept as unshortened import on separate line otherwise import circularity emerges
 import { getConnect, specComponentFactory } from '../../../state/spec_factory';
 import { IndexedAccessorFn } from '../../../utils/accessor';
-import {
-  Datum,
-  LabelAccessor,
-  RecursivePartial,
-  ShowAccessor,
-  ValueAccessor,
-  ValueFormatter,
-} from '../../../utils/common';
-import { config, percentFormatter } from '../layout/config';
-import { Config, FillFontSizeRange, FillLabelConfig } from '../layout/types/config_types';
+import { Datum, LabelAccessor, ShowAccessor, ValueAccessor, ValueFormatter } from '../../../utils/common';
+import { FillFontSizeRange, FillLabelConfig } from '../../../utils/themes/partition';
+import { percentFormatter } from '../layout/config';
+import { AnimationConfig, PartitionLayout } from '../layout/types/config_types';
 import { NodeColorAccessor, ShapeTreeNode, ValueGetter } from '../layout/types/viewmodel_types';
 import { AGGREGATE_KEY, NodeSorter, PrimitiveValue } from '../layout/utils/group_by_rollup';
 
-interface ExtendedFillLabelConfig extends FillLabelConfig, FillFontSizeRange {}
+interface ExtendedFillLabelConfig extends FillLabelConfig, FillFontSizeRange {
+  valueFormatter: ValueFormatter;
+}
 
 /**
  * Specification for a given layer in the partition chart
@@ -42,10 +38,13 @@ export interface Layer {
   shape?: { fillColor: string | NodeColorAccessor };
 }
 
+/**
+ * @todo: we really need these typed, but since `specComponentFactory` has the typing
+ * for optional and required props built-in, this is not currently possible.
+ */
 const defaultProps = {
   chartType: ChartType.Partition,
   specType: SpecType.Series,
-  config,
   valueAccessor: (d: Datum) => (typeof d === 'number' ? d : 0),
   valueGetter: (n: ShapeTreeNode): number => n[AGGREGATE_KEY],
   valueFormatter: (d: number): string => String(d),
@@ -60,16 +59,24 @@ const defaultProps = {
       fillLabel: {},
     },
   ],
+  clockwiseSectors: true,
+  specialFirstInnermostSector: true,
+  layout: PartitionLayout.sunburst,
+  drilldown: false,
+  maxRowCount: 12,
+  fillOutside: false,
+  radiusOutside: 128,
+  fillRectangleWidth: Infinity,
+  fillRectangleHeight: Infinity,
 };
 
 /**
  * Specifies the partition chart
  * @public
  */
-export interface PartitionSpec extends Spec {
+export interface PartitionSpec extends Spec, AnimationConfig {
   specType: typeof SpecType.Series;
   chartType: typeof ChartType.Partition;
-  config: RecursivePartial<Config>;
   data: Datum[];
   valueAccessor: ValueAccessor;
   valueFormatter: ValueFormatter;
@@ -78,6 +85,24 @@ export interface PartitionSpec extends Spec {
   topGroove: Pixels;
   smallMultiples: string | null;
   layers: Layer[];
+  /**
+   * Largest to smallest sectors are positioned in a clockwise order
+   */
+  clockwiseSectors: boolean;
+  /**
+   * Starts placement with the second largest slice, for the innermost pie/ring
+   */
+  specialFirstInnermostSector: boolean;
+  layout: PartitionLayout;
+  maxRowCount: number;
+  /** @alpha */
+  drilldown: boolean;
+
+  // These need examples or documentation
+  fillOutside: boolean;
+  radiusOutside: Radius;
+  fillRectangleWidth: Distance;
+  fillRectangleHeight: Distance;
 }
 
 type SpecRequiredProps = Pick<PartitionSpec, 'id' | 'data'>;
@@ -90,10 +115,18 @@ export const Partition: React.FunctionComponent<SpecRequiredProps & SpecOptional
     | 'valueAccessor'
     | 'valueGetter'
     | 'valueFormatter'
-    | 'layers'
-    | 'config'
     | 'percentFormatter'
     | 'topGroove'
     | 'smallMultiples'
+    | 'layers'
+    | 'clockwiseSectors'
+    | 'specialFirstInnermostSector'
+    | 'layout'
+    | 'drilldown'
+    | 'maxRowCount'
+    | 'fillOutside'
+    | 'radiusOutside'
+    | 'fillRectangleWidth'
+    | 'fillRectangleHeight'
   >(defaultProps),
 );

packages/charts/src/chart_types/heatmap/layout/viewmodel/viewmodel.ts
@@ -134,7 +134,7 @@ export function shapeViewModel(
 
   if (colorToRgba(background.color)[3] < 1) {
     Logger.expected(
-      `Text contrast requires a opaque background color, using white as fallback`,
+      'Text contrast requires a opaque background color, using fallbackColor',
       'an opaque color',
       background.color,
     );
@@ -185,7 +185,7 @@ export function shapeViewModel(
       visible: !isValueHidden(d.value, bandsToHide),
       formatted: formattedValue,
       fontSize,
-      textColor: fillTextColor(cellBackgroundColor, background.color),
+      textColor: fillTextColor(background.fallbackColor, cellBackgroundColor, background.color),
     };
     return acc;
   }, {});

packages/charts/src/index.ts
@@ -43,11 +43,8 @@ export {
 export { GeometryValue, BandedAccessorType } from './utils/geometry';
 export { LegendPath, LegendPathElement } from './state/actions/legend';
 export { CategoryKey } from './common/category';
-export {
-  Config as PartitionConfig,
-  FillLabelConfig as PartitionFillLabel,
-  PartitionLayout,
-} from './chart_types/partition_chart/layout/types/config_types';
+export { FillLabelConfig as PartitionFillLabel, PartitionStyle } from './utils/themes/partition';
+export { PartitionLayout } from './chart_types/partition_chart/layout/types/config_types';
 export { Layer as PartitionLayer } from './chart_types/partition_chart/specs/index';
 export * from './chart_types/goal_chart/specs/index';
 export * from './chart_types/wordcloud/specs/index';
@@ -106,7 +103,7 @@ export {
 export { DataGenerator } from './utils/data_generators/data_generator';
 export * from './utils/themes/merge_utils';
 export * from './utils/use_legend_action';
-export { MODEL_KEY } from './chart_types/partition_chart/layout/config';
+export { MODEL_KEY, defaultPartitionValueFormatter } from './chart_types/partition_chart/layout/config';
 export { LegendStrategy } from './chart_types/partition_chart/layout/utils/highlighted_geoms';
 export { Pixels, Ratio } from './common/geometry';
 export { AdditiveNumber } from './utils/accessor';

commit ce0c45e4ff2ca8952fc5fa474c5f18e0dfb66070

fix: animation regression introduced with 6db2677

packages/charts/src/chart_types/partition_chart/layout/types/config_types.ts
@@ -36,7 +36,7 @@ export interface AnimKeyframe {
 /** @public */
 export interface AnimationConfig {
   /** @alpha */
-  animation?: {
+  animation: {
     duration: TimeMs;
     keyframes?: Array<AnimKeyframe>;
   };

packages/charts/src/chart_types/partition_chart/layout/types/viewmodel_types.ts
@@ -208,6 +208,7 @@ export const nullShapeViewModel = (
     width: 0,
     height: 0,
   },
+  animation: { duration: 0 },
 });
 
 /** @internal */

packages/charts/src/chart_types/partition_chart/layout/viewmodel/viewmodel.ts
@@ -309,6 +309,7 @@ export function shapeViewModel(
     clockwiseSectors,
     maxRowCount,
     specialFirstInnermostSector,
+    animation,
   } = spec;
   const {
     emptySizeRatio,
@@ -511,6 +512,7 @@ export function shapeViewModel(
     pickQuads,
     outerRadius,
     chartDimensions,
+    animation,
   };
 }
 

packages/charts/src/chart_types/partition_chart/specs/index.ts
@@ -68,6 +68,7 @@ const defaultProps = {
   radiusOutside: 128,
   fillRectangleWidth: Infinity,
   fillRectangleHeight: Infinity,
+  animation: { duration: 0 },
 };
 
 /**
@@ -128,5 +129,6 @@ export const Partition: React.FunctionComponent<SpecRequiredProps & SpecOptional
     | 'radiusOutside'
     | 'fillRectangleWidth'
     | 'fillRectangleHeight'
+    | 'animation'
   >(defaultProps),
 );