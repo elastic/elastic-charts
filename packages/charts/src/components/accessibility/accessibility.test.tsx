197736265fe6fc2db33a3f88b4e5f289dc9228ae
rshen91 <rshen@elastic.co>
2022-01-24 13:18:36 -0700

    test: add unit tests

--- packages/charts/src/components/accessibility/accessibility.test.tsx
+++ packages/charts/src/components/accessibility/accessibility.test.tsx
@@ -16,8 +16,10 @@ import { PartitionLayout } from '../../chart_types/partition_chart/layout/types/
 import { arrayToLookup } from '../../common/color_calcs';
 import { mocks } from '../../mocks/hierarchical';
 import { productDimension } from '../../mocks/hierarchical/dimension_codes';
-import { BarSeries, LineSeries, Partition, Settings } from '../../specs';
-import { Datum } from '../../utils/common';
+import { ScaleType } from '../../scales/constants';
+import { AreaSeries, Axis, BarSeries, LineSeries, Partition, Settings } from '../../specs';
+import { Datum, Position } from '../../utils/common';
+import { KIBANA_METRICS } from '../../utils/data_samples/test_dataset_kibana';
 import { Chart } from '../chart';
 
 describe('Accessibility', () => {
@@ -176,5 +178,91 @@ describe('Accessibility', () => {
         '0 - 200freezing200 - 250chilly250 - 300brisk',
       );
     });
+  });
+
+  describe('screen reader table for cartesian charts', () => {
+    const XYChartSingleSeriesSmall = mount(
+      <Chart size={[100, 100]} id="chart1">
+        <Settings debug rendering="svg" showLegend />
+        <BarSeries id="test" data={[{ x: 0, y: 2 }]} xAccessor="x" yAccessors={['y']} />
+      </Chart>,
+    );
+    it('should have the data table in the markdown by default', () => {
+      expect(XYChartSingleSeriesSmall.find('.echScreenReaderTable').first().text()).toBeDefined();
+    });
+    it('should not have buttons for more data with small dataset less than 20', () => {
+      expect(XYChartSingleSeriesSmall.find('button')).toEqual({});
+    });
+
+    const XYChartSingleSeriesLarge = mount(
+      <Chart size={[100, 100]} id="chart1">
+        <Settings debug rendering="svg" showLegend />
+        <AreaSeries
+          id="area"
+          xScaleType={ScaleType.Time}
+          yScaleType={ScaleType.Linear}
+          xAccessor={0}
+          yAccessors={[1]}
+          data={KIBANA_METRICS.metrics.kibana_os_load[0].data}
+        />
+      </Chart>,
+    );
+    it('should have buttons for more data with small dataset less than 20', () => {
+      expect(XYChartSingleSeriesLarge.find('button')).toBeDefined();
+    });
+  });
+
+  const timeslipLargeExample = mount(
+    <Chart>
+      <Settings theme={{ axes: { tickLine: { visible: true } } }} debug xDomain={undefined} />
+      <Axis
+        id="x_minor"
+        position={Position.Top}
+        showOverlappingTicks={false}
+        showOverlappingLabels={false}
+        showGridLines={true}
+        style={{
+          axisLine: { stroke: 'rgba(96, 146, 192, 1)' },
+          tickLine: { size: 0.0001, padding: 4 },
+          tickLabel: {
+            ...{ fontSize: 11, fontFamily: 'Atkinson Hyperlegible' },
+            alignment: { horizontal: Position.Left, vertical: Position.Bottom },
+            padding: 0,
+            offset: { x: 0, y: 0 },
+          },
+          axisTitle: { fontFamily: 'Atkinson Hyperlegible', fill: 'rgb(112,112,112)', fontSize: 15 },
+        }}
+        tickFormat={(d: any) =>
+          new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          }).format(d)
+        }
+        labelFormat={(d: any) =>
+          `${new Intl.DateTimeFormat('en-US', { minute: 'numeric' }).format(d).padStart(2, '0')}′`
+        }
+        title="time (1-minute measurements)"
+        timeAxisLayerCount={3}
+      />
+      <AreaSeries
+        id="Utilization"
+        xScaleType={ScaleType.Time}
+        yScaleType={ScaleType.Linear}
+        xAccessor={0}
+        yAccessors={[1]}
+        yNice
+        color="rgba(96, 146, 192, 1)"
+        areaSeriesStyle={{ area: { opacity: 0.3 }, line: { opacity: 1 } }}
+        data={KIBANA_METRICS.metrics.kibana_os_load[0].data}
+      />
+    </Chart>,
+  );
+
+  it('should show date time formatting correctly if tickFormatter is present', () => {
+    // expect the third td showing the x values to be formatted as a date
+    expect(timeslipLargeExample.find('td').slice(2, 3).text()).toBe('March 1, 2019, 4:00 AM');
   });
 });
