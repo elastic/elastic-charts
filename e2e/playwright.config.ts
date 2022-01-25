f05d1acea53be2b764d43fe2d02c564f799261f7
nickofthyme <nicholas.partridge@elastic.co>
2022-01-24 22:39:32 -0600

    starting of testing playwright

---
+++ e2e/playwright.config.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  use: {
+    // headless: false,
+    locale: 'en-us',
+    viewport: { width: 800, height: 600 },
+    screenshot: 'off',
+    video: 'retain-on-failure',
+  },
+  // eslint-disable-next-line prettier/prettier
+  reporter: process.env.CI ? 'github' : [
+    ['html', { open: 'never', outputFolder: 'report' }],
+    ['list'],
+  ],
+  expect: {
+    toMatchSnapshot: { threshold: 0.1 },
+  },
+  preserveOutput: 'failures-only',
+  snapshotDir: 'screenshots',
+  testDir: 'tests',
+  outputDir: 'test-failures',
+  projects: [
+    {
+      name: 'Chrome',
+      use: {
+        browserName: 'chromium',
+      },
+    },
+    {
+      name: 'Firefox',
+      use: {
+        browserName: 'firefox',
+      },
+    },
+    {
+      name: 'Safari',
+      use: {
+        browserName: 'webkit',
+      },
+    },
+  ],
+};
+
+export default config;
---
+++ e2e/tests/foo.test.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { test, expect } from '@playwright/test';
+
+// eslint-disable-next-line jest/no-done-callback
+test('basic test', async ({ page }, info) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+
+  // console.log(info);
+  expect(await page.screenshot()).toMatchSnapshot([`${info.title}.png`]);
+});
