ec51c8765afddda27481be35725777f1fe3f909d
nickofthyme <nicholas.partridge@elastic.co>
2022-02-09 17:31:07 -0600

    fix(legend): width with duplicate pie slice labels

--- packages/charts/src/chart_types/partition_chart/layout/utils/legend_labels.ts
+++ packages/charts/src/chart_types/partition_chart/layout/utils/legend_labels.ts
@@ -8,18 +8,29 @@
 
 import { LegendItemLabel } from '../../../../state/selectors/get_legend_items_labels';
 import { Layer } from '../../specs';
-import { CHILDREN_KEY, HIERARCHY_ROOT_KEY, HierarchyOfArrays } from './group_by_rollup';
+import { CHILDREN_KEY, HIERARCHY_ROOT_KEY, HierarchyOfArrays, PATH_KEY, ArrayNode, NULL_SMALL_MULTIPLES_KEY } from './group_by_rollup';
 
 /** @internal */
 export function getLegendLabels(layers: Layer[], tree: HierarchyOfArrays, legendMaxDepth: number) {
+  console.log(flatSlicesNames(layers, 0, tree));
+
   return flatSlicesNames(layers, 0, tree).filter(({ depth }) => depth <= legendMaxDepth);
+}
+
+/** @internal */
+export function getArrayNodeKey(arrayNode: ArrayNode): string {
+  return arrayNode[PATH_KEY].reduce<string>((acc, { value, index }) => {
+    if (value === HIERARCHY_ROOT_KEY || value === NULL_SMALL_MULTIPLES_KEY) return acc;
+    return `${acc}(${index}):${value}__`;
+  }, '__');
 }
 
 function flatSlicesNames(
   layers: Layer[],
   depth: number,
   tree: HierarchyOfArrays,
-  keys: Map<string, number> = new Map(),
+  keys: Map<string, string> = new Map(),
+  depths: Map<string, number> = new Map(),
 ): LegendItemLabel[] {
   if (tree.length === 0) {
     return [];
@@ -32,15 +43,19 @@ function flatSlicesNames(
     const formattedValue = formatter ? formatter(key) : `${key}`;
     // preventing errors from external formatters
     if (formattedValue && formattedValue !== HIERARCHY_ROOT_KEY) {
+      // Node key must be unique for each node in the tree
+      const nodeKey = getArrayNodeKey(arrayNode);
       // save only the max depth, so we can compute the the max extension of the legend
-      keys.set(formattedValue, Math.max(depth, keys.get(formattedValue) ?? 0));
+      depths.set(nodeKey, Math.max(depth, depths.get(nodeKey) ?? 0));
+      keys.set(nodeKey, formattedValue);
     }
 
     const children = arrayNode[CHILDREN_KEY];
-    flatSlicesNames(layers, depth + 1, children, keys);
+    flatSlicesNames(layers, depth + 1, children, keys, depths);
   }
-  return [...keys.keys()].map((k) => ({
-    label: k,
-    depth: keys.get(k) ?? 0,
+
+  return [...depths.keys()].map((key) => ({
+    label: keys.get(key) ?? '',
+    depth: depths.get(key) ?? 0,
   }));
 }
