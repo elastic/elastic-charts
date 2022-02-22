4eb91963b121b8c302a4d4bcf3cb2488232d01b7
nickofthyme <nicholas.partridge@elastic.co>
2022-02-22 16:54:37 -0600

    fix: rename new test case story

--- storybook/stories/test_cases/test_cases.stories.tsx
+++ storybook/stories/test_cases/test_cases.stories.tsx
@@ -19,4 +19,4 @@ export { Example as accessibilityCustomizations } from './6_a11y_custom_descript
 export { Example as rtlText } from './7_rtl_text.story';
 export { Example as testPointsOutsideOfDomain } from './8_test_points_outside_of_domain.story';
 export { Example as heatmapAxisLabels } from './9_heatmap_axis_labels.story';
-export { Example as partitionLegendLabels } from './10_partition_legend_labels.story';
+export { Example as duplicateLabelsInPartitionLegend } from './10_duplicate_labels_in_partition_legend.story';
