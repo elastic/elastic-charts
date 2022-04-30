f4f3505f4cc0d5eb80ff66ba25573781d98a841a
Marco Vettorello <vettorello.marco@gmail.com>
2022-04-30 18:33:25 +0200

    update marginal scatterplot lnf

--- storybook/stories/aeroelastic/02_marginal_scatterplot.tsx
+++ storybook/stories/aeroelastic/02_marginal_scatterplot.tsx
@@ -9,17 +9,151 @@
 import { boolean } from '@storybook/addon-knobs';
 import React from 'react';
 
-import { Axis, Chart, BubbleSeries, Position, ScaleType, Settings, TooltipType, BarSeries } from '@elastic/charts';
+import {
+  Axis,
+  Chart,
+  BubbleSeries,
+  Position,
+  ScaleType,
+  Settings,
+  TooltipType,
+  BarSeries,
+  Heatmap,
+} from '@elastic/charts';
 import { Canvas } from '@elastic/charts/src/common/canvas_components/view_components';
 
 import { data } from './data';
 
-const yDomain = { min: 0, max: 600000 };
-const xDomain = { min: 0, max: 6000 };
+const yDomain = { min: 0, max: 400000 };
+const xDomain = { min: 0, max: 4000 };
+const filteredData = data.filter(([x, y]) => x < xDomain.max && y < yDomain.max);
+
+const colors = ['#6e40aa', '#5465d6', '#2f96e0', '#1ac7c2', '#28ea8d', '#60f760', '#aff05b'].map((d) => `${d}D0`);
 
 const charts = (buckets: number) => {
   return (
     <>
+      <Chart>
+        <Settings
+          theme={{
+            axes: {
+              axisLine: {
+                visible: false,
+              },
+            },
+            heatmap: {
+              cell: {
+                maxHeight: 'fill',
+                maxWidth: 'fill',
+
+                label: {
+                  visible: false,
+                },
+                border: {
+                  strokeWidth: 0.2,
+                  stroke: 'transparent',
+                },
+              },
+              grid: {
+                stroke: {
+                  width: 0,
+                  color: 'transparent',
+                },
+                cellHeight: {
+                  min: 0,
+                  max: 100,
+                },
+              },
+              yAxisLabel: {
+                visible: false,
+              },
+              xAxisLabel: {
+                visible: false,
+              },
+            },
+          }}
+          rotation={90}
+          xDomain={Array.from({ length: buckets }, (d, i) => (i * yDomain.max) / buckets).reverse()}
+        />
+
+        <Heatmap
+          id="# of apartments"
+          colorScale={{
+            type: 'bands',
+            bands: [
+              {
+                start: -Infinity,
+                end: 1,
+                color: 'transparent',
+              },
+              {
+                start: 1,
+                end: 10,
+                color: colors[0],
+              },
+              {
+                start: 10,
+                end: 20,
+                color: colors[1],
+              },
+              {
+                start: 20,
+                end: 30,
+                color: colors[2],
+              },
+              {
+                start: 30,
+                end: 40,
+                color: colors[3],
+              },
+              {
+                start: 40,
+                end: 50,
+                color: colors[4],
+              },
+              {
+                start: 50,
+                end: 60,
+                color: colors[5],
+              },
+              {
+                start: 60,
+                end: Infinity,
+                color: colors[6],
+              },
+            ],
+          }}
+          xAccessor={(d) => d[0]}
+          yAccessor={(d) => d[1]}
+          valueAccessor={(d) => d[2]}
+          xSortPredicate="numAsc"
+          ySortPredicate="numDesc"
+          data={[
+            ...filteredData
+              .reduce(
+                (acc, curr) => {
+                  const xBand = xDomain.max / buckets;
+                  const yBand = yDomain.max / buckets;
+                  const x = Math.floor(curr[0] / xBand) * xBand;
+                  const y = Math.floor(curr[1] / yBand) * yBand;
+                  const key = `${x}---${y}`;
+                  const existing = acc.get(key) ?? [x, y, 0];
+                  acc.set(key, [x, y, existing[2] + 1]);
+                  return acc;
+                },
+                new Map<string, [number, number, number]>(
+                  Array.from({ length: buckets * buckets }, (d, i) => {
+                    const x = (Math.floor(i / buckets) * xDomain.max) / buckets;
+                    const y = ((i % buckets) * yDomain.max) / buckets;
+                    return [`${x}--${y}`, [x, y, 0]];
+                  }),
+                ),
+              )
+              .values(),
+          ]}
+        />
+      </Chart>
+
       <div style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.03)' }}></div>
       <Chart>
         <Settings
@@ -33,6 +167,21 @@ const charts = (buckets: number) => {
           xDomain={xDomain}
         />
         <Axis
+          id="top"
+          position={Position.Top}
+          ticks={5}
+          style={{
+            tickLine: {
+              size: 5,
+              padding: 3,
+            },
+          }}
+          gridLine={{
+            visible: false,
+            stroke: 'black',
+          }}
+        />
+        <Axis
           id="bottom"
           position={Position.Bottom}
           title="Ground living area"
@@ -44,8 +193,8 @@ const charts = (buckets: number) => {
             },
           }}
           gridLine={{
-            visible: true,
-            stroke: '#eaeaea',
+            visible: false,
+            stroke: 'black',
           }}
         />
         <Axis
@@ -61,8 +210,26 @@ const charts = (buckets: number) => {
             },
           }}
           gridLine={{
-            visible: true,
-            stroke: '#eaeaea',
+            visible: false,
+            stroke: 'black',
+          }}
+          domain={yDomain}
+        />
+
+        <Axis
+          id="right"
+          position={Position.Right}
+          ticks={5}
+          tickFormat={(d) => `${d / 1000}`}
+          style={{
+            tickLine: {
+              size: 5,
+              padding: 3,
+            },
+          }}
+          gridLine={{
+            visible: false,
+            stroke: 'black',
           }}
           domain={yDomain}
         />
@@ -73,21 +240,23 @@ const charts = (buckets: number) => {
           yScaleType={ScaleType.Linear}
           xAccessor={0}
           yAccessors={[1]}
+          tickFormat={(d) => `${Math.round(d / 1000)}`}
           bubbleSeriesStyle={{
             point: {
               shape: 'circle',
-              fill: '__use__series__color__',
-              opacity: 0.1,
-              radius: 4,
+              fill: 'black',
+              opacity: 1,
+              radius: 1,
+              strokeWidth: 0,
             },
           }}
-          data={data}
+          data={filteredData}
         />
       </Chart>
       <Chart>
         <Settings
           theme={{
-            colors: { vizColors: ['#74a9cf'] },
+            colors: { vizColors: ['#69707d'] },
             chartMargins: {
               bottom: 0,
               left: 55,
@@ -98,18 +267,19 @@ const charts = (buckets: number) => {
               histogramPadding: 0.01,
             },
           }}
+          xDomain={Array.from({ length: buckets }, (d, i) => (i * xDomain.max) / buckets)}
         />
         <Axis id="x" position={Position.Bottom} tickFormat={(d) => `${d} - ${d + xDomain.max / buckets}`} hide />
         <BarSeries
           id="# of apartments"
-          xScaleType={ScaleType.Linear}
+          xScaleType={ScaleType.Ordinal}
           yScaleType={ScaleType.Linear}
           xAccessor={0}
           yAccessors={[1]}
-          minBarHeight={1}
+          minBarHeight={2}
           enableHistogramMode
           data={[
-            ...data
+            ...filteredData
               .reduce((acc, curr) => {
                 const band = xDomain.max / buckets;
                 const x = Math.floor(curr[0] / band) * band;
@@ -124,12 +294,12 @@ const charts = (buckets: number) => {
       <Chart>
         <Settings
           theme={{
-            colors: { vizColors: ['#74a9cf'] },
+            colors: { vizColors: ['#69707d'] },
             chartMargins: {
               bottom: 48,
               left: 0,
               right: 0,
-              top: 17,
+              top: 27,
             },
             scales: {
               histogramPadding: 0.01,
@@ -141,8 +311,17 @@ const charts = (buckets: number) => {
         <Axis
           id="x"
           position={Position.Left}
-          tickFormat={(d) => `${d / 1000}k$ - ${d / 1000 + yDomain.max / buckets}k$`}
-          hide
+          tickFormat={(d) => `${d / 1000}k$ - ${(d + yDomain.max / buckets) / 1000}k$`}
+          style={{
+            tickLine: {
+              size: 5,
+              padding: 3,
+            },
+            tickLabel: {
+              visible: false,
+            },
+          }}
+          ticks={5}
         />
         <BarSeries
           id="# of apartments"
@@ -153,7 +332,7 @@ const charts = (buckets: number) => {
           minBarHeight={1}
           enableHistogramMode
           data={[
-            ...data
+            ...filteredData
               .reduce((acc, curr) => {
                 const band = yDomain.max / buckets;
                 const x = Math.floor(curr[1] / band) * band;
@@ -171,13 +350,17 @@ const charts = (buckets: number) => {
 
 const chartDescriptors = [
   {
+    id: 'heatmap',
+    position: { left: 100 + 55, top: 179, width: 409, height: 408, angle: 0 },
+  },
+  {
     id: 'group_marginalScatter',
     group: true,
     position: { left: 100, top: 71, width: 580, height: 380, angle: 0, parent: null },
   },
   {
     id: 'bubbles',
-    position: { left: 100, top: 150, width: 500, height: 300, angle: 0, parent: 'group_marginalScatter' },
+    position: { left: 100, top: 150, width: 500, height: 485, angle: 0, parent: 'group_marginalScatter' },
   },
   {
     id: 'barsOnTop',
@@ -185,13 +368,12 @@ const chartDescriptors = [
   },
   {
     id: 'barsOnSide',
-    position: { left: 599, top: 150, width: 80, height: 300, angle: 0, parent: 'group_marginalScatter' },
+    position: { left: 599, top: 150, width: 80, height: 485, angle: 0, parent: 'group_marginalScatter' },
   },
 ];
 
 export const Example = () => {
-  // const buckets = number('buckets', 12, { min: 1, max: 48, step: 1, range: true });
-  return <Canvas charts={charts(24)} chartDescriptors={chartDescriptors}></Canvas>;
+  return <Canvas charts={charts(20)} chartDescriptors={chartDescriptors}></Canvas>;
 };
 
 Example.parameters = {
