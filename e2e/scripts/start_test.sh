64e8bded226cda16ae32df4327c33192901c5725
nickofthyme <nicholas.partridge@elastic.co>
2022-05-04 18:17:28 -0500

    test playwright install

--- e2e/scripts/start_test.sh
+++ e2e/scripts/start_test.sh
@@ -5,16 +5,19 @@ set -e
 ### starts up a playwright docker container to run e2e tests
 
 # Get correct playwright image - must match installed version of @playwright/test
-regex="@playwright/test@(.+)"
-result="$(yarn list --pattern "@playwright/test" --depth=0 | grep playwright/test)"
+# regex="@playwright/test@(.+)"
+# result="$(yarn list --pattern "@playwright/test" --depth=0 | grep playwright/test)"
 
-if [[ $result =~ $regex ]]; then
-  pw_version=${BASH_REMATCH[1]}
-  pw_image="mcr.microsoft.com/playwright:v${pw_version}-focal"
-else
-  echo "Unable to find '@playwright/test' version"
-  exit 1
-fi
+# if [[ $result =~ $regex ]]; then
+#   pw_version=${BASH_REMATCH[1]}
+#   pw_image="mcr.microsoft.com/playwright:v${pw_version}-focal"
+# else
+#   echo "Unable to find '@playwright/test' version"
+#   exit 1
+# fi
+
+pw_image="mcr.microsoft.com/playwright:v1.21.1-focal"
+
 
 # Run e2e playwright tests inside container
 docker run \
