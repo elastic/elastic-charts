83dba0f2361b96a66cbad68b4aa6ce683839fd8e
Robert Monfera <monfera.robert@gmail.com>
2022-05-29 17:37:32 +0200

    refactor: extract out non-domain specific function

--- packages/charts/src/chart_types/flame_chart/render/ensure_webgl.ts
+++ packages/charts/src/chart_types/flame_chart/render/ensure_webgl.ts
@@ -18,6 +18,32 @@ import { GL } from '../../../common/webgl_constants';
 import { attributeLocations, colorFrag, roundedRectVert, roundedRectFrag, simpleRectVert } from '../shaders';
 import { GLResources, NULL_GL_RESOURCES } from '../types';
 
+function blockUniforms(
+  gl: WebGL2RenderingContext,
+  uboVariableNames: string[],
+  [program, ...otherPrograms]: WebGLProgram[],
+) {
+  const blockIndex = gl.getUniformBlockIndex(program, 'Settings');
+  const blockSize = gl.getActiveUniformBlockParameter(program, blockIndex, GL.UNIFORM_BLOCK_DATA_SIZE);
+  const uboBuffer = gl.createBuffer();
+  if (uboBuffer === null) throw new Error('Whoa, could not create uboBuffer');
+  gl.bindBuffer(gl.UNIFORM_BUFFER, uboBuffer);
+  gl.bufferData(gl.UNIFORM_BUFFER, blockSize, gl.DYNAMIC_DRAW);
+  // gl.bindBuffer(gl.UNIFORM_BUFFER, null);
+  gl.bindBufferBase(gl.UNIFORM_BUFFER, 0, uboBuffer);
+  const uboVariableIndices = gl.getUniformIndices(program, uboVariableNames);
+  if (uboVariableIndices === null) throw new Error('Whoa, could not get uboVariableIndices');
+  const uboVariableOffsets = gl.getActiveUniforms(program, uboVariableIndices, gl.UNIFORM_OFFSET);
+  const uniforms = new Map(
+    uboVariableNames.map((name, i) => [name, { index: uboVariableIndices[i], offset: uboVariableOffsets[i] }]),
+  );
+
+  // per program part
+  [program, ...otherPrograms].forEach((p) => gl.uniformBlockBinding(p, gl.getUniformBlockIndex(p, 'Settings'), 0));
+
+  return { uboBuffer, uniforms };
+}
+
 /** @internal */
 export function ensureWebgl(
   gl: WebGL2RenderingContext,
@@ -59,16 +85,6 @@ export function ensureWebgl(
    * Uniform blocks
    */
 
-  // common part
-  const program = geomProgram;
-  const blockIndex = gl.getUniformBlockIndex(program, 'Settings');
-  const blockSize = gl.getActiveUniformBlockParameter(program, blockIndex, GL.UNIFORM_BLOCK_DATA_SIZE);
-  const uboBuffer = gl.createBuffer();
-  if (uboBuffer === null) throw new Error('Whoa, could not create uboBuffer');
-  gl.bindBuffer(gl.UNIFORM_BUFFER, uboBuffer);
-  gl.bufferData(gl.UNIFORM_BUFFER, blockSize, gl.DYNAMIC_DRAW);
-  // gl.bindBuffer(gl.UNIFORM_BUFFER, null);
-  gl.bindBufferBase(gl.UNIFORM_BUFFER, 0, uboBuffer);
   const uboVariableNames = [
     'focus',
     'resolution',
@@ -81,16 +97,8 @@ export function ensureWebgl(
     'hoverIndex',
     'pickLayer',
   ];
-  const uboVariableIndices = gl.getUniformIndices(program, uboVariableNames);
-  if (uboVariableIndices === null) throw new Error('Whoa, could not get uboVariableIndices');
-  const uboVariableOffsets = gl.getActiveUniforms(program, uboVariableIndices, gl.UNIFORM_OFFSET);
-  const uniforms = new Map(
-    uboVariableNames.map((name, i) => [name, { index: uboVariableIndices[i], offset: uboVariableOffsets[i] }]),
-  );
 
-  // per program part
-  gl.uniformBlockBinding(geomProgram, gl.getUniformBlockIndex(geomProgram, 'Settings'), 0);
-  gl.uniformBlockBinding(pickProgram, gl.getUniformBlockIndex(pickProgram, 'Settings'), 0);
+  const { uboBuffer, uniforms } = blockUniforms(gl, uboVariableNames, [geomProgram, pickProgram]);
 
   /**
    * Resource allocation: Render setup
