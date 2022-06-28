613cdec70c477677acf57f043d05614d773712d8
nickofthyme <nicholas.partridge@elastic.co>
2022-06-28 09:22:34 -0700

    chore: add closed logic to gh bot

---
+++ github_bot/src/github/events/pull_request/cleanup.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { Probot } from 'probot';
+
+import { buildkiteClient } from '../../../utils/buildkite';
+import { updateLastDeployment } from '../../../utils/github_utils/deployments';
+
+/**
+ * Cleanups prs after closure. Includes:
+ *
+ * - cancels running buildkite builds
+ * - sets GitHub deployment to inactive
+ *
+ * TODOs
+ * - deletes firebase deployment (auto expires after 7 days)
+ */
+export function setupBuildTrigger(app: Probot) {
+  app.on(['pull_request.closed'], async (ctx) => {
+    console.log(`------- Triggered probot ${ctx.name} | ${ctx.payload.action}`);
+
+    const { head } = ctx.payload.pull_request;
+
+    await buildkiteClient.cancelRunningBuilds(head.sha);
+
+    await updateLastDeployment(ctx);
+  });
+}
---
+++ github_bot/src/utils/github_utils/deployments.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { RestEndpointMethodTypes } from '@octokit/rest';
+
+import { ProbotEventContext } from '../../github/types';
+
+export async function updateLastDeployment(
+  ctx: ProbotEventContext<'pull_request'>,
+  state: RestEndpointMethodTypes['repos']['createDeploymentStatus']['parameters']['state'] = 'inactive',
+) {
+  const {
+    data: [deployment],
+  } = await ctx.octokit.repos.listDeployments({
+    ...ctx.repo(),
+    task: `deploy:pr:${ctx.payload.pull_request.number}`,
+    per_page: 1,
+  });
+
+  if (!deployment) return;
+
+  const {
+    data: [status],
+  } = await ctx.octokit.repos.listDeploymentStatuses({
+    ...ctx.repo(),
+    deployment_id: deployment.id,
+    per_page: 1,
+  });
+
+  if (!status) return;
+  const { state: currentState, log_url, environment_url } = status;
+  if ('success' !== currentState) return;
+
+  console.log(`Updating deployment ${deployment.id} state: ${currentState} -> ${state}`);
+
+  await ctx.octokit.repos.createDeploymentStatus({
+    ...ctx.repo(),
+    deployment_id: deployment.id,
+    description: 'Deployment destroyed after PR closed',
+    log_url,
+    environment_url,
+    state,
+  });
+}
