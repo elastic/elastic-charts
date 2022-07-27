f58b6f29b45495d13b2df7cdf369ffa5e9507b2a
Marco Vettorello <vettorello.marco@gmail.com>
2022-07-27 15:07:59 +0200

    fix: remove duplicated copyright

--- packages/charts/src/chart_types/flame_chart/flame_chart.tsx
+++ packages/charts/src/chart_types/flame_chart/flame_chart.tsx
@@ -6,14 +6,6 @@
  * Side Public License, v 1.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
-
 import React, { createRef, CSSProperties, KeyboardEvent, MouseEvent, RefObject, WheelEventHandler } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
