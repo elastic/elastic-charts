7f8dfb8370ccf1ee818b19e05b13cc51b43e5026
nickofthyme <nicholas.partridge@elastic.co>
2022-08-30 12:41:28 -0700

    ci: add retry logic to exec processes

--- .buildkite/utils/exec.ts
+++ .buildkite/utils/exec.ts
@@ -18,7 +18,21 @@ interface ExecOptions extends ExecSyncOptionsWithBufferEncoding {
   onFailure?: () => Promise<void> | void;
   onSuccess?: () => Promise<void> | void;
   cwd?: string;
+  /**
+   * Buildkite has network issues that require retrying
+   */
+  retry?: number;
+  /**
+   * Seconds to wait between retries
+   */
+  retryWait?: number;
+  /**
+   * Logic to run before next retry
+   */
+  onRetry?: () => void | Promise<void>;
 }
+
+export const wait = (n: number) => new Promise((resolve) => setTimeout(resolve, n));
 
 /**
  * Wrapper for execSync to catch and handle errors.
@@ -26,42 +40,64 @@ interface ExecOptions extends ExecSyncOptionsWithBufferEncoding {
  */
 export const exec = async (
   command: string,
-  { input, cwd, failureMsg, onFailure, onSuccess, env, stdio = ['pipe', 'inherit', 'inherit'] }: ExecOptions = {},
-) => {
-  try {
-    const result = execSync(command, {
-      input,
-      encoding: 'utf8',
-      cwd: cwd && path.isAbsolute(cwd) ? cwd : path.resolve(__dirname, '../../', cwd ?? ''),
-      stdio,
-      env: {
-        ...process.env,
-        ...env,
-      },
-    });
+  {
+    input,
+    cwd,
+    failureMsg,
+    onFailure,
+    onSuccess,
+    env,
+    stdio = ['pipe', 'inherit', 'inherit'],
+    retry = 0,
+    retryWait = 0,
+    onRetry,
+  }: ExecOptions = {},
+): Promise<string> => {
+  let retryCount = 0;
+  async function execInner(): Promise<string> {
+    try {
+      const result = execSync(command, {
+        input,
+        encoding: 'utf8',
+        cwd: cwd && path.isAbsolute(cwd) ? cwd : path.resolve(__dirname, '../../', cwd ?? ''),
+        stdio,
+        env: {
+          ...process.env,
+          ...env,
+        },
+      });
 
-    await onSuccess?.();
+      await onSuccess?.();
 
-    return result;
-  } catch (error) {
-    console.error(`Failed to run command: [${command}]`);
-    await setJobMetadata('failed', 'true');
-    await onFailure?.();
-    await updateCheckStatus(
-      {
-        status: 'completed',
-        conclusion: 'failure',
-      },
-      undefined,
-      failureMsg,
-    );
+      return result;
+    } catch (error) {
+      if (retryCount < retry) {
+        retryCount++;
+        console.log(`⚠️  Failed to run command: [${command}], attempting retry ${retryCount} of ${retry}`);
+        if (onRetry) await onRetry();
+        if (retryWait) await wait(retryWait * 1000);
+        return await execInner();
+      }
+      console.error(`❌ Failed to run command: [${command}]`);
+      await setJobMetadata('failed', 'true');
+      await onFailure?.();
+      await updateCheckStatus(
+        {
+          status: 'completed',
+          conclusion: 'failure',
+        },
+        undefined,
+        failureMsg,
+      );
 
-    throw error;
+      throw error;
+    }
   }
+  return await execInner();
 };
 
 export const yarnInstall = async (cwd?: string, ignoreScripts = true) => {
   startGroup(`Installing node modules${cwd ? ` [${cwd}]` : ''}`);
   const scriptFlag = ignoreScripts ? ' --ignore-scripts' : '';
-  await exec(`yarn install --frozen-lockfile${scriptFlag}`, { cwd });
+  await exec(`yarn install --frozen-lockfile${scriptFlag}`, { cwd, retry: 5, retryWait: 15 });
 };
