5e618a12d80a04b7d301ca195fe75441e7f47176
Marco Vettorello <vettorello.marco@gmail.com>
2022-10-11 16:37:04 +0200

    feat: expose all hierarchical information to the partition tooltip

--- packages/charts/src/chart_types/partition_chart/layout/viewmodel/picked_shapes.ts
+++ packages/charts/src/chart_types/partition_chart/layout/viewmodel/picked_shapes.ts
@@ -6,12 +6,26 @@
  * Side Public License, v 1.
  */
 
-import { LayerValue } from '../../../../specs';
+import { TooltipInfo } from '../../../../components/tooltip';
+import { LayerValue, TooltipValue } from '../../../../specs';
+import { LabelAccessor, ValueFormatter } from '../../../../utils/common';
+import { SpecId } from '../../../../utils/ids';
 import { Point } from '../../../../utils/point';
 import { ContinuousDomainFocus } from '../../renderer/canvas/partition';
-import { MODEL_KEY } from '../config';
-import { QuadViewModel, ShapeViewModel } from '../types/viewmodel_types';
-import { AGGREGATE_KEY, DEPTH_KEY, getNodeName, PARENT_KEY, PATH_KEY, SORT_INDEX_KEY } from '../utils/group_by_rollup';
+import { MODEL_KEY, percentValueGetter } from '../config';
+import { QuadViewModel, ShapeViewModel, ValueGetter } from '../types/viewmodel_types';
+import {
+  AGGREGATE_KEY,
+  ArrayNode,
+  CHILDREN_KEY,
+  DEPTH_KEY,
+  entryValue,
+  getNodeName,
+  PARENT_KEY,
+  PATH_KEY,
+  SORT_INDEX_KEY,
+} from '../utils/group_by_rollup';
+import { isLinear } from './viewmodel';
 
 /** @internal */
 export const pickedShapes = (
@@ -54,4 +68,81 @@ export function pickShapesLayerValues(shapes: QuadViewModel[]): LayerValue[][] {
       }
       return values.reverse();
     });
+}
+
+/** @internal */
+export function pickShapesTooltipValues(
+  shapes: QuadViewModel[],
+  shapeViewModel: ShapeViewModel[],
+  valueGetter: ValueGetter,
+  valueFormatter: ValueFormatter,
+  percentFormatter: ValueFormatter,
+  id: SpecId,
+): TooltipInfo {
+  const maxDepth = shapes.reduce((acc, curr) => Math.max(acc, curr.depth), 0);
+  const currentShapeViewModel = shapeViewModel.find((d) => d.smAccessorValue === shapes[0]?.smAccessorValue);
+  if (!currentShapeViewModel) {
+    return { values: [], header: null };
+  }
+  const labelFormatters = currentShapeViewModel.layers.map((d) => d.nodeLabel);
+
+  return {
+    header: null,
+    values: shapes
+      .filter(({ depth }) => depth === maxDepth) // eg. lowest layer in a treemap, where layers overlap in screen space; doesn't apply to sunburst/flame
+      .flatMap<TooltipValue>((viewModel) => {
+        const values: TooltipValue[] = [
+          getTooltipValueFromNode(
+            entryValue(viewModel[PARENT_KEY][CHILDREN_KEY][viewModel[SORT_INDEX_KEY]]),
+            labelFormatters,
+            valueFormatter,
+            percentFormatter,
+            currentShapeViewModel,
+            id,
+          ),
+        ];
+        // return only the leaf node if flame/icicle to avoid its highly hierarchical nature
+        if (isLinear(currentShapeViewModel.layout)) {
+          return values;
+        }
+        let node = viewModel[MODEL_KEY];
+        while (node[DEPTH_KEY] > 0) {
+          values.push(
+            getTooltipValueFromNode(node, labelFormatters, valueFormatter, percentFormatter, currentShapeViewModel, id),
+          );
+          node = node[PARENT_KEY];
+        }
+        return values.reverse();
+      }),
+  };
+}
+
+function getTooltipValueFromNode(
+  node: ArrayNode,
+  labelFormatters: (LabelAccessor | undefined)[],
+  valueFormatter: ValueFormatter,
+  percentFormatter: ValueFormatter,
+  shapeViewModel: ShapeViewModel,
+  id: SpecId,
+): TooltipValue {
+  const depth = node[DEPTH_KEY];
+  const value = node[AGGREGATE_KEY];
+  const dataName = getNodeName(node);
+  const formatter = labelFormatters[depth - 1];
+  const model = shapeViewModel.quadViewModel.find(
+    (d) => d.depth === depth && d.dataName === dataName && d.value === value,
+  );
+  return {
+    label: formatter ? formatter(dataName) : dataName,
+    color: model?.fillColor ?? 'transparent',
+    isHighlighted: false,
+    isVisible: true,
+    seriesIdentifier: {
+      specId: id,
+      key: id,
+    },
+    value: node[AGGREGATE_KEY],
+    formattedValue: `${valueFormatter(value)} (${percentFormatter(percentValueGetter(node))})`,
+    valueAccessor: node[DEPTH_KEY],
+  };
 }
