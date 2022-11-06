bc19ee98f1bc1b2c8802fc0d4a87f8606b37a0e6
Robert Monfera <monfera.robert@gmail.com>
2022-11-06 11:43:42 +0100

    feat: flame api doc change

--- packages/charts/src/chart_types/flame_chart/flame_api.ts
+++ packages/charts/src/chart_types/flame_chart/flame_api.ts
@@ -41,10 +41,11 @@ export interface ControlReceiverCallbacks {
  *   - label: array of N strings
  *   - value: Float64Array of N numbers, for tooltip value display
  *   - color: Float32Array of 4 * N numbers, eg. green[i] = color[4 * i + 1]
- *   - position0: Float32Array of 2 * N numbers with unit coordinates [x0, y0, x1, y1, ..., xN-1, yN-1]
- *   - position1: for now, the same typed array for position0 to be used
- *   - size0: Float32Array of N numbers with unit widths [width0, width1, ... , widthN-1]
- *   - size1: for now, the same typed array for size0 to be used
+ *   - position0: Tween from: Float32Array of 2 * N numbers with unit coordinates [x0, y0, x1, y1, ..., xN-1, yN-1]
+ *   - position1: Tween to: Float32Array of 2 * N numbers with unit coordinates [x0, y0, x1, y1, ..., xN-1, yN-1]
+ *   - size0: Tween from: Float32Array of N numbers with unit widths [width0, width1, ... , widthN-1]
+ *   - size1: Tween to: Float32Array of N numbers with unit widths [width0, width1, ... , widthN-1]
+ * If position0 === position1 and size0 === size1, then the nodes are not animated
  * @public
  */
 export interface ColumnarViewModel {
