f70676aa09c80aa3710d819ee4a239ae61618334
Marco Vettorello <vettorello.marco@gmail.com>
2022-12-19 17:26:02 +0100

    build: release on main branch (#1907)

--- .releaserc.js
+++ .releaserc.js
@@ -10,6 +10,14 @@ const isDryRun = process.argv.includes('--dry-run');
  */
 
 module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
   plugins: [
     [
       '@semantic-release/commit-analyzer',
