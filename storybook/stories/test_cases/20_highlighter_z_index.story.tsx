8575fb30dbca6176978530387fbaa6b4e7c14b7f
Marco Vettorello <vettorello.marco@gmail.com>
2023-02-01 15:32:02 +0100

    test: add VRTs of specific test failures (#1922)
    Add two test cases to catch the failures of #1920 and #1921. When those bugs are fixed the changes should be reflected where.

---
+++ storybook/stories/test_cases/20_highlighter_z_index.story.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+
+import { Axis, Chart, LineSeries, Position, ScaleType, Settings } from '@elastic/charts';
+
+import { useBaseTheme } from '../../use_base_theme';
+
+export const Example = () => {
+  return (
+    <Chart>
+      <Settings baseTheme={useBaseTheme()} showLegend />
+      <Axis id="bottom" position={Position.Bottom} />
+      <Axis id="left" position={Position.Left} />
+
+      <LineSeries
+        id="Sensor 1"
+        xScaleType={ScaleType.Linear}
+        yScaleType={ScaleType.Linear}
+        xAccessor={0}
+        yAccessors={[1]}
+        data={[
+          [0, 1],
+          [1, 0.8],
+          [2, 0.5],
+          [3, 0.12],
+        ]}
+      />
+      <LineSeries
+        id="Sensor 2"
+        xScaleType={ScaleType.Linear}
+        yScaleType={ScaleType.Linear}
+        xAccessor={0}
+        yAccessors={[1]}
+        data={[
+          [0, 1],
+          [1, 0.8],
+          [2, 0.76],
+          [3, 0.12],
+        ]}
+      />
+    </Chart>
+  );
+};
+
+Example.parameters = {
+  markdown: 'Currently not correctly rendered due to [#1921](https://github.com/elastic/elastic-charts/issues/1921)',
+};
