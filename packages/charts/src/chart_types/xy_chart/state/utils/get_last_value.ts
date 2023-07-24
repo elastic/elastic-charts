613b7ee0478fa20b22e06a3df7dd98bbd24ca422
Marco Vettorello <vettorello.marco@gmail.com>
2023-07-24 14:28:08 +0200

    fix: skip last value calculation on ordinal x scale

--- packages/charts/src/chart_types/xy_chart/state/utils/get_last_value.ts
+++ packages/charts/src/chart_types/xy_chart/state/utils/get_last_value.ts
@@ -12,13 +12,23 @@ import { XDomain } from '../../domains/types';
 import { isDatumFilled } from '../../rendering/utils';
 import { DataSeries, getSeriesKey, XYChartSeriesIdentifier } from '../../utils/series';
 import { StackMode } from '../../utils/specs';
+import { ScaleType } from '../../../../scales/constants';
 
 /**
  * @internal
  * @param dataSeries
  * @param xDomain
  */
-export function getLastValues(dataSeries: DataSeries[], xDomain: XDomain): Map<SeriesKey, LastValues> {
+export function getLastValues(dataSeries: DataSeries[],
+   xDomain: XDomain): Map<SeriesKey, LastValues> {
+  // 24/05/2023 A decision was made by the Kibana Visualization Team (MarcoV, StratoulaK)
+  // to disable representing `current` hovered values if the X scale is Ordinal. at Elastic this feature wasn't used
+  // and the the information was redundant because it was alredy available in the tooltip.
+  // A possible enhancement will probably update this configuration to allow `current` values if explicitly configured.
+  // See https://github.com/elastic/elastic-charts/issues/2050
+  if (xDomain.type === ScaleType.Ordinal) {
+    return new Map();
+  }
   const lastValues = new Map<SeriesKey, LastValues>();
 
   // we need to get the latest
