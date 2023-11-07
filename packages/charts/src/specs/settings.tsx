be30c1bd48ce3e50b8e9fa28c0d8999b6366b305
Nick Partridge <nicholas.partridge@elastic.co>
2023-11-07 08:29:05 -0700

    fix: `onRenderChange` callback trigger on resize (#2228)

--- packages/charts/src/specs/settings.tsx
+++ packages/charts/src/specs/settings.tsx
@@ -206,12 +206,22 @@ export type LegendItemListener = (series: SeriesIdentifier[]) => void;
  */
 export type PointerUpdateListener = (event: PointerEvent) => void;
 /**
+ * Listener to be called when chart resizes
+ * @alpha
+ */
+export type ResizeListener = () => void;
+/**
  * Listener to be called when chart render state changes
  *
  * `isRendered` value is `true` when rendering is complete and `false` otherwise
  * @public
  */
 export type RenderChangeListener = (isRendered: boolean) => void;
+/**
+ * Listener to be called *before* chart renders
+ * @public
+ */
+export type WillRenderListener = () => void;
 /** @public */
 export type BasicListener = () => undefined | void;
 /** @public */
@@ -510,7 +520,12 @@ export interface SettingsSpec extends Spec, LegendSpec {
   onElementOut?: BasicListener;
   onBrushEnd?: BrushEndListener;
   onPointerUpdate?: PointerUpdateListener;
+  /**
+   * @alpha subject to be removed in the future
+   */
+  onResize?: ResizeListener;
   onRenderChange?: RenderChangeListener;
+  onWillRender?: WillRenderListener;
   onProjectionAreaChange?: ProjectionAreaChangeListener;
 
   /**
