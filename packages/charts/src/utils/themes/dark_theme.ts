afdef1c0142d954dd72a5833d6b9f0abce064f4a
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2023-11-29 11:32:10 +0100

    feat: increase tooltip width to 500px and truncate items to 2 lines (#2261)
    feat: truncate to 2 + width 500

--- packages/charts/src/utils/themes/dark_theme.ts
+++ packages/charts/src/utils/themes/dark_theme.ts
@@ -416,7 +416,7 @@ export const DARK_THEME: Theme = {
   },
   bulletGraph: DARK_THEME_BULLET_STYLE,
   tooltip: {
-    maxWidth: 260,
+    maxWidth: 500,
     maxTableHeight: 120,
     defaultDotColor: Colors.White.keyword,
   },
