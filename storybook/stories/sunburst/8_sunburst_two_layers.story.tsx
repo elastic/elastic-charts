a1185ba9798780d49951e851a25882ba932141da
Marco Vettorello <vettorello.marco@gmail.com>
2023-12-06 18:15:23 +0100

    remove resizable from story [update vrts]

--- storybook/stories/sunburst/8_sunburst_two_layers.story.tsx
+++ storybook/stories/sunburst/8_sunburst_two_layers.story.tsx
@@ -46,58 +46,46 @@ const theme: PartialTheme = {
 export const Example: ChartsStory = (_, { title, description }) => {
   const showDebug = boolean('show table for debugging', false);
   return (
-    <div
-      style={{
-        maxWidth: 600,
-        maxHeight: 600,
-        resize: 'both',
-        width: '100%',
-        height: '100%',
-        padding: '0px',
-        overflow: 'auto',
-      }}
-    >
-      <Chart title={title} description={description}>
-        <Settings showLegend legendMaxDepth={1} theme={theme} baseTheme={useBaseTheme()} debug={showDebug} />
-        <Partition
-          id="spec_1"
-          data={mocks.sunburst}
-          layout={PartitionLayout.sunburst}
-          valueAccessor={(d: Datum) => d.exportVal as number}
-          valueFormatter={(d: number) => `$${defaultPartitionValueFormatter(Math.round(d / 1000000000))}\u00A0Bn`}
-          layers={[
-            {
-              groupByRollup: (d: Datum) => countryLookup[d.dest].continentCountry.slice(0, 2),
-              nodeLabel: (d: any) => regionLookup[d].regionName,
-              fillLabel: {
-                fontFamily: 'Impact',
-                valueFormatter: (d: number) =>
-                  `$${defaultPartitionValueFormatter(Math.round(d / 1000000000000))}\u00A0Tn`,
-              },
-              shape: {
-                fillColor: (key, sortIndex, node) => {
-                  // concat all leaf and define the color based on the index of the fist children
-                  const rootTree = node.parent.children.flatMap((d) => entryValue(d).children);
-                  const index = rootTree.findIndex((d) => entryValue(d) === entryValue(node.children[0]));
-                  return indexInterpolatedFillColor(interpolatorCET2s(0.8))(null, index, rootTree);
-                },
+    <Chart title={title} description={description}>
+      <Settings showLegend legendMaxDepth={1} theme={theme} baseTheme={useBaseTheme()} debug={showDebug} />
+      <Partition
+        id="spec_1"
+        data={mocks.sunburst}
+        layout={PartitionLayout.sunburst}
+        valueAccessor={(d: Datum) => d.exportVal as number}
+        valueFormatter={(d: number) => `$${defaultPartitionValueFormatter(Math.round(d / 1000000000))}\u00A0Bn`}
+        layers={[
+          {
+            groupByRollup: (d: Datum) => countryLookup[d.dest].continentCountry.slice(0, 2),
+            nodeLabel: (d: any) => regionLookup[d].regionName,
+            fillLabel: {
+              fontFamily: 'Impact',
+              valueFormatter: (d: number) =>
+                `$${defaultPartitionValueFormatter(Math.round(d / 1000000000000))}\u00A0Tn`,
+            },
+            shape: {
+              fillColor: (key, sortIndex, node) => {
+                // concat all leaf and define the color based on the index of the fist children
+                const rootTree = node.parent.children.flatMap((d) => entryValue(d).children);
+                const index = rootTree.findIndex((d) => entryValue(d) === entryValue(node.children[0]));
+                return indexInterpolatedFillColor(interpolatorCET2s(0.8))(null, index, rootTree);
               },
             },
-            {
-              groupByRollup: (d: Datum) => d.dest,
-              nodeLabel: (d: any) => countryLookup[d].name,
-              shape: {
-                fillColor: (key, sortIndex, node) => {
-                  // concat all leaf and define the color based on their index
-                  const rootTree = node.parent.parent.children.flatMap((d) => entryValue(d).children);
-                  const index = rootTree.findIndex((d) => entryValue(d) === node);
-                  return indexInterpolatedFillColor(interpolatorCET2s(0.8))(null, index, rootTree);
-                },
+          },
+          {
+            groupByRollup: (d: Datum) => d.dest,
+            nodeLabel: (d: any) => countryLookup[d].name,
+            shape: {
+              fillColor: (key, sortIndex, node) => {
+                // concat all leaf and define the color based on their index
+                const rootTree = node.parent.parent.children.flatMap((d) => entryValue(d).children);
+                const index = rootTree.findIndex((d) => entryValue(d) === node);
+                return indexInterpolatedFillColor(interpolatorCET2s(0.8))(null, index, rootTree);
               },
             },
-          ]}
-        />
-      </Chart>
-    </div>
+          },
+        ]}
+      />
+    </Chart>
   );
 };
