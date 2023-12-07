465a6bc52ae9245d1d6e25eed8e46752bc312d67
Marco Vettorello <vettorello.marco@gmail.com>
2023-12-07 11:40:15 +0100

    style(tooltip): increase width to 500px and 2 lines items (#2261) (#2275)
    Backport of #2261

--- packages/charts/src/utils/themes/light_theme.ts
+++ packages/charts/src/utils/themes/light_theme.ts
@@ -414,7 +414,7 @@ export const LIGHT_THEME: Theme = {
     minHeight: 64,
   },
   tooltip: {
-    maxWidth: 260,
+    maxWidth: 500,
     maxTableHeight: 120,
     defaultDotColor: Colors.Black.keyword,
   },
