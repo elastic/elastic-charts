9f9944734c4a13bfe9e4ffc9f4c0f39da5f9931f
Marco Vettorello <vettorello.marco@gmail.com>
2024-01-09 10:23:14 +0100

    fix(legend): improve last value handling (#2115)
    This commit cleans up the concept of what the value, shown after the legend item label, represents. We removed every concept of bucket/time interval from this calculation to clarify the legend item value concept.
    This allows us to decouple our code from Elasticsearch or other data sources.
    A small, but fixable side-effect is: the last value depends entirely on the passed data, so if you pass just 10 values of a series that should cover 100 data points, we consider the last value the last of these 10 values: if the 10th value is not at the extreme edge of your data domain we still consider that 10th value as the last one. If you instead, are looking to depict the extreme edge of your data domain (the 100th element) that in the mentioned example is empty/null then you should pass all the 100 data points with null values where needed.
    
    BREAKING CHANGE:  In cartesian charts, the default legend value now represents the data points that coincide with the latest datum in the X domain. Please consider passing every data point, even the empty ones (like empty buckets/bins/etc) if your x data domain doesn't fully cover a custom x domain passed to the chart configuration.

---
+++ storybook/stories/test_cases/21_domain_edges.story.tsx
@@ -0,0 +1,222 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { boolean, number } from '@storybook/addon-knobs';
+import React from 'react';
+
+import {
+  LineSeries,
+  Axis,
+  BarSeries,
+  Chart,
+  ScaleType,
+  Settings,
+  Position,
+  HistogramBarSeries,
+  niceTimeFormatter,
+} from '@elastic/charts';
+import { getRandomNumberGenerator } from '@elastic/charts/src/mocks/utils';
+
+import { ChartsStory } from '../../types';
+import { useBaseTheme } from '../../use_base_theme';
+
+const rng = getRandomNumberGenerator('chart');
+const start = Date.parse('2023-07-01T00:00:00.000Z').valueOf();
+const interval = 1000 * 60 * 5;
+const data = Array.from({ length: 20 }, (d, i) => {
+  return [start + interval * i, Math.floor(rng(2, 10))];
+});
+
+export const Example: ChartsStory = (_, { title, description }) => {
+  const customDomain = boolean('custom domain', false);
+  const startTimeSlider = number('start time', 0, { min: 0, max: 20, range: true, step: 0.2 });
+  const rangeSlider = number('end time', 19, { min: 1, max: 25, range: true, step: 0.2 });
+  const subtract = boolean('subtract 1ms', false);
+
+  const tickFormat = (d: number) => new Date(d).toISOString();
+  const xDomain = customDomain
+    ? { min: start + interval * startTimeSlider, max: start + interval * rangeSlider - (subtract ? 1 : 0) }
+    : undefined;
+  const domain: [number, number] = [xDomain?.min ?? data[0][0] ?? 0, xDomain?.max ?? data.at(-1)?.[0] ?? 0];
+  const limitedData = data
+    .filter((d) => d[0] <= (xDomain?.max ?? Infinity))
+    .map((d, i) => [d[0], i === 10 ? null : d[1]]);
+
+  return (
+    <>
+      <p>
+        {xDomain ? 'configured' : 'data'} domain: {new Date(domain[0]).toISOString()} to{' '}
+        {new Date(domain[1]).toISOString()}
+      </p>
+      <Chart title={title} description={description} size={['100%', 250]}>
+        <Settings
+          baseTheme={useBaseTheme()}
+          showLegend
+          legendPosition={{
+            floatingColumns: 1,
+            floating: true,
+            direction: 'vertical',
+            hAlign: 'right',
+            vAlign: 'top',
+          }}
+          showLegendExtra
+          xDomain={xDomain}
+          theme={{
+            scales: {
+              histogramPadding: 0,
+              barsPadding: 0,
+            },
+            barSeriesStyle: {
+              rectBorder: {
+                visible: true,
+                strokeWidth: 0,
+                stroke: 'white',
+              },
+            },
+          }}
+        />
+        <Axis
+          id="x"
+          tickFormat={tickFormat}
+          position={Position.Bottom}
+          gridLine={{ visible: true }}
+          timeAxisLayerCount={2}
+          style={{
+            tickLabel: {
+              alignment: { horizontal: 'left' },
+              padding: 0,
+            },
+            tickLine: {
+              visible: true,
+              padding: 0,
+              size: 5,
+            },
+          }}
+        />
+        <Axis id="y" position={Position.Left} gridLine={{ visible: true }} />
+        <HistogramBarSeries
+          id="Area"
+          xScaleType={ScaleType.Time}
+          yScaleType={ScaleType.Linear}
+          xAccessor={0}
+          yAccessors={[1]}
+          data={limitedData}
+          timeZone="UTC"
+          color="#FF6666"
+        />
+      </Chart>
+      <Chart title={title} description={description} size={['100%', 250]}>
+        <Settings
+          baseTheme={useBaseTheme()}
+          legendPosition={{
+            floatingColumns: 1,
+            floating: true,
+            direction: 'vertical',
+            hAlign: 'right',
+            vAlign: 'top',
+          }}
+          showLegend
+          showLegendExtra
+          xDomain={xDomain}
+          theme={{
+            scales: {
+              histogramPadding: 0,
+              barsPadding: 0,
+            },
+          }}
+        />
+        <Axis
+          id="x"
+          tickFormat={tickFormat}
+          position={Position.Bottom}
+          gridLine={{ visible: true }}
+          timeAxisLayerCount={2}
+          style={{
+            tickLabel: {
+              alignment: { horizontal: 'left' },
+              padding: 0,
+            },
+            tickLine: {
+              visible: true,
+              padding: 0,
+              size: 5,
+            },
+          }}
+        />
+        <Axis id="y" position={Position.Left} gridLine={{ visible: true }} />
+        <LineSeries
+          id="Line"
+          xScaleType={ScaleType.Time}
+          yScaleType={ScaleType.Linear}
+          xAccessor={0}
+          yAccessors={[1]}
+          data={limitedData}
+          timeZone="UTC"
+        />
+      </Chart>
+      <Chart title={title} description={description} size={['100%', 250]}>
+        <Settings
+          baseTheme={useBaseTheme()}
+          showLegend
+          legendPosition={{
+            floatingColumns: 1,
+            floating: true,
+            direction: 'vertical',
+            hAlign: 'right',
+            vAlign: 'top',
+          }}
+          showLegendExtra
+          xDomain={xDomain}
+          theme={{
+            scales: {
+              histogramPadding: 0,
+              barsPadding: 0.2,
+            },
+          }}
+        />
+        <Axis
+          id="x"
+          position={Position.Bottom}
+          gridLine={{ visible: true }}
+          timeAxisLayerCount={2}
+          style={{
+            tickLabel: {
+              alignment: { horizontal: 'center' },
+              padding: 0,
+            },
+            tickLine: {
+              visible: true,
+              padding: 0,
+              size: 5,
+            },
+          }}
+          tickFormat={(d) => niceTimeFormatter(domain)(d, { timeZone: 'UTC' })}
+        />
+        <Axis id="y" position={Position.Left} gridLine={{ visible: true }} />
+        <BarSeries
+          id="Categorical"
+          xScaleType={ScaleType.Ordinal}
+          yScaleType={ScaleType.Linear}
+          xAccessor={0}
+          yAccessors={[1]}
+          data={limitedData}
+          timeZone="UTC"
+        />
+      </Chart>
+    </>
+  );
+};
+
+Example.parameters = {
+  resize: {
+    height: '100%',
+    maxHeight: '100%',
+    overflow: 'hidden',
+    resize: 'none',
+  },
+};
