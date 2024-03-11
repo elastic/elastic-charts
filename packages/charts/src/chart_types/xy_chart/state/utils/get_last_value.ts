464d50e473d237a0734beacbe92bd3d3fda1539f
Marco Vettorello <vettorello.marco@gmail.com>
2024-03-11 18:42:20 +0100

    feat: add aggregation functions

--- packages/charts/src/chart_types/xy_chart/state/utils/get_last_value.ts
+++ packages/charts/src/chart_types/xy_chart/state/utils/get_last_value.ts
@@ -8,18 +8,67 @@
 
 import { $Values } from 'utility-types';
 
+import {
+  firstNonNull,
+  lastNonNull,
+  median,
+  average,
+  min,
+  max,
+  sum,
+  countNonNull,
+  distinctCount,
+  variance,
+  stdDeviation,
+  range,
+  difference,
+  differencePercent,
+} from '../../../../common/aggregations';
 import { ScaleType } from '../../../../scales/constants';
 import { XDomain } from '../../domains/types';
-import { isDatumFilled } from '../../rendering/utils';
 import { DataSeries, DataSeriesDatum } from '../../utils/series';
 
-/** @internal */
+/** @public */
 export const LegendValue = Object.freeze({
   None: 'none' as const,
+  /** Last value considering all data points in the chart */
   LastValue: 'lastValue' as const,
+  /** Last non-null value */
   LastNonNullValue: 'lastNonNullValue' as const,
+  /** Average value considering all data points in the chart */
+  Average: 'average' as const,
+  /** Median value considering all data points in the chart */
+  Median: 'median' as const,
+  /** Maximum value considering all data points in the chart */
+  Max: 'max' as const,
+  /** Minimum value considering all data points in the chart */
+  Min: 'min' as const,
+  /** First value considering all data points in the chart */
+  FirstValue: 'firstValue' as const,
+  /** First non-null value */
+  FirstNonNullValue: 'firstNonNullValue' as const,
+  /** Sum of al values plotted in the chart */
+  Total: 'total' as const,
+  /** number of data points plotted in the chart */
+  Count: 'count' as const,
+  /** number of data points with different values plotted in the chart */
+  DistinctCount: 'distinctCount' as const,
+  /** Variance of all data points plotted in the chart */
+  Variance: 'variance' as const,
+  /** Standard deviation of all data points plotted in the chart */
+  StdDeviation: 'stdDeviation' as const,
+  /**  Difference between min and max values */
+  Range: 'range' as const,
+  /** Difference between first and last values */
+  Difference: 'difference' as const,
+  /** % difference between first and last values */
+  DifferencePercent: 'differencePercent' as const,
+  /** Partition section value */
+  Value: 'value' as const,
+  /** Partition section value in percent */
+  Percent: 'percent' as const,
 });
-/** @internal */
+/** @public */
 export type LegendValue = $Values<typeof LegendValue>;
 
 /**
@@ -38,18 +87,39 @@ export function getLegendValue(
   if (xDomain.type === ScaleType.Ordinal) {
     return null;
   }
-
   switch (type) {
-    case LegendValue.LastNonNullValue: {
-      const last = series.data.findLast((d) => d.x === xDomain.dataDomain[1] && valueAccessor(d) !== null);
-      return last ? valueAccessor(last) : null;
-    }
+    case LegendValue.FirstNonNullValue:
+      return firstNonNull(series.data, valueAccessor);
+    case LegendValue.FirstValue:
+      return series.data.length === 0 ? null : valueAccessor(series.data.at(1)!);
+    case LegendValue.LastNonNullValue:
+      return lastNonNull(series.data, valueAccessor);
     case LegendValue.LastValue:
-      const last = series.data.findLast((d) => d.x === xDomain.dataDomain[1]);
-      if (last && !isDatumFilled(last)) {
-        return valueAccessor(last);
-      }
-      return null;
+      return series.data.length === 0 ? null : valueAccessor(series.data.at(-1)!);
+    case LegendValue.Average:
+      return average(series.data, valueAccessor);
+    case LegendValue.Median:
+      return median(series.data, valueAccessor);
+    case LegendValue.Min:
+      return min(series.data, valueAccessor);
+    case LegendValue.Max:
+      return max(series.data, valueAccessor);
+    case LegendValue.Total:
+      return sum(series.data, valueAccessor).sum;
+    case LegendValue.Count:
+      return countNonNull(series.data, valueAccessor);
+    case LegendValue.DistinctCount:
+      return distinctCount(series.data, valueAccessor);
+    case LegendValue.Variance:
+      return variance(series.data, valueAccessor);
+    case LegendValue.StdDeviation:
+      return stdDeviation(series.data, valueAccessor);
+    case LegendValue.Range:
+      return range(series.data, valueAccessor);
+    case LegendValue.Difference:
+      return difference(series.data, valueAccessor);
+    case LegendValue.DifferencePercent:
+      return differencePercent(series.data, valueAccessor);
     default:
     case LegendValue.None:
       return null;
---
+++ packages/charts/src/common/aggregations.ts
@@ -0,0 +1,165 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+/** @internal */
+export function sum<D>(arr: D[], accessor: (d: D) => number | null): { sum: number; validCount: number } {
+  return arr.reduce<{ sum: number; validCount: number }>(
+    (acc, d) => {
+      const value = accessor(d);
+      return {
+        sum: acc.sum + (value !== null ? value : 0),
+        validCount: value !== null ? acc.validCount + 1 : acc.validCount,
+      };
+    },
+    { sum: 0, validCount: 0 },
+  );
+}
+
+/** @internal */
+export function average<D>(arr: D[], accessor: (d: D) => number | null): number {
+  const total = sum(arr, accessor);
+  if (total.validCount === 0) return NaN;
+  return total.sum / total.validCount;
+}
+
+/** @internal */
+export function median<D>(input: D[], accessor: (d: D) => number | null): number {
+  const arr: number[] = input.reduce<number[]>((acc, d) => {
+    const value = accessor(d);
+    if (value !== null) {
+      acc.push(value);
+    }
+    return acc;
+  }, []);
+  if (!arr.length) return NaN;
+  const s = arr.toSorted((a, b) => a - b);
+  const mid = Math.floor(s.length / 2);
+  if (s.length % 2) {
+    return s[mid] ?? NaN;
+  }
+  return ((s[mid - 1] ?? NaN) + (s[mid] ?? NaN)) / 2;
+}
+
+/** @internal */
+export function min<D>(arr: D[], accessor: (d: D) => number | null): number {
+  if (arr.length === 0) return NaN;
+  return arr.reduce<number>((m, d) => {
+    const value = accessor(d);
+    return value !== null ? Math.min(m, value) : m;
+  }, +Infinity);
+}
+/** @internal */
+export function max<D>(arr: D[], accessor: (d: D) => number | null): number {
+  if (arr.length === 0) return NaN;
+  return arr.reduce<number>((m, d) => {
+    const value = accessor(d);
+    return value !== null ? Math.max(m, value) : m;
+  }, -Infinity);
+}
+
+/** @internal */
+export function range<D>(arr: D[], accessor: (d: D) => number | null): number {
+  if (arr.length === 0) return NaN;
+  const minMax = arr.reduce<{ min: number; max: number }>(
+    (m, d) => {
+      const value = accessor(d);
+      return {
+        max: value !== null ? Math.max(m.max, value) : m.max,
+        min: value !== null ? Math.min(m.min, value) : m.min,
+      };
+    },
+    { min: +Infinity, max: -Infinity },
+  );
+  return minMax.max - minMax.min;
+}
+
+/** @internal */
+export function countNonNull<D>(arr: D[], accessor: (d: D) => number | null): number {
+  return arr.filter((d) => accessor(d) !== null).length;
+}
+/** @internal */
+export function distinctCount<D>(arr: D[], accessor: (d: D) => number | null): number {
+  return new Set(nonNullArray(arr, accessor)).size;
+}
+
+/** @internal */
+export function nonNullArray<D>(arr: D[], accessor: (d: D) => number | null): number[] {
+  return arr.reduce<number[]>((acc, d) => {
+    const value = accessor(d);
+    if (value !== null) {
+      acc.push(value);
+    }
+    return acc;
+  }, []);
+}
+
+/** @internal */
+export function variance<D>(arr: D[], accessor: (d: D) => number | null): number {
+  const nonNullArr = nonNullArray(arr, accessor);
+  const ddof = 1;
+  const total = sum(nonNullArr, (d) => d);
+  const avg = total.sum / total.validCount;
+  const squareDiffs = nonNullArr.map((d) => {
+    return Math.abs(Math.pow(d - avg, 2));
+  });
+  const totalSumOfSquareDiffs = sum(squareDiffs, (d: number) => d);
+  return totalSumOfSquareDiffs.sum / (totalSumOfSquareDiffs.validCount - ddof);
+}
+
+/** @internal */
+export function stdDeviation<D>(arr: D[], accessor: (d: D) => number | null): number {
+  const v = variance(arr, accessor);
+  const std = Math.sqrt(v);
+  return std;
+}
+
+/** @internal */
+export function firstNonNull<D>(arr: D[], accessor: (d: D) => number | null): number | null {
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (!item) continue;
+    const value = accessor(item);
+    if (value) {
+      return value;
+    }
+  }
+  return null;
+}
+
+/** @internal */
+export function lastNonNull<D>(arr: D[], accessor: (d: D) => number | null): number | null {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const item = arr[i];
+    if (!item) continue;
+    const value = accessor(item);
+    if (value) {
+      return value;
+    }
+  }
+  return null;
+}
+
+/** @internal */
+export function difference<D>(arr: D[], accessor: (d: D) => number | null): number | null {
+  const first = firstNonNull(arr, accessor);
+  const last = lastNonNull(arr, accessor);
+  if (first !== null && last !== null) {
+    return last - first;
+  }
+  return null;
+}
+
+/** @internal */
+export function differencePercent<D>(arr: D[], accessor: (d: D) => number | null): number | null {
+  const first = firstNonNull(arr, accessor);
+  const last = lastNonNull(arr, accessor);
+  if (first !== null && last !== null) {
+    return (last - first) / last;
+  }
+  return null;
+}
