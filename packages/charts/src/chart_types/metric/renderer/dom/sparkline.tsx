5742a9471119e40382be60e5ed884c9c88d13ff3
dej611 <dej611@gmail.com>
2024-04-18 12:44:49 +0200

    :bug: Sort and squash multiple series

--- packages/charts/src/chart_types/metric/renderer/dom/sparkline.tsx
+++ packages/charts/src/chart_types/metric/renderer/dom/sparkline.tsx
@@ -21,6 +21,52 @@ export const getSparkLineColor = (color: MetricWTrend['color']) => {
   return hslToColor(h, s, l >= 0.8 ? l - 0.1 : l + 0.1, a);
 };
 
+// Sometimes the trend data arrives with multiple series
+// appended one on top of each other. This function makes
+/**
+ * sure to realign all the histograms and implicitly stack them
+ * @internal
+ */
+export const getSafeTrendData = (trend: MetricWTrend['trend']) => {
+  let lastX = trend[0]?.x;
+  // Skip all the work asap if trend is already sorted
+  const shouldBeSorted =
+    // eslint-disable-next-line eqeqeq
+    lastX != null &&
+    trend.some(({ x }) => {
+      // eslint-disable-next-line eqeqeq
+      if (lastX == null) {
+        return false;
+      }
+      if (lastX > x) {
+        return true;
+      }
+      lastX = x;
+    });
+  if (!shouldBeSorted) {
+    return trend;
+  }
+  // Group by x and sum all the y values
+  return Object.entries(
+    trend.reduce(
+      (memo, { x, y }) => {
+        // eslint-disable-next-line eqeqeq
+        if (memo[x] == null) {
+          memo[x] = 0;
+        }
+        // eslint-disable-next-line eqeqeq
+        if (y != null) {
+          memo[x] += y;
+        }
+        return memo;
+      },
+      {} as Record<number, number>,
+    ),
+  )
+    .map(([x, y]) => ({ x: Number(x), y }))
+    .sort((a, b) => a.x - b.x);
+};
+
 /** @internal */
 export const SparkLine: FunctionComponent<{
   id: string;
@@ -29,8 +75,9 @@ export const SparkLine: FunctionComponent<{
   if (!trend) {
     return null;
   }
-  const [xMin, xMax] = extent(trend.map((d) => d.x));
-  const [, yMax] = extent(trend.map((d) => d.y));
+  const groupedTrendData = getSafeTrendData(trend);
+  const [xMin, xMax] = extent(groupedTrendData.map((d) => d.x));
+  const [, yMax] = extent(groupedTrendData.map((d) => d.y));
   const xScale = (value: number) => (value - xMin) / (xMax - xMin);
   const yScale = (value: number) => value / yMax;
 
@@ -65,7 +112,7 @@ export const SparkLine: FunctionComponent<{
         <rect x={0} y={0} width={1} height={1} fill={color} />
 
         <path
-          d={path.area(trend)}
+          d={path.area(groupedTrendData)}
           transform="translate(0, 0.5),scale(1,0.5)"
           fill={getSparkLineColor(color)}
           stroke="none"
