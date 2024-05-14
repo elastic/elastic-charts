d5c3e69dfa1e5116ddb1fbd50c92f2eeb57f0825
dej611 <dej611@gmail.com>
2024-05-14 11:00:41 +0200

    :sparkles: Add new command for playwright

--- e2e/tests/legend_stories.test.ts
+++ e2e/tests/legend_stories.test.ts
@@ -160,13 +160,6 @@ test.describe('Legend stories', () => {
       );
     });
     test('should change aria label to hidden when clicked', async ({ page }) => {
-      // in this specific tests we need to detect the OS specific (mostly if it's Mac or not)
-      // as "Control" key has a different behaviour there vs others
-      const isMac: string = String(
-        await page.evaluate(() => {
-          return navigator.userAgent.includes('Mac');
-        }),
-      );
       await common.loadElementFromURL(page)(
         'http://localhost:9001/?path=/story/legend--positioning&knob-position=right',
         '.echLegendItem__label',
@@ -182,7 +175,7 @@ test.describe('Legend stories', () => {
       await page.keyboard.press('Tab');
       await page.keyboard.press('Tab');
       // hold the meta/control key to hide rather than isolate
-      await page.keyboard.down(isMac ? 'Meta' : 'Control');
+      await page.keyboard.down('ControlOrMeta');
       await page.keyboard.press('Enter');
 
       const hiddenResults: number[] = [];
