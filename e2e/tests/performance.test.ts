b46e43d69a74a869685e01ba36dc536e53dbabef
Marco Vettorello <vettorello.marco@gmail.com>
2024-09-09 14:53:22 +0200

    update test

--- e2e/tests/performance.test.ts
+++ e2e/tests/performance.test.ts
@@ -8,33 +8,32 @@
 
 import { test, expect } from '@playwright/test';
 
-import { CommonPage } from '../page_objects';
+import { common, CommonPage } from '../page_objects';
 
 test.describe('Performance', () => {
-  test('Get performance metrics', async ({ page, browser }) => {
-    await browser.startTracing(page, { path: './perfTraces.json', screenshots: true });
+  for (let i = 0; i < 10; i++) {
+    test(`Get performance metrics ${i}`, async ({ page, browser }) => {
+      await browser.startTracing(page, { path: 'perfTraces.json', screenshots: true });
 
-    //To tell the CDPsession to record performance metrics.
-    // await session.send('Performance.enable');
-    const url = CommonPage.parseUrl('http://localhost:9001/?path=/story/test-cases--lens-stress-test');
-    await page.goto(url);
-    //await page.evaluate(() => window.performance.mark('Perf:Started'));
-    await page.waitForSelector('canvas');
-    // await page.evaluate(() => window.performance.mark('Perf:Ended'));
-    await page.evaluate(() => window.performance.measure('overall', 'Perf:Started', 'Perf:Ended'));
+      const url = CommonPage.parseUrl('http://localhost:9001/?path=/story/test-cases--lens-stress-test');
+      await page.goto(url);
 
-    console.log('=============CDP Performance Metrics===============');
+      await common.waitForElement(page)(common.chartWaitSelector);
+      await page.evaluate(() => window.performance.measure('overall', 'Perf:Started', 'Perf:Ended'));
 
-    const getAllMarksJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType('mark')));
-    const getAllMarks = await JSON.parse(getAllMarksJson);
-    console.log('window.performance.getEntriesByType("mark")', getAllMarks);
+      console.log('=============Performance Metrics===============');
 
-    const getAllMeasuresJson = await page.evaluate(() =>
-      JSON.stringify(window.performance.getEntriesByType('measure')),
-    );
-    const getAllMeasures = await JSON.parse(getAllMeasuresJson);
-    console.log('window.performance.getEntriesByType("measure")', getAllMeasures);
-    await browser.stopTracing();
-    expect(getAllMeasures[0].duration).toBeLessThan(500);
-  });
+      const getAllMarksJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType('mark')));
+      const getAllMarks = await JSON.parse(getAllMarksJson);
+      console.log('window.performance.getEntriesByType("mark")', getAllMarks);
+
+      const getAllMeasuresJson = await page.evaluate(() =>
+        JSON.stringify(window.performance.getEntriesByType('measure')),
+      );
+      const getAllMeasures = await JSON.parse(getAllMeasuresJson);
+      console.log('window.performance.getEntriesByType("measure")', getAllMeasures);
+      await browser.stopTracing();
+      expect(getAllMeasures[0].duration).toBeLessThan(2500);
+    });
+  }
 });
