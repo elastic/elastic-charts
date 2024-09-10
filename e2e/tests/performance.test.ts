0088bb1d6f8b70117996715abce6b9aef05ff9ee
Marco Vettorello <vettorello.marco@gmail.com>
2024-09-10 13:02:25 +0200

    fix ci errors

--- e2e/tests/performance.test.ts
+++ e2e/tests/performance.test.ts
@@ -14,23 +14,21 @@ test.describe('Performance', () => {
   for (let i = 0; i < 10; i++) {
     test(`Get performance metrics ${i}`, async ({ page, browser }) => {
       await browser.startTracing(page, { path: 'perfTraces.json', screenshots: true });
-
       const url = CommonPage.parseUrl('http://localhost:9001/?path=/story/test-cases--lens-stress-test');
       await page.goto(url);
-
       await common.waitForElement(page)(common.chartWaitSelector);
       await page.evaluate(() => window.performance.measure('overall', 'Perf:Started', 'Perf:Ended'));
-
+      // eslint-disable-next-line no-console
       console.log('=============Performance Metrics===============');
-
       const getAllMarksJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType('mark')));
       const getAllMarks = await JSON.parse(getAllMarksJson);
+      // eslint-disable-next-line no-console
       console.log('window.performance.getEntriesByType("mark")', getAllMarks);
-
       const getAllMeasuresJson = await page.evaluate(() =>
         JSON.stringify(window.performance.getEntriesByType('measure')),
       );
       const getAllMeasures = await JSON.parse(getAllMeasuresJson);
+      // eslint-disable-next-line no-console
       console.log('window.performance.getEntriesByType("measure")', getAllMeasures);
       await browser.stopTracing();
       // using these two values as min/max to understand if we are increasing or decreasing the rendering speed.
