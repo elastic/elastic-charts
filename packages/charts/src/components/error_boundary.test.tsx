dac798fd84e18503a5529e9f72745852cef2d526
nickofthyme <nicholas.partridge@elastic.co>
2025-01-13 10:13:50 -0600

    test: fix and add error boundary cases

---
+++ packages/charts/src/components/error_boundary.test.tsx
@@ -0,0 +1,64 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { mount } from 'enzyme';
+import React from 'react';
+
+import { Chart } from './chart';
+
+class SimpleErrorBoundary extends React.Component<{ onError?: (error: Error) => void }, { hasError: boolean }> {
+  onError?: (error: Error) => void;
+  constructor(props: { onError: (error: Error) => void }) {
+    super(props);
+    this.onError = props.onError;
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error): void {
+    this.onError?.(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div id="simple-error">Error occurred</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+describe('Error boundary', () => {
+  it('should render error boundary when error thrown inside chart', () => {
+    const onError = jest.fn();
+    const Series = () => {
+      throw new Error('What happened???');
+    };
+
+    const wrapper = mount(
+      <SimpleErrorBoundary onError={onError}>
+        <Chart size={[100, 100]} id="chart1">
+          <Series />
+        </Chart>
+      </SimpleErrorBoundary>,
+    );
+    const errorEl = wrapper.find('#simple-error');
+    const chartEl = wrapper.find('.echChart');
+
+    expect(errorEl.exists()).toBe(true);
+    expect(chartEl.exists()).toBe(false);
+    expect(onError).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: 'What happened???',
+      }),
+    );
+  });
+});
