5f936669f324d29356d6a3e9a0dc4e3b6e1ddaea
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-01-14 16:55:13 +0100

    fix style application

--- packages/charts/src/chart_types/xy_chart/state/selectors/get_axis_styles.ts
+++ packages/charts/src/chart_types/xy_chart/state/selectors/get_axis_styles.ts
@@ -9,17 +9,44 @@
 import { getAxisSpecsSelector } from './get_specs';
 import { createCustomCachedSelector } from '../../../../state/create_selector';
 import { getChartThemeSelector } from '../../../../state/selectors/get_chart_theme';
-import { mergePartial } from '../../../../utils/common';
+import { mergePartial, Position, type RecursivePartial } from '../../../../utils/common';
 import { AxisId } from '../../../../utils/ids';
 import { AxisStyle } from '../../../../utils/themes/theme';
 import { isVerticalAxis } from '../../utils/axis_type_utils';
+
+const MULTILAYER_TIME_AXIS_STYLE: RecursivePartial<AxisStyle> = {
+  tickLabel: {
+    visible: true,
+    padding: 0,
+    rotation: 0,
+    alignment: {
+      vertical: Position.Bottom,
+      horizontal: Position.Left,
+    },
+  },
+  tickLine: {
+    visible: true,
+    size: 0,
+    padding: 4,
+  },
+};
 
 /** @internal */
 export const getAxesStylesSelector = createCustomCachedSelector(
   [getAxisSpecsSelector, getChartThemeSelector],
   (axesSpecs, { axes: sharedAxesStyle }): Map<AxisId, AxisStyle | null> =>
-    axesSpecs.reduce((axesStyles, { id, style, gridLine, position }) => {
+    axesSpecs.reduce((axesStyles, { id, style, gridLine, position, timeAxisLayerCount }) => {
+      const timeAxisStyle = timeAxisLayerCount > 0 ? MULTILAYER_TIME_AXIS_STYLE : undefined;
       const gridStyle = gridLine && { gridLine: { [isVerticalAxis(position) ? 'vertical' : 'horizontal']: gridLine } };
-      return axesStyles.set(id, style ? mergePartial(sharedAxesStyle, { ...style, ...gridStyle }) : null);
+      return axesStyles.set(
+        id,
+        style || timeAxisStyle
+          ? mergePartial(sharedAxesStyle, {
+              ...(style ? style : {}),
+              ...(timeAxisStyle ? timeAxisStyle : {}),
+              ...gridStyle,
+            })
+          : null,
+      );
     }, new Map()),
 );
