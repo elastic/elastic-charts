b64ec1055282cb04a9a2b7cae4f8d7b2d4e391e1
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-03-03 14:20:16 +0100

    rename chart specific chart_state to chart_selectors

--- packages/charts/src/chart_types/chart_type_selectors.ts
+++ packages/charts/src/chart_types/chart_type_selectors.ts
@@ -6,16 +6,16 @@
  * Side Public License, v 1.
  */
 
-import { chartSelectorsFactory as bulletSelectorsFactory } from './bullet_graph/chart_state';
-import { chartSelectorsFactory as flameSelectorsFactory } from './flame_chart/chart_state';
-import { chartSelectorsFactory as goalSelectorsFactory } from './goal_chart/state/chart_state';
-import { chartSelectorsFactory as heatmapSelectorsFactory } from './heatmap/state/chart_state';
+import { chartSelectorsFactory as bulletSelectorsFactory } from './bullet_graph/chart_selectors';
+import { chartSelectorsFactory as flameSelectorsFactory } from './flame_chart/chart_selectors';
+import { chartSelectorsFactory as goalSelectorsFactory } from './goal_chart/state/chart_selectors';
+import { chartSelectorsFactory as heatmapSelectorsFactory } from './heatmap/state/chart_selectors';
 import { ChartType } from './index';
-import { chartSelectorsFactory as metricSelectorsFactory } from './metric/state/chart_state';
-import { chartSelectorsFactory as partitionSelectorsFactory } from './partition_chart/state/chart_state';
-import { chartSelectorsFactory as timeslipSelectorsFactory } from './timeslip/chart_state';
-import { chartSelectorsFactory as wordcloudSelectorsFactory } from './wordcloud/state/chart_state';
-import { chartSelectorsFactory as xyAxisChartSelectorsFactory } from './xy_chart/state/chart_state';
+import { chartSelectorsFactory as metricSelectorsFactory } from './metric/state/chart_selectors';
+import { chartSelectorsFactory as partitionSelectorsFactory } from './partition_chart/state/chart_selectors';
+import { chartSelectorsFactory as timeslipSelectorsFactory } from './timeslip/chart_selectors';
+import { chartSelectorsFactory as wordcloudSelectorsFactory } from './wordcloud/state/chart_selectors';
+import { chartSelectorsFactory as xyAxisChartSelectorsFactory } from './xy_chart/state/chart_selectors';
 import type { ChartSelectors } from '../state/chart_selectors';
 
 /** @internal */
