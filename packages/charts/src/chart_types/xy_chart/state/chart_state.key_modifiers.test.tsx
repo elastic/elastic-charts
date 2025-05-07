1568caf42b73934de225eae08012795f66eaefe3
Maria Iriarte <maria.iriarte@elastic.co>
2025-05-07 12:27:04 +0200

    Update tests for listeners

--- packages/charts/src/chart_types/xy_chart/state/chart_state.key_modifiers.test.tsx
+++ packages/charts/src/chart_types/xy_chart/state/chart_state.key_modifiers.test.tsx
@@ -11,39 +11,14 @@ import type { Store } from 'redux';
 import { createOnBrushEndCaller } from './selectors/on_brush_end_caller';
 import { createOnClickCaller } from './selectors/on_click_caller';
 import { ChartType } from '../..';
+import { simulateBrushSequence, simulateClickSequence } from '../../../mocks/interactions';
 import { MockGlobalSpec, MockSeriesSpec } from '../../../mocks/specs/specs';
 import { createMockBrushEndListener, MockStore } from '../../../mocks/store';
 import { ScaleType } from '../../../scales/constants';
 import { SpecType } from '../../../specs/spec_type';
-import { onMouseDown, onMouseUp, onPointerMove } from '../../../state/actions/mouse';
 import type { GlobalChartState } from '../../../state/chart_state';
 import { getModifierKeys, type KeyPressed, noModifierKeysPressed } from '../../../utils/keys';
 import { SeriesType } from '../utils/specs';
-
-type BrushEventTestCase = {
-  description: string;
-  start: { x: number; y: number };
-  end: { x: number; y: number };
-  /**
-   * An array of three timestamps representing:
-   * - The time when the brush started.
-   * - The time during the brush movement.
-   * - The time when the brush ended.
-   */
-  time: [number, number, number];
-  /**
-   * An array of four `KeyPressed` states representing:
-   * - The key state when the brush started.
-   * - The key state during the brush movement.
-   * - The key state when the brush ended.
-   * - The expected key state passed to the listener.
-   *
-   * We should only consider the modifier keys that were pressed during the mouseDown event,
-   * which marks the start of the sequence. Any changes to the keyPressed state that occur
-   * during the sequence (e.g., during pointerMove or mouseUp) should be ignored.
-   */
-  keys: [KeyPressed, KeyPressed, KeyPressed, KeyPressed];
-};
 
 const metaKeyPressed = getModifierKeys({ metaKey: true });
 
@@ -73,14 +48,6 @@ const barSeries = MockSeriesSpec.bar({
   ],
 });
 
-const POSITION = { x: 50, y: 75 };
-
-const START_POSITION_1 = { x: 0, y: 0 };
-const END_POSITION_1 = { x: 75, y: 0 };
-
-const START_POSITION_2 = { x: 75, y: 0 };
-const END_POSITION_2 = { x: 100, y: 0 };
-
 describe('Key Modifiers on BrushEnd', () => {
   let store: Store<GlobalChartState>;
   let mockBrushEndListener: ReturnType<typeof createMockBrushEndListener>;
@@ -97,67 +64,17 @@ describe('Key Modifiers on BrushEnd', () => {
     });
   });
 
-  const testCases = [
-    {
-      description: 'no modifier key pressed during the entire brush sequence',
-      start: START_POSITION_1,
-      end: END_POSITION_1,
-      time: [0, 200, 300],
-      keys: [noModifierKeysPressed, noModifierKeysPressed, noModifierKeysPressed, noModifierKeysPressed],
-    },
-    {
-      description: 'modifier key released mid-brush',
-      start: START_POSITION_2,
-      end: END_POSITION_2,
-      time: [400, 500, 600],
-      keys: [metaKeyPressed, noModifierKeysPressed, noModifierKeysPressed, metaKeyPressed],
-    },
-    {
-      description: 'modifier key pressed at the end of the brush event',
-      start: { x: 75, y: 0 },
-      end: { x: 2500, y: 0 },
-      time: [700, 800, 900],
-      keys: [noModifierKeysPressed, noModifierKeysPressed, metaKeyPressed, noModifierKeysPressed],
-    },
-    {
-      description: 'modifier key pressed mid-brush',
-      start: { x: 25, y: 0 },
-      end: { x: -20, y: 0 },
-      time: [1000, 1100, 1200],
-      keys: [noModifierKeysPressed, metaKeyPressed, noModifierKeysPressed, noModifierKeysPressed],
-    },
-    {
-      description: 'modifier key pressed during the entire brush sequence',
-      start: { x: 75, y: 0 },
-      end: { x: 2500, y: 0 },
-      time: [1300, 1400, 1500],
-      keys: [metaKeyPressed, metaKeyPressed, metaKeyPressed, metaKeyPressed],
-    },
-  ] satisfies BrushEventTestCase[];
-
-  test.each(testCases)('should handle $description', ({ start, end, time, keys }) => {
-    store.dispatch(onMouseDown({ position: start, time: time[0], keyPressed: keys[0] }));
-    store.dispatch(onPointerMove({ position: end, time: time[1], keyPressed: keys[1] }));
-    store.dispatch(onMouseUp({ position: end, time: time[2], keyPressed: keys[2] }));
-
-    expect(mockBrushEndListener).toHaveBeenCalledTimes(1);
-    expect(mockBrushEndListener).toHaveBeenCalledWith(expect.anything(), { keyPressed: keys[3] });
+  it('should call the brush end listener with no modifier keys pressed when keyPressed is not explicitly passed', () => {
+    simulateBrushSequence({ store });
+    verifyListenerCall(mockBrushEndListener, noModifierKeysPressed);
   });
 
-  test('should handle multiple brush sequences with different modifier keys', () => {
-    store.dispatch(onMouseDown({ position: START_POSITION_1, time: 100, keyPressed: metaKeyPressed }));
-    store.dispatch(onPointerMove({ position: END_POSITION_1, time: 200, keyPressed: noModifierKeysPressed }));
-    store.dispatch(onMouseUp({ position: END_POSITION_1, time: 300, keyPressed: noModifierKeysPressed }));
+  it('should call the brush end listener with the correct modifier keys', () => {
+    simulateBrushSequence({ store, keysOnMouseDown: metaKeyPressed });
+    verifyListenerCall(mockBrushEndListener, metaKeyPressed);
 
-    expect(mockBrushEndListener).toHaveBeenCalledTimes(1);
-    expect(mockBrushEndListener).toHaveBeenNthCalledWith(1, expect.anything(), { keyPressed: metaKeyPressed });
-
-    store.dispatch(onMouseDown({ position: START_POSITION_2, time: 400, keyPressed: noModifierKeysPressed }));
-    store.dispatch(onPointerMove({ position: END_POSITION_2, time: 500, keyPressed: noModifierKeysPressed }));
-    store.dispatch(onMouseUp({ position: END_POSITION_2, time: 600, keyPressed: noModifierKeysPressed }));
-
-    expect(mockBrushEndListener).toHaveBeenCalledTimes(2);
-    expect(mockBrushEndListener).toHaveBeenNthCalledWith(2, expect.anything(), { keyPressed: noModifierKeysPressed });
+    simulateBrushSequence({ store, time: [300, 400, 500], keysOnMouseDown: noModifierKeysPressed });
+    verifyListenerCall(mockBrushEndListener, noModifierKeysPressed, 2);
   });
 });
 
@@ -177,19 +94,21 @@ describe('Key Modifiers on ElementClick', () => {
     });
   });
 
-  test('should handle multiple brush sequences with different modifier keys', () => {
-    store.dispatch(onPointerMove({ position: POSITION, time: 0, keyPressed: noModifierKeysPressed }));
-    store.dispatch(onMouseDown({ position: POSITION, time: 100, keyPressed: metaKeyPressed }));
-    store.dispatch(onMouseUp({ position: POSITION, time: 200, keyPressed: noModifierKeysPressed }));
+  it('should call the element click listener with no modifier keys pressed when keyPressed is not explicitly passed', () => {
+    simulateClickSequence({ store });
+    verifyListenerCall(mockOnElementClick, noModifierKeysPressed);
+  });
 
-    expect(mockOnElementClick).toHaveBeenCalledTimes(1);
-    expect(mockOnElementClick).toHaveBeenNthCalledWith(1, expect.anything(), { keyPressed: metaKeyPressed });
+  it('should call the element click listener with the correct modifier keys', () => {
+    simulateClickSequence({ store, keysOnMouseDown: metaKeyPressed });
+    verifyListenerCall(mockOnElementClick, metaKeyPressed);
 
-    store.dispatch(onPointerMove({ position: POSITION, time: 300, keyPressed: noModifierKeysPressed }));
-    store.dispatch(onMouseDown({ position: POSITION, time: 400, keyPressed: noModifierKeysPressed }));
-    store.dispatch(onMouseUp({ position: POSITION, time: 500, keyPressed: noModifierKeysPressed }));
-
-    expect(mockOnElementClick).toHaveBeenCalledTimes(2);
-    expect(mockOnElementClick).toHaveBeenNthCalledWith(2, expect.anything(), { keyPressed: noModifierKeysPressed });
+    simulateClickSequence({ store, time: [300, 400, 500], keysOnMouseDown: metaKeyPressed });
+    verifyListenerCall(mockOnElementClick, metaKeyPressed, 2);
   });
 });
+
+function verifyListenerCall(mockListener: jest.Mock, expectedKeyPressed: KeyPressed, numberOfCalls: number = 1) {
+  expect(mockListener).toHaveBeenCalledTimes(numberOfCalls);
+  expect(mockListener).toHaveBeenNthCalledWith(numberOfCalls, expect.anything(), { keyPressed: expectedKeyPressed });
+}
