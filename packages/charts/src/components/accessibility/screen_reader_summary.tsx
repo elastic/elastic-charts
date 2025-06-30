a298272205ac3dd30cf44400d2dcd83b61bdbc60
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-06-30 10:14:44 +0200

    refactor(a11y): replace div with figcaption for better semantic structure and remove unnecessary wrapper div

--- packages/charts/src/components/accessibility/screen_reader_summary.tsx
+++ packages/charts/src/components/accessibility/screen_reader_summary.tsx
@@ -37,11 +37,11 @@ const ScreenReaderSummaryComponent = ({
   goalChartLabels,
 }: ScreenReaderSummaryStateProps) => {
   return (
-    <div className="echScreenReaderOnly" id={`${a11ySettings.descriptionId}-summary`}>
+    <figcaption className="echScreenReaderOnly" id={`${a11ySettings.descriptionId}-summary`}>
       <ScreenReaderLabel {...a11ySettings} goalChartLabels={goalChartLabels} />
       <ScreenReaderDescription {...a11ySettings} />
       <ScreenReaderTypes {...a11ySettings} chartTypeDescription={chartTypeDescription} goalChartData={goalChartData} />
-    </div>
+    </figcaption>
   );
 };
 
