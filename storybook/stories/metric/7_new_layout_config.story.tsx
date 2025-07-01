8bfbc3fb08d089a4099f7eba5ca0ff8f002b35d3
Maria Iriarte <maria.iriarte@elastic.co>
2025-07-01 12:33:20 +0200

    Re-organize new layout config story

--- storybook/stories/metric/7_new_layout_config.story.tsx
+++ storybook/stories/metric/7_new_layout_config.story.tsx
@@ -28,69 +28,66 @@ const getIcon =
     <EuiIcon type={type} width={width} height={height} fill={color} style={{ width, height }} />
   );
 
-const sizeMap: Record<'small' | 'medium' | 'large', Array<{ height: string; width: string }>> = {
-  small: [
-    { height: '100px', width: '200px' },
-    { height: '150px', width: '200px' },
-    { height: '200px', width: '200px' },
-    { height: '300px', width: '200px' },
-  ],
-  medium: [{ height: '400px', width: '400px' }],
-  large: [
-    { height: '600px', width: '600px' },
-    { height: '700px', width: '700px' },
-  ],
+const sizeMap = {
+  xxxs: { height: '100px', maxWidth: '200px' },
+  xxs: { height: '150px', maxWidth: '200px' },
+  xs: { height: '200px', maxWidth: '300px' },
+  s: { height: '300px', maxWidth: '200px' },
+  m: { height: '400px', maxWidth: '400px' },
+  l: { height: '500px', maxWidth: '500px' },
+  xl: { height: '600px', maxWidth: '600px' },
+  xxl: { height: '700px', maxWidth: '700px' },
 };
 
-// TODO: Add target to story
+const textConfigurationAndPositionGroup = 'Text configuration and position';
+const secondaryMetricGroup = 'Secondary metric';
+const colorGroup = 'Colors';
 
 export const Example: ChartsStory = (_, { title: storyTitle, description }) => {
-  // title and subtitle values
-  const title = text('title', 'Count of records');
-  const subtitle = text('subtitle', 'Little description of this component');
+  // Title and subtitle values
+  const title = text('Title', 'Count of records');
+  const subtitle = text('Subtitle', 'Little description of this component');
 
   // Visualization type
-  const progressOrTrend = select('progress or trend', { trend: 'trend', bar: 'bar', none: 'none' }, 'bar');
+  const progressOrTrend = select('Visualization type', { trend: 'trend', bar: 'bar', none: 'none' }, 'bar');
   const progressBarDirection = select(
-    'progress bar direction',
+    'Progress bar direction',
     { horizontal: 'horizontal', vertical: 'vertical' },
     'vertical',
   );
 
-  const maxDataPoints = number('trend data points', 30, { min: 0, max: 50, step: 1 });
-  const trendShape = customKnobs.fromEnum('trend shape', MetricTrendShape, MetricTrendShape.Area);
-  const trendA11yTitle = text('trend a11y title', 'The Cluster CPU Usage trend');
+  const maxDataPoints = number('Trend data points', 30, { min: 0, max: 50, step: 1 });
+  const trendShape = customKnobs.fromEnum('Trend shape', MetricTrendShape, MetricTrendShape.Area);
+  const trendA11yTitle = text('Trend a11y title', 'The Cluster CPU Usage trend');
   const trendA11yDescription = text(
-    'trend a11y description',
+    'Trend a11y description',
     'The trend shows a peak of CPU usage in the last 5 minutes',
   );
 
-  //
+  const progressMax = number('Progress max', 100);
+  const numberTextSwitch = boolean('Is numeric metric', true);
+  const value = text('Value', '55.23');
+  const valuePrefix = text('Value prefix', '');
+  const valuePostfix = text('Value postfix', ' %');
 
-  const progressMax = number('progress max', 100);
-  const numberTextSwitch = boolean('is numeric metric', true);
-  const value = text('value', '55.23');
-  const valuePrefix = text('value prefix', '');
-  const valuePostfix = text('value postfix', ' %');
+  // Secondary metric
 
-  //
+  const showExtra = boolean('Show extra', true, secondaryMetricGroup);
+  const useCustomExtra = boolean('Use custom extra', false, secondaryMetricGroup);
 
-  const showExtra = boolean('Show extra', true, 'Secondary metric');
-  const useCustomExtra = boolean('Use custom extra', false, 'Secondary metric');
-
-  let extra = text('Custom extra', 'last <b>5m</b>', 'Secondary metric');
+  let extra = text('Custom extra', 'last <b>5m</b>', secondaryMetricGroup);
   extra = extra.replace('&lt;b&gt;', '<b>');
   extra = extra.replace('&lt;/b&gt;', '</b>');
 
-  const secondaryMetricValue = text('Secondary metric value', '87.20 ↑', 'Secondary metric');
-  const label = text('Label', 'Last week', 'Secondary metric');
-  const colorByValue = boolean('Color by value', true, 'Secondary metric');
-  const badgeColor = color('Secondary metric value color', '#61A2FF', 'Secondary metric');
+  const secondaryMetricValue = text('Secondary metric value', '87.20 ↑', secondaryMetricGroup);
+  const label = text('Label', 'Last week', secondaryMetricGroup);
+  const colorByValue = boolean('Color by value', true, secondaryMetricGroup);
+  const badgeColor = color('Secondary metric value color', '#A71627', secondaryMetricGroup);
   const secondaryMetricValuePosition = select(
     'Secondary metric value position',
     { before: 'before', after: 'after' },
     'after',
-    'Secondary metric',
+    secondaryMetricGroup,
   );
 
   const secondaryProps = {
@@ -106,11 +103,10 @@ export const Example: ChartsStory = (_, { title: storyTitle, description }) => {
       : { extra: secondaryProps }
     : {};
 
-  //
+  // Colors
 
-  const group2 = 'Colors';
-  const metricColor = color('color', '#61A2FF', group2);
-  const useValueColor = boolean('Use value color', false, group2);
+  const metricColor = color('Metric color', '#61A2FF', colorGroup);
+  const useValueColor = boolean('Use value color', false, colorGroup);
   const valueColor = select(
     'Value color',
     {
@@ -124,20 +120,24 @@ export const Example: ChartsStory = (_, { title: storyTitle, description }) => {
       textAccentSecondary: '#047471',
     },
     '#1750BA',
-    group2,
+    colorGroup,
   );
-  const useBlendingBackground = boolean('use blending background', false, group2);
-  const blendingBackground = color('blending background', 'rgba(255,255,255,1)', group2);
-  const barBackground = color('Bar background', '#CAD3E2', group2);
+  const useBlendingBackground = boolean('Use blending background', false, colorGroup);
+  const blendingBackground = color('Blending background', 'rgba(255,255,255,1)', colorGroup);
+  const barBackground = color('Bar background', '#CAD3E2', colorGroup);
 
+  // Metric icon
   const iconType = 'warning';
+  const showIcon = boolean('Show metric icon', false, textConfigurationAndPositionGroup);
+  const iconAlign = select(
+    'Metric icon align',
+    { Left: 'left', Right: 'right' },
+    'right',
+    textConfigurationAndPositionGroup,
+  );
 
-  // icon
-  const showIcon = boolean('show icon', false);
-  const iconAlign = select('icon align', { Left: 'left', Right: 'right' }, 'right');
-
-  // value icon
-  const showValueIcon = boolean('show value icon', false);
+  // Value icon
+  const showValueIcon = boolean('Show value icon', false);
   const valueIconType = 'sortUp';
 
   const data = {
@@ -187,76 +187,104 @@ export const Example: ChartsStory = (_, { title: storyTitle, description }) => {
 
   // Configurations
 
-  const group1 = 'Text configuration and position';
-
-  // title and subtitle
-  const titlesTextAlign = getTextAlignKnob('Title and subtitle alignment', 'left', group1);
-  const titleWeight = select('Title weight', { Bold: 'bold', Regular: 'regular' }, 'regular', group1);
-  // value (primary metric)
-  const valuePosition = select('Primary metric position', { Bottom: 'bottom', Top: 'top' }, 'top', group1);
-  const valueTextAlign = getTextAlignKnob('Primary metric alignment', 'left', group1);
+  // Title and subtitle
+  const titlesTextAlign = getTextAlignKnob('Title and subtitle alignment', 'left', textConfigurationAndPositionGroup);
+  const titleWeight = select(
+    'Title weight',
+    { Bold: 'bold', Regular: 'regular' },
+    'regular',
+    textConfigurationAndPositionGroup,
+  );
+  // Value (primary metric)
+  const valuePosition = select(
+    'Primary metric position',
+    { Bottom: 'bottom', Top: 'top' },
+    'top',
+    textConfigurationAndPositionGroup,
+  );
+  const valueTextAlign = getTextAlignKnob('Primary metric alignment', 'left', textConfigurationAndPositionGroup);
   const valueFontSizeMode = select(
     'Primary metric font size mode',
     { Default: 'default', Fit: 'fit', Custom: 'custom' },
     'default',
-    group1,
+    textConfigurationAndPositionGroup,
   );
   const valueFontSize = number(
     'Primary metric font size (only if custom font size selected)',
     40,
     { min: 0, step: 10 },
-    group1,
+    textConfigurationAndPositionGroup,
   );
-  // extra (secondary metric)
-  const extraTextAlign = getTextAlignKnob('Extra element alignment', 'left', group1);
+  // Extra (secondary metric)
+  const extraTextAlign = getTextAlignKnob('Extra element alignment', 'left', textConfigurationAndPositionGroup);
+
+  // Breakpoints and size
+
+  const breakpointShow = select(
+    'Show breakpoint',
+    {
+      xxxs: 'xxxs',
+      xxs: 'xxs',
+      xs: 'xs',
+      s: 's',
+      m: 'm',
+      l: 'l',
+      xl: 'xl',
+      xxl: 'xxl',
+    },
+    'm',
+    textConfigurationAndPositionGroup,
+  );
+
+  const customSize = boolean('Custom size', false, textConfigurationAndPositionGroup);
+
+  const height = number('Metric height', 400, { min: 100, step: 10 }, textConfigurationAndPositionGroup);
+  const width = number('Metric width', 400, { min: 100, step: 10 }, textConfigurationAndPositionGroup);
+
+  const sizeStyle = customSize ? { height, maxWidth: width } : sizeMap[breakpointShow];
 
   const baseTheme = useBaseTheme();
 
-  const breakpointShow = select('Show breakpoints', { Small: 'small', Medium: 'medium', Large: 'large' }, 'small');
-  const breakpoints = sizeMap[breakpointShow];
-
   return (
     <EuiFlexGroup>
-      {breakpoints.map(({ height, width }, i) => (
-        <EuiFlexItem key={height} style={{ height, maxWidth: width }}>
-          <Chart title={storyTitle} description={description}>
-            <Settings
-              theme={{
-                metric: {
-                  blendingBackground: useBlendingBackground ? blendingBackground : undefined,
-                  valueFontSize: valueFontSizeMode === 'custom' ? valueFontSize : valueFontSizeMode,
-                  titlesTextAlign,
-                  valueTextAlign,
-                  extraTextAlign,
-                  iconAlign,
-                  valuePosition,
-                  titleWeight,
-                  barBackground,
-                },
-              }}
-              baseTheme={baseTheme}
-              onElementClick={([d]) => {
-                if (isMetricElementEvent(d)) {
-                  const { rowIndex, columnIndex } = d;
-                  onEventClickAction(
-                    `row:${rowIndex} col:${columnIndex} value:${configuredData[rowIndex][columnIndex].value}`,
-                  );
-                }
-              }}
-              onElementOver={([d]) => {
-                if (isMetricElementEvent(d)) {
-                  const { rowIndex, columnIndex } = d;
-                  onEventOverAction(
-                    `row:${rowIndex} col:${columnIndex} value:${configuredData[rowIndex][columnIndex].value}`,
-                  );
-                }
-              }}
-              onElementOut={() => onEventOutAction('out')}
-            />
-            <Metric id={`${i}`} data={configuredData} />
-          </Chart>
-        </EuiFlexItem>
-      ))}
+      <EuiFlexItem style={sizeStyle}>
+        <Chart title={storyTitle} description={description}>
+          <Settings
+            theme={{
+              metric: {
+                blendingBackground: useBlendingBackground ? blendingBackground : undefined,
+                valueFontSize: valueFontSizeMode === 'custom' ? valueFontSize : valueFontSizeMode,
+                titlesTextAlign,
+                valueTextAlign,
+                extraTextAlign,
+                iconAlign,
+                valuePosition,
+                titleWeight,
+                barBackground,
+              },
+            }}
+            baseTheme={baseTheme}
+            onElementClick={([d]) => {
+              if (isMetricElementEvent(d)) {
+                const { rowIndex, columnIndex } = d;
+                onEventClickAction(
+                  `row:${rowIndex} col:${columnIndex} value:${configuredData[rowIndex][columnIndex].value}`,
+                );
+              }
+            }}
+            onElementOver={([d]) => {
+              if (isMetricElementEvent(d)) {
+                const { rowIndex, columnIndex } = d;
+                onEventOverAction(
+                  `row:${rowIndex} col:${columnIndex} value:${configuredData[rowIndex][columnIndex].value}`,
+                );
+              }
+            }}
+            onElementOut={() => onEventOutAction('out')}
+          />
+          <Metric id="metric-id" data={configuredData} />
+        </Chart>
+      </EuiFlexItem>
     </EuiFlexGroup>
   );
 };
