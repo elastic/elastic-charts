b242a5d8707788b1c100adf73426c3585d5688de
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-07-03 12:02:37 +0200

    refactor(a11y): relocate goal chart summary utils to chart type directory

---
+++ packages/charts/src/chart_types/goal_chart/utils/summary_utils.tsx
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { ChartType } from '../../../chart_types';
+import type { GoalChartData } from '../state/selectors/get_goal_chart_data';
+
+/** @internal */
+export function createGoalChartDescription(chartType: ChartType | null, goalChartData?: GoalChartData): string | null {
+  const isGoalChart = chartType === ChartType.Goal || chartType === ChartType.Bullet;
+
+  if (!isGoalChart || !goalChartData || isNaN(goalChartData.maximum)) {
+    return null;
+  }
+
+  return `Minimum: ${goalChartData.minimum}, Maximum: ${goalChartData.maximum}, Target: ${goalChartData.target}, Value: ${goalChartData.value}`;
+}
--- packages/charts/src/chart_types/xy_chart/state/selectors/get_screen_reader_data.ts
+++ packages/charts/src/chart_types/xy_chart/state/selectors/get_screen_reader_data.ts
@@ -8,7 +8,6 @@
 
 import { computeSeriesDomainsSelector } from './compute_series_domains';
 import { getAxisSpecsSelector, getSeriesSpecsSelector } from './get_specs';
-import { createAxisDescriptions } from '../../../../components/accessibility/axis_summary_utils';
 import type { ChartSpecificScreenReaderData } from '../../../../state/chart_selectors';
 import { createCustomCachedSelector } from '../../../../state/create_selector';
 
@@ -24,12 +23,6 @@ export const getScreenReaderDataSelector = createCustomCachedSelector(
   [getSeriesSpecsSelector, getAxisSpecsSelector, computeSeriesDomainsSelector],
   (seriesSpecs, axisSpecs, seriesDomains): ChartSpecificScreenReaderData => {
     const summaryParts: string[] = [];
-
-    // Axis descriptions
-    if (axisSpecs.length > 0) {
-      const axisDescriptions = createAxisDescriptions(axisSpecs, seriesDomains);
-      summaryParts.push(...axisDescriptions);
-    }
 
     return {
       data: {
