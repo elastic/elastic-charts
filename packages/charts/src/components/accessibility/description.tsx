a71cbe3930910b0c0855f58350f53bfd6f3dfd77
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-08-11 21:29:58 +0200

    update snapshot, revert description tweaks

--- packages/charts/src/components/accessibility/description.tsx
+++ packages/charts/src/components/accessibility/description.tsx
@@ -12,9 +12,6 @@ import type { A11ySettings } from '../../state/selectors/get_accessibility_confi
 
 /** @internal */
 export function ScreenReaderDescription(props: A11ySettings) {
-  const { description, descriptionId } = props;
-
-  if (!description) return null;
-
-  return <p id={descriptionId}>{description}</p>;
+  if (!props.description) return null;
+  return <p id={props.descriptionId}>{props.description}</p>;
 }
