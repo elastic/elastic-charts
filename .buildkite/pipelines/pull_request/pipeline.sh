ca1232bb76c96f0b80a874ab899c21e8c6fc7e03
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-08-13 23:42:52 +0200

    Revert "move docker pre-pull from pre-command to build-pipeline"
    This reverts commit d6f4d37eba5a4275306eec54059c7f693b39ec55.

--- .buildkite/pipelines/pull_request/pipeline.sh
+++ .buildkite/pipelines/pull_request/pipeline.sh
@@ -7,23 +7,3 @@ cd '.buildkite'
 echo '--- Build pipeline'
 
 yarn build:pipeline
-
-echo '--- Docker environment information'
-docker --version
-docker info | grep -E "Server Version:|Registry:"
-
-echo '--- Pre-pulling Playwright Docker image for agent caching'
-
-# Docker Playwright version must match package.json version of @playwright/test
-PLAYWRIGHT_VERSION="1.47.2"
-
-# This pre-pull will cache the image on each Buildkite agent,
-# reducing the chance of network timeouts during the actual job execution.
-# If pulling the image fails, we'll fail the CI run early at this stage.
-if [ -n "$PLAYWRIGHT_VERSION" ]; then
-  if ! docker pull "mcr.microsoft.com/playwright:v${PLAYWRIGHT_VERSION}-focal"; then
-    echo "Error: Failed to pre-pull Playwright image mcr.microsoft.com/playwright:v${PLAYWRIGHT_VERSION}-focal"
-    echo "This is likely a network connectivity issue with Microsoft Container Registry"
-    exit 1
-  fi
-fi
