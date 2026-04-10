3102b1b482bc8c81a66f40e2d76c4a51971b0027
Abdul Zahid <awahab07@yahoo.com>
2026-04-10 17:32:25 +0200

    Upgrade docusaurus to `v3.10.0`.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -32,7 +32,11 @@ const config: Config = {
   projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
